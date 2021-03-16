3327c130223a46648c45681b2e948a645c41ea3a
Akash0003 <akash@webnexs.in>
2021-03-16 12:03:44 +0530

    Home page changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -371,7 +371,7 @@
             <div class="sign-user_card ">                    
                <div class="sign-in-page-data">
                   <div class="sign-in-from w-100 m-auto">
-                     <h3 class="mb-3 text-center">Sign in</h3>
+                     <h3 class="mb-3 text-center">{{ __('Sign-In') }}</h3>
                        @if (Session::has('message'))
                        <div class="alert alert-info">{{ Session::get('message') }}</div>
                     @endif
