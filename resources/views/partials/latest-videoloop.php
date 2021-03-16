3422d741d06d71ca2c0bfbfa46de2cb9d9abfb17
Akash0003 <akash@webnexs.in>
2021-03-16 16:51:43 +0530

    changes

--- resources/views/partials/latest-videoloop.php
+++ resources/views/partials/latest-videoloop.php
@@ -1,140 +1,183 @@
-<div class="slider" data-slick='{"slidesToShow": 4, "slidesToScroll": 4, "autoplay": false}'>
+<meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <title>Flicknexs</title>
+   <!-- Favicon -->
+   <link rel="shortcut icon" href="<?= URL::to('/'). '/assets/images/fl-logo.png';?>" />
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="<?= URL::to('/'). '/assets/css/bootstrap.min.css';?>" />
+    
+   <link rel="stylesheet" href="<?= URL::to('/'). '/assets/css/responsive.css';?>" />
+
+   <!--datatable CSS -->
+   <link rel="stylesheet" href="<?= URL::to('/'). '/assets/css/dataTables.bootstrap4.min.css';?>" />
+   <!-- Typography CSS -->
+   <link rel="stylesheet" href="<?= URL::to('/'). '/assets/css/typography.css';?>" />
+   <!-- Style CSS -->
+   <link rel="stylesheet" href="<?= URL::to('/'). '/assets/css/style.css';?>" />
+   <!-- Responsive CSS -->
+   <link rel="stylesheet" href="<?= URL::to('/'). '/assets/css/responsive.css';?>" />
+<div >
 <?php if(isset($latest_videos)) :
 foreach($latest_videos as $watchlater_video): ?>
 <?php endforeach; 
 endif; ?>
 </div>
-<div class="container " id="home-content">
-<h4 style="color:#fff;">Latest Videos</h4>
-    <div  style="margin-bottom:15px;border-bottom: 1px solid #232429;"></div>
-	<div class="row page-height">
-	<div class="slider" data-slick='{"slidesToShow": 4, "slidesToScroll": 4, "autoplay": false}'>
-        <?php  if(isset($latest_videos)) :
-			foreach($latest_videos as $watchlater_video): ?>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 new-art latest">
-	
-	<article class="block expand">
-		
-		<div class="block-thumbnail" >
-            <div class="play-button-block">
-				
-			<a  href="<?= ($settings->enable_https) ? secure_url('category') : URL::to('category') ?><?= '/videos/' . $watchlater_video->slug ?>">	
-                <div class="play-block">
-                    <i class="fa fa-play flexlink" aria-hidden="true"></i> 
-				</div></a>
-                <div class="detail-block">
-					<a class="title-dec" href="<?= ($settings->enable_https) ? secure_url('category') : URL::to('category') ?><?= '/videos/' . $watchlater_video->slug ?>">
-                <p class="movie-title"><?php echo __($watchlater_video->title); ?></p>
-					</a>
-					
-                <p class="movie-rating">
-                    <span class="star-rate"><i class="fa fa-star"></i><?= $watchlater_video->rating;?></span>
-                    <span class="viewers"><i class="fa fa-eye"></i>(<?= $watchlater_video->views;?>)</span>
-                    <span class="running-time"><i class="fa fa-clock-o"></i><?= gmdate('H:i:s', $watchlater_video->duration); ?></span>
-					</p>
+ <style>
+           .overflow-hidden {
+    margin-top: 60px;
+    overflow: hidden;
+}
+       </style>
+ 
+ <!-- MainContent -->
+<section id="iq-favorites">
+            <div class="container-fluid">
+               <div class="row">
+                  <div class="col-sm-12 overflow-hidden">
+                     <div class="iq-main-header d-flex align-items-center justify-content-between">
+                        <!--<h4 class="main-title"><a href="<?php echo URL::to('home') ?>">Latest Videos</a></h4> -->                     
+                     </div>
+                     <div class="favorites-contens">
+                        <ul class="favorites-slider list-inline  row p-0 mb-0">
+<?php if(isset($latest_videos)) :
+foreach($latest_videos as $watchlater_video): ?>
+                           <li class="slide-item">
+                              <a href="<?php echo URL::to('home') ?>">
+                                 <div class="block-images position-relative">
+                                    <div class="img-box">
+                                       <img src="<?php echo URL::to('/').'/public/uploads/images/'.$watchlater_video->image;  ?>" class="img-fluid" alt="">
+                                    </div>
+                                    <div class="block-description">
+                                       <h6><?php echo __($watchlater_video->title); ?></h6>
+                                       <div class="movie-time d-flex align-items-center my-2">
+                                          <div class="badge badge-secondary p-1 mr-2">13+</div>
+                                          <span class="text-white"><i class="fa fa-clock-o"></i><?= gmdate('H:i:s', $watchlater_video->duration); ?></span>
+                                       </div>
+                                       <div class="hover-buttons">
+                                           <a  href="<?php echo URL::to('category') ?><?= '/videos/' . $watchlater_video->slug ?>">	
+                                          <span class="btn btn-hover">
+                                          <i class="fa fa-play mr-1" aria-hidden="true"></i>
+                                          Play Now
+                                          </span>
+                                           </a>
+                                       </div>
+                                        <div>
+                                            <button class="show-details-button" data-id="<?= $watchlater_video->id;?>">
+                                                <span class="text-center thumbarrow-sec">
+                                                    <img src="<?php echo URL::to('/').'/assets/img/arrow-red.png';?>" class="thumbarrow thumbarrow-red" alt="right-arrow">
+                                                </span>
+                                                    </button></div>
+                                        </div>
+                                   <!-- <div class="block-social-info">
+                                       <ul class="list-inline p-0 m-0 music-play-lists">
+                                          <li><span><i class="ri-volume-mute-fill"></i></span></li>
+                                          <li><span><i class="ri-heart-fill"></i></span></li>
+                                          <li><span><i class="ri-add-line"></i></span></li>
+                                       </ul>
+                                    </div>-->
+                                 </div>
+                              </a>
+                           </li>
+                           
+                            <?php endforeach; 
+		                                   endif; ?>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         
+                                  <?php if(isset($latest_videos)) :
+foreach($latest_videos as $watchlater_video): ?>
+                                <div class="thumb-cont" id="<?= $watchlater_video->id;?>"  style="background:url('<?php echo URL::to('/').'/public/uploads/images/'.$watchlater_video->image;  ?>') no-repeat;background-size: cover;"> 
+                                    <div class="img-black-back">
+                                    </div>
+                                    <div align="right">
+                                    <button type="button" class="closewin btn btn-danger" id="<?= $watchlater_video->id;?>"><span aria-hidden="true">X</span></button>
+                                        </div>
+                                <div class="tab-sec">
+                                    <div class="tab-content">
+                                    <div id="overview<?= $watchlater_video->id;?>" class="container tab-pane active"><br>
+                                           <h1 class="movie-title-thumb"><?php echo __($watchlater_video->title); ?></h1>
+                                                   <p class="movie-rating">
+                                                    <span class="thumb-star-rate"><i class="fa fa-star fa-w-18"></i><?= $watchlater_video->rating;?></span>
+                                                    <span class="viewers"><i class="fa fa-eye"></i>(<?= $watchlater_video->views;?>)</span>
+                                                    <span class="running-time"><i class="fa fa-clock-o"></i><?= gmdate('H:i:s', $watchlater_video->duration); ?></span>
+                                                    </p>
+                                                  <p>Welcome</p>
+                                           	
+                                                       <!-- <div class="btn btn-danger btn-right-space br-0">
+                                                    <i class="fa fa-play flexlink" aria-hidden="true"></i> Play
+                                                </div>-->
+                                        <a href="<? URL::to('category') ?><?= '/videos/' . $watchlater_video->slug ?>" class="btn btn-hover"><i class="fa fa-play mr-2"
+                                 aria-hidden="true"></i>Play Now</a>
+                                    </div>
+        <div id="trailer<?= $watchlater_video->id;?>" class="container tab-pane "><br>
 
-				</div>
-				
-		<div>
-		<button class="show-details-button" data-id="<?= $watchlater_video->id;?>">
-			<span class="text-center thumbarrow-sec">
-				<!--<img src="<?php echo URL::to('/').'/assets/img/arrow-white.png';?>" class="thumbarrow thumbarrow-white" alt="left-arrow">-->
-				<img src="<?php echo URL::to('/').'/assets/img/arrow-red.png';?>" class="thumbarrow thumbarrow-red" alt="right-arrow">
-			</span>
-				</button></div>
-		</div>
+         <div class="block expand">
+		
+		<a class="block-thumbnail-trail" href="<? URL::to('category') ?><?= '/videos/' . $watchlater_video->slug ?>" >
+
 		
 				<?php if (!empty($watchlater_video->trailer)) { ?>
-                        <video width="100%" height="auto" class="play-video" poster="<?php echo URL::to('/').'/public/uploads/images/'.$watchlater_video->image;  ?>" data-play="hover" muted="muted">
+                        <video class="trail-vid" width="30%" height="auto" class="play-video" poster="<?php echo URL::to('/').'/public/uploads/images/'.$watchlater_video->image;  ?>" data-play="hover" muted="muted">
                                     <source src="<?= $watchlater_video->trailer; ?>" type="video/mp4">
 								 </video>
                             <?php } else { ?>
                                 <img src="<?php echo URL::to('/').'/public/uploads/images/'.$watchlater_video->image;  ?>" class="thumb-img">
 			
 		                   <?php } ?>  
+			            <div class="play-button-trail" >
+				
+<!--			<a  href="<? URL::to('category') ?><?= '/videos/' . $watchlater_video->slug ?>">	
+                <div class="play-block">
+                    <i class="fa fa-play flexlink" aria-hidden="true"></i> 
+				</div></a>-->
+                <div class="detail-block">
+<!--					<a class="title-dec" href="<? URL::to('category') ?><?= '/videos/' . $watchlater_video->slug ?>">
+                <p class="movie-title"><?php echo __($watchlater_video->title); ?></p>
+					</a>-->
+					
+                <!--<p class="movie-rating">
+                    <span class="star-rate"><i class="fa fa-star"></i><?= $watchlater_video->rating;?></span>
+                    <span class="viewers"><i class="fa fa-eye"></i>(<?= $watchlater_video->views;?>)</span>
+                    <span class="running-time"><i class="fa fa-clock-o"></i><?= gmdate('H:i:s', $watchlater_video->duration); ?></span>
+					</p>-->
+
+				</div>
 		</div>
+		</a>
 		<div class="block-contents">
 			<!--<p class="movie-title padding"><?php echo __($watchlater_video->title); ?></p>-->
         </div>
-	</article>
-</div>
-			<?php endforeach; 
-		endif; ?>
-        </div>
-	</div>
-
-</div>
-<!--<?php  if(isset($latest_videos)) :
-			foreach($latest_videos as $watchlater_video): ?>
-<div class="thumb-cont" id="<?= $watchlater_video->id;?>"  style="background:url('<?php echo URL::to('/').'/public/uploads/images/'.$watchlater_video->image;  ?>') no-repeat;background-size: cover;"> 
-	<div class="img-black-back">
-	</div>
-	<div align="right">
-	<button type="button" class="closewin btn btn-danger" id="<?= $watchlater_video->id;?>"><span aria-hidden="true">X</span></button>
-		</div>
-<div class="tab-sec">
-	<div class="tab-content">
-    <div id="overview<?= $watchlater_video->id;?>" class="container tab-pane active"><br>
-           <h1 class="movie-title-thumb"><?php echo __($watchlater_video->title); ?></h1>
-                   <p class="movie-rating">
-                    <span class="thumb-star-rate"><i class="fa fa-star fa-w-18"></i><?= $watchlater_video->rating;?></span>
-                    <span class="viewers"><i class="fa fa-eye"></i>(<?= $watchlater_video->views;?>)</span>
-                    <span class="running-time"><i class="fa fa-clock-o"></i><?= gmdate('H:i:s', $watchlater_video->duration); ?></span>
-					</p>
-	              <p>Welcome</p>
-			<a class="" href="<?= ($settings->enable_https) ? secure_url('category') : URL::to('category') ?><?= '/videos/' . $watchlater_video->slug ?>">	
-						<div class="btn btn-danger btn-right-space br-0">
-                    <i class="fa fa-play flexlink" aria-hidden="true"></i> Play
-				</div></a>
-    </div>
-    <div id="trailer<?= $watchlater_video->id;?>" class="container tab-pane "><br>
-
-          <p class="movie-title-thumb"><?php echo __($watchlater_video->title); ?></p>
-                   <p class="movie-rating">
-                    <span class="thumb-star-rate"><i class="fa fa-star fa-w-18"></i><?= $watchlater_video->rating;?></span>
-                    <span class="viewers"><i class="fa fa-eye"></i>(<?= $watchlater_video->views;?>)</span>
-                    <span class="running-time"><i class="fa fa-clock-o"></i><?= gmdate('H:i:s', $watchlater_video->duration); ?></span>
-					</p>
+	</div> 
 	            
     </div>
     <div id="like<?= $watchlater_video->id;?>" class="container tab-pane "><br>
      
-           <p class="movie-title-thumb"><?php echo __($watchlater_video->title); ?></p>
-                   <p class="movie-rating">
-                    <span class="thumb-star-rate"><i class="fa fa-star fa-w-18"></i><?= $watchlater_video->rating;?></span>
-                    <span class="viewers"><i class="fa fa-eye"></i>(<?= $watchlater_video->views;?>)</span>
-                    <span class="running-time"><i class="fa fa-clock-o"></i><?= gmdate('H:i:s', $watchlater_video->duration); ?></span>
-					</p>
-	              <p>Welcome</p>
-			<a class="" href="<?= ($settings->enable_https) ? secure_url('category') : URL::to('category') ?><?= '/videos/' . $watchlater_video->slug ?>">	
-						<div class="btn btn-danger btn-right-space br-0">
-                    <i class="fa fa-play flexlink" aria-hidden="true"></i> Play
-				</div></a>
+           <h2>More Like This</h2>
     </div>
      <div id="details<?= $watchlater_video->id;?>" class="container tab-pane "><br>
-           <p class="movie-title-thumb"><?php echo __($watchlater_video->title); ?></p>
-                   <p class="movie-rating">
-                    <span class="thumb-star-rate"><i class="fa fa-star fa-w-18"></i><?= $watchlater_video->rating;?></span>
-                    <span class="viewers"><i class="fa fa-eye"></i>(<?= $watchlater_video->views;?>)</span>
-                    <span class="running-time"><i class="fa fa-clock-o"></i><?= gmdate('H:i:s', $watchlater_video->duration); ?></span>
-					</p>
+        <h2>Description</h2>
 
     </div>
 	</div>
-	<ul class="nav nav-tabs">
-    <li class="nav-item">
-      <a class="nav-link active" data-toggle="tab" href="#overview<?= $watchlater_video->id;?>">OVERVIEW</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" data-toggle="tab" href="#trailer<?= $watchlater_video->id;?>">TRAILER AND MORE</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" data-toggle="tab" href="#like<?= $watchlater_video->id;?>">MORE LIKE THIS</a>
-    </li>
-     <li class="nav-item">
-      <a class="nav-link" data-toggle="tab" href="#details<?= $watchlater_video->id;?>">DETAILS </a>           
-    </li>
-  </ul>
+    <div align="center">
+            <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                      <a class="nav-link active" data-toggle="tab" href="#overview<?= $watchlater_video->id;?>">OVERVIEW</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" data-toggle="tab" href="#trailer<?= $watchlater_video->id;?>">TRAILER AND MORE</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" data-toggle="tab" href="#like<?= $watchlater_video->id;?>">MORE LIKE THIS</a>
+                    </li>
+                     <li class="nav-item">
+                      <a class="nav-link" data-toggle="tab" href="#details<?= $watchlater_video->id;?>">DETAILS </a>           
+                    </li>
+              </ul>
+        </div>
 
 
 	
@@ -142,6 +185,41 @@ endif; ?>
 
 <?php endforeach; 
 endif; ?>
+</section>
+
+<!-- Imported styles on this page -->
+	 <script src="<?= URL::to('/'). '/assets/js/jquery.min.js';?>"></script>
+   <script src="<?= URL::to('/'). '/assets/js/popper.min.js';?>"></script>
+   <script src="<?= URL::to('/'). '/assets/css/bootstrap.min.css';?>"></script>
+   <script src="<?= URL::to('/'). '/assets/js/jquery.dataTables.min.js';?>"></script>
+   <script src="<?= URL::to('/'). '/assets/js/dataTables.bootstrap4.min.js';?>"></script>
+   <!-- Appear JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/jquery.appear.js';?>"></script>
+   <!-- Countdown JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/countdown.min.js';?>"></script>
+   <!-- Select2 JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/select2.min.js';?>"></script>
+   <!-- Counterup JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/waypoints.min.js';?>"></script>
+   <script src="<?= URL::to('/'). '/assets/js/jquery.counterup.min.js';?>"></script>
+   <!-- Wow JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/wow.min.js';?>"></script>
+   <!-- Slick JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/slick.min.js';?>"></script>
+   <!-- Owl Carousel JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/owl.carousel.min.js';?>"></script>
+   <!-- Magnific Popup JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/jquery.magnific-popup.min.js';?>"></script>
+   <!-- Smooth Scrollbar JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/smooth-scrollbar.js';?>"></script>
+   <!-- apex Custom JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/apexcharts.js';?>"></script>
+   <!-- Chart Custom JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/chart-custom.js';?>"></script>
+   <!-- Custom JavaScript -->
+   <script src="<?= URL::to('/'). '/assets/js/custom.js';?>"></script>
+	<!-- End Notifications -->
+
  <script> 
         
         $(document).ready(function() { 
