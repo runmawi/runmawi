9035d65b030a3586b2c995ad7eec5cd4d3f2cf28
Barath <barath@webnexs.in>
2021-06-29 09:43:00 +0530

    env

--- resources/views/first_landing.php
+++ resources/views/first_landing.php
@@ -51,7 +51,7 @@
         
         <div class="col-sm-4 button d-flex">
             <div>
-              <select style="padding:6px 10px;">
+              <select  style="padding:6px 10px;">
                   <option>English</option>
                   <option>Hindi</option>
                   <option>Tamil</option>
