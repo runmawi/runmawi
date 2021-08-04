58d6af313b7ad1561e5005981f7ab3cc1a634953
Amrudha Webnexs <amrudha@webnexs.in>
2021-08-04 21:04:37 +0530

    reset pwd

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -6,7 +6,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Lang;
 
-class ResetPassword extends Notification
+class ResetPasswordNotification extends Notification
 {
     /**
      * The password reset token.
