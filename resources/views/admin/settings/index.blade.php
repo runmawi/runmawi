c825f6ed909e29e784a0a788b61ef1618fa3fc97
apple <apple@apples-MacBook-Air.local>
2021-09-14 10:24:07 +0530

    Admin Settings

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -74,7 +74,7 @@
 
 		<div class="panel panel-primary col-md-6 mt-3 p-0" data-collapsed="0"> <div class="panel-heading"> 
 			<div class="panel-title"><label>Logo</label></div> <div class="panel-options"> <a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a> </div></div> 
-			<div class="panel-body" style="display: block; > 
+			<div class="panel-body" style="display: block;"> 
 				@if(!empty($settings->logo))
 					<img src="{{ URL::to('/') . '/public/uploads/settings/' . $settings->logo }}" style="max-height:100px" />
 				@endif
