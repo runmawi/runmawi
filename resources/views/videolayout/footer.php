8a3802b3d97fc3fa9ec97a180d4913aa22fc6516
apple <apple@apples-MacBook-Air.local>
2021-11-09 15:27:52 +0530

    video

--- resources/views/videolayout/footer.php
+++ resources/views/videolayout/footer.php
@@ -14,7 +14,6 @@
         <!-- <script type="text/javascript" src="js/videojs-contrib-hls.js"></script> -->
           <!-- <script src="js/forward.js"></script> -->
   	<!-- <script src="assets/videojs-resolution-switcher/lib/videojs-resolution-switcher.js"></script> -->
-          <script type="text/javascript" src="js/videoplayer.js"></script>
 
           <?php 
     if($playerui_settings->thumbnail == 1){  ?>
@@ -29,6 +28,8 @@
                 <script type="text/javascript" src="js/videojs-watermark.min.js"></script>
 
      <?php } else{ } ?>
+     <script type="text/javascript" src="js/videoplayer.js"></script>
+
             <!-- <script src="https://cdn.jsdelivr.net/hls.js/latest/hls.js"></script> -->
             <!-- <script src="js/videojs-contrib-quality-levels.js"></script> -->
 
