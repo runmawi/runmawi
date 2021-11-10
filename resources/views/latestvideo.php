9bb2a04e7deabaeec049cd7b63db70281fa41d2a
Bright <manoj@webnexs.in>
2021-11-10 22:48:14 +0530

    latest video

--- resources/views/latestvideo.php
+++ resources/views/latestvideo.php
@@ -12,7 +12,7 @@
                         <ul class="category-page list-inline row p-0 mb-0">
                             <?php if(isset($latest_videos)) :
                            foreach($latest_videos as $latest_video): ?>
-                           <li class="slide-item col-sm-3 col-md-3 col-xs-12">
+                           <li class="slide-item col-sm-2 col-md-2 col-xs-12">
                               <a href="<?php echo URL::to('home') ?>">
                                  <div class="block-images position-relative">
                                     <div class="img-box">
