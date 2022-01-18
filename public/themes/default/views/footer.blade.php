39d4c6a6dc19fcca7ebe162c396786d87551efd4
Webnexs Manikandan <manikandan@webnexs.in>
2022-01-18 16:51:29 +0530

    defaultview_frontend

---
+++ public/themes/default/views/footer.blade.php
@@ -0,0 +1,302 @@
+<?php $settings = App\Setting::first();
+ $user = App\User::where('id','=',1)->first(); 
+ $app_setting = App\AppSetting::where('id','=',1)->first();
+ $session = session()->all();
+
+?>
+
+<footer class="mb-0">
+         <div class="container-fluid">
+            <div class="block-space">
+               <div class="row align-items-center">
+                   <div class="col-lg-3 col-md-4 col-sm-12 r-mt-15">
+                       <a class="navbar-brand" href="<?php echo URL::to('home') ?>"> <img src="<?php echo URL::to('/').'/public/uploads/settings/'. $settings->logo ; ?>" class="c-logo" alt="<?php echo $settings->website_name; ?>"> </a>
+                     <div class="d-flex mt-2">
+                       <?php if(!empty($settings->facebook_page_id)){?>
+                        <a href="<?php echo $settings->facebook_page_id; ?>" target="_blank"  class="s-icon">
+                        <i class="ri-facebook-fill"></i>
+                        </a>
+                        <?php } ?>
+                        <?php  if(!empty($settings->skype_page_id)){?>
+                        <a href=" <?php echo $settings->skype_page_id; ?>" class="s-icon">
+                        <i class="ri-skype-fill"></i>
+                        </a>
+                        <?php } ?>
+                        <?php   if(!empty($settings->instagram_page_id)){?>
+                        <a href="<?php echo $settings->instagram_page_id; ?>" class="s-icon">
+                        <i class="fa fa-instagram"></i>
+                        </a>
+                        <?php } ?>
+                        <?php  if(!empty($settings->twitter_page_id)){?>
+                        <a href="<?php echo $settings->twitter_page_id; ?>" class="s-icon">
+                        <i class="fa fa-twitter"></i>
+                        </a>
+                        <?php } ?>
+                        <?php if(!empty($settings->linkedin_page_id)){?>
+                        <a href="<?php echo $settings->linkedin_page_id; ?>" class="s-icon">
+                        <i class="ri-linkedin-fill"></i>
+                        </a>
+                        <?php } ?>
+                        <?php if(!empty($settings->whatsapp_page_id)){ ?>
+                        <a href="<?php echo $settings->whatsapp_page_id; ?>" class="s-icon">
+                        <i class="ri-whatsapp-fill"></i>
+                        </a>
+                        <?php } ?>
+                        <?php if(!empty($settings->youtube_page_id)){ ?>
+                        <a href="<?php echo $settings->youtube_page_id; ?>" class="s-icon">
+                        <i class="fa fa-youtube"></i>
+                        </a>
+                        <?php } ?>
+                        <?php if(!empty($settings->google_page_id)){ ?>
+                        <a href="<?php echo $settings->google_page_id; ?>" class="s-icon">
+                        <i class="fa fa-google-plus"></i>
+                        </a>
+                        <?php } ?>
+                        
+                        <?php if(!empty($app_setting->android_url) || !empty($app_setting->ios_url)){ ?>
+                          <!-- <label for="">Mobile App</label> -->
+                        <?php } ?>
+                        <?php if(!empty($app_setting->android_url)){ ?>
+                        <a href="<?php echo$app_setting->android_url; ?>" class="s-icon">
+                        <i class="fa fa-android"></i>
+                        </a>
+                        <?php } ?>
+                        <?php if(!empty($app_setting->ios_url)){ ?>
+                        <a href="<?php echo$app_setting->android_url; ?>" class="s-icon">
+                        <i class="fa fa-apple"></i>
+                        </a>
+                        <?php } ?>
+                        <!-- //  <a href="https://www.google.com/<?php //echo GoogleId();?>" target="_blank" class="s-icon">
+                        // <i class="fa fa-google-plus"></i>
+                        // </a> -->
+                     </div>
+                  </div>
+                  
+                  <div class="col-lg-3 col-md-4 col-sm-12 p-0">
+                     <ul class="f-link list-unstyled mb-0">
+                        <!-- <li><a href="<?php echo URL::to('home') ?>">Movies</a></li> -->
+                        <!-- <li><a href="<?php echo URL::to('tv-shows') ?>">Tv Shows</a></li> -->
+                        <!-- <li><a href="<?php echo URL::to('home') ?>">Coporate Information</a></li> -->
+                        <?php if($user->package == 'Pro' && empty($session['password_hash']) || empty($session['password_hash']) ){ ?> 
+                          <li><a href="<?php echo URL::to('/cpp/signup') ;?>">Content Partner Portal</a></li>
+                          <li><a href="<?php echo URL::to('/advertiser/register') ;?>">Advertiser Portal</a></li>
+                        <?php }else{ }?>
+                     </ul>
+                  </div>
+                  <!--<div class="col-lg-3 col-md-4">
+                     <ul class="f-link list-unstyled mb-0">
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms & Conditions</a></li>
+                        <li><a href="#">Help</a></li>
+                     </ul>
+                  </div>-->
+                  <?php $video_category = App\VideoCategory::where('footer',1)->get(); ?>
+                  <div class="col-lg-3 col-md-4">
+                      <div class="row">
+
+                     <ul class="f-link list-unstyled mb-0 catag">
+                     <?php foreach($video_category as $key => $category) { ?>
+                        <li><a href="<?php echo  URL::to('category').'/'.$category->slug ;?>"><?php echo $category->name ;?></a></li>
+                        <?php } ?>
+                          </ul>
+                          <ul class="f-link list-unstyled mb-0">
+                        
+                         <!-- <li><a href="<?php echo URL::to('category/horror'); ?>">Horror</a></li>
+                         <li><a href="<?php echo URL::to('category/mystery'); ?>">Mystery</a></li>
+                         <li><a href="<?php echo URL::to('category/Romance'); ?>">Romance</a></li> -->
+                          </ul>
+                      </div>
+				</div>
+                   <div class="col-lg-3 col-md-4 p-0">
+                      <ul class="f-link list-unstyled mb-0">    
+						<?php 
+                        $pages = App\Page::all();
+                        foreach($pages as $page): ?>
+							<li><a href="<?php echo URL::to('page'); ?><?= '/' . $page->slug ?>"><?= __($page->title) ?></a></li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+                  
+                   </div>
+               </div>
+            </div>
+         <div class="copyright py-2">
+            <div class="container-fluid">
+               <p class="mb-0 text-center font-size-14 text-body" style="color:#fff!important;"><?php echo $settings->website_name ; ?> - 2021 All Rights Reserved</p>
+            </div>
+         </div>
+      </footer>
+
+          <!-- back-to-top End -->
+     <!-- back-to-top End -->
+      <!-- jQuery, Popper JS -->
+      <script src="<?= URL::to('/'). '/assets/js/jquery-3.4.1.min.js';?>"></script>
+      <script src="<?= URL::to('/'). '/assets/js/popper.min.js';?>"></script>
+      <!-- Bootstrap JS -->
+      <script src="<?= URL::to('/'). '/assets/js/bootstrap.min.js';?>"></script>
+      <!-- Slick JS -->
+      <script src="<?= URL::to('/'). '/assets/js/slick.min.js';?>"></script>
+      <!-- owl carousel Js -->
+      <script src="<?= URL::to('/'). '/assets/js/owl.carousel.min.js';?>"></script>
+      <!-- select2 Js -->
+      <script src="<?= URL::to('/'). '/assets/js/select2.min.js';?>"></script>
+      <!-- Magnific Popup-->
+      <script src="<?= URL::to('/'). '/assets/js/jquery.magnific-popup.min.js';?>"></script>
+      <!-- Slick Animation-->
+      <script src="<?= URL::to('/'). '/assets/js/slick-animation.min.js';?>"></script>
+      <!-- Custom JS-->
+      <script src="<?= URL::to('/'). '/assets/js/custom.js';?>"></script>
+       <script>
+    $(document).ready(function () {
+      $(".thumb-cont").hide();
+      $(".show-details-button").on("click", function () {
+        var idval = $(this).attr("data-id");
+        $(".thumb-cont").hide();
+        $("#" + idval).show();
+      });
+		$(".closewin").on("click", function () {
+        var idval = $(this).attr("data-id");
+        $(".thumb-cont").hide();
+        $("#" + idval).hide();
+      });
+    });
+  </script>
+<script>
+function about(evt , id) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    
+  }
+	
+  document.getElementById(id).style.display = "block";
+ 
+}
+// Get the element with id="defaultOpen" and click on it
+//document.getElementById("defaultOpen").click();
+</script>
+
+<script type="text/javascript">
+  $(document).ready(function () {
+    $('.searches').on('keyup',function() {
+      var query = $(this).val();
+      //alert(query);
+      // alert(query);
+       if (query !=''){
+      $.ajax({
+        url:"<?php echo URL::to('/search');?>",
+        type:"GET",
+        data:{
+          'country':query}
+        ,
+        success:function (data) {
+          $('.search_list').html(data);
+        }
+      }
+            )
+       } else {
+            $('.search_list').html("");
+       }
+    }
+                     );
+    $(document).on('click', 'li', function(){
+      var value = $(this).text();
+      $('.search').val(value);
+      $('.search_list').html("");
+    }
+                  );
+  }
+                   );
+</script>
+<!--<script>
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+</script>-->
+
+
+ <script src="https://cdn.plyr.io/3.6.3/plyr.polyfilled.js"></script>
+ <script src="https://cdn.rawgit.com/video-dev/hls.js/18bb552/dist/hls.min.js"></script>
+          
+ <script>
+  // alert($('#hls_m3u8').val());
+
+   document.addEventListener('DOMContentLoaded', () => {
+	// const source = 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8';
+  const source = $('#hls_m3u8').val();
+  // alert(source);
+	const video = document.querySelector('video');
+	
+	// For more options see: https://github.com/sampotts/plyr/#options
+	// captions.update is required for captions to work with hls.js
+	const player = new Plyr(video, {captions: {active: true, update: true, language: 'en'}});
+	
+	if (!Hls.isSupported()) {
+		video.src = source;
+	} else {
+		// For more Hls.js options, see https://github.com/dailymotion/hls.js
+		const hls = new Hls();
+		hls.loadSource(source);
+		hls.attachMedia(video);
+		window.hls = hls;
+		
+		// Handle changing captions
+		player.on('languagechange', () => {
+			// Caption support is still flaky. See: https://github.com/sampotts/plyr/issues/994
+			setTimeout(() => hls.subtitleTrack = player.currentTrack, 50);
+		});
+	}
+	
+	// Expose player so it can be used from the console
+	window.player = player;
+
+});
+// const player = new Plyr('#trailor-videos');
+
+</script>
+ <script src="plyr-plugin-capture.js"></script>
+ <script src="<?= URL::to('/'). '/assets/admin/dashassets/js/plyr-plugin-capture.js';?>"></script>
+
+ <script>
+        const player = new Plyr('#videoPlayer',{
+          controls: [
+
+      'play-large',
+			'restart',
+			'rewind',
+			'play',
+			'fast-forward',
+			'progress',
+			'current-time',
+			'mute',
+			'volume',
+			'captions',
+			'settings',
+			'pip',
+			'airplay',
+			'fullscreen',
+			'capture'
+		],
+    i18n:{
+    // your other i18n
+    capture: 'capture'
+}
+
+        });
+
+      </script>
+</body>
+</html>
---
+++ public/themes/default/views/partials/home/continue-watching.php
@@ -0,0 +1,111 @@
+<?php  if(count($cnt_watching) > 0) : ?>
+  <?php  if(!empty($data['password_hash'])) { 
+ $id = Auth::user()->id ; } else { $id = 0 ; } ?>
+<div class="iq-main-header d-flex align-items-center justify-content-between">
+<h4 class="main-title"><a href="<?php echo URL::to('home') ?>">Continue Watching</a></h4>                      
+                 </div>
+                 <div class="favorites-contens">
+                    <ul class="favorites-slider list-inline  row p-0 mb-0">
+                         <?php  if(isset($cnt_watching)) :
+                         foreach($cnt_watching as $cont_video): 
+                          ?>
+                       <li class="slide-item">
+                          <a  href="<?php echo URL::to('category') ?><?= '/videos/' . $cont_video->slug ?>">
+                             <div class="block-images position-relative">
+                             <!-- block-images -->
+                                <div class="img-box">
+                                
+                                   <!-- <img src="<?php echo URL::to('/').'/public/uploads/images/'.$cont_video->image;  ?>" class="img-fluid" alt=""> -->
+                                   <video width="100%" height="auto" class="play-video" poster="<?php echo URL::to('/').'/public/uploads/images/'.$cont_video->image;  ?>"  data-play="hover" >
+                                    <source src="<?php echo $cont_video->trailer;  ?>" type="video/mp4">
+                                      </video>
+                                     
+                                     <div class="corner-text-wrapper">
+                                        <div class="corner-text">
+                                          <?php  if(!empty($cont_video->ppv_price)){?>
+                                          <p class="p-tag1"><?php echo $currency->symbol.' '.$cont_video->ppv_price; ?></p>
+                                          <?php }elseif( !empty($cont_video->global_ppv || !empty($cont_video->global_ppv) && $cont_video->ppv_price == null)){ ?>
+                                            <p class="p-tag1"><?php echo $cont_video->global_ppv.' '.$currency->symbol; ?></p>
+                                            <?php }elseif($cont_video->global_ppv == null && $cont_video->ppv_price == null ){ ?>
+                                            <p class="p-tag"><?php echo "Free"; ?></p>
+                                            <?php } ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="block-description">
+                                   <h6><?php echo __($cont_video->title); ?></h6>
+                                   <div class="movie-time d-flex align-items-center my-2">
+                                      <div class="badge badge-secondary p-1 mr-2"><?php echo $cont_video->age_restrict.' '.'+' ?></div>
+                                      <span class="text-white"><i class="fa fa-clock-o"></i> <?= gmdate('H:i:s', $cont_video->duration); ?></span>
+                                   </div>
+                                    
+                                    
+                                    
+                                    <div class="hover-buttons text-white">
+                                        <a class="text-white" href="<?php echo URL::to('category') ?><?= '/videos/' . $cont_video->slug ?>" >
+                                            <i class="fa fa-play mr-1" aria-hidden="true"></i>Watch Now
+                                        </a>
+                                        <div>
+                                    <!-- <a   href="<?php  // echo URL::to('category') ?><?  // '/wishlist/' . $cont_video->slug ?>" class="text-white mt-4"><i class="fa fa-plus" aria-hidden="true"></i> Add to Watchlist -->
+                                    <!-- </a> -->
+                                    
+                                            <!-- <div class="d-flex" style="color:white;" id="<?= $cont_video->id ?>">
+                                                <span style="color: white;"class="mywishlist <?php // if(isset($mywishlisted->id)): ?>active<?php //endif; ?>" data-authenticated="<?= !Auth::guest() ?>" data-videoid="<?= $cont_video->id ?>">
+                                    <i style="" <?php // if(isset($mywishlisted->id)): ?> class="ri-heart-fill" <?php //else: ?> class="ri-heart-line " <?php // endif; ?> style="" ></i>
+
+                                                </span>
+                                              <div style="color:white;" id="<?= $cont_video->id ?>"><?php // if(@$cont_video->mywishlisted->user_id == $id && @$watchlater_video->cont_video->video_id == $cont_video->id  ) { echo "Remove From Wishlist"; } else { echo "Add To Wishlist" ; } ?></div> 
+
+                                            </div>  -->
+                                        </div>
+                                    </div>
+                                </div>
+                              </div>
+                          </a>
+                       </li>
+                       <?php                     
+                        endforeach; 
+                                   endif; ?>
+                    </ul>
+                 </div>
+                 <?php endif; ?>
+
+
+<script>
+$('.mywishlist').click(function(){
+     var video_id = $(this).data('videoid');
+        if($(this).data('authenticated')){
+            $(this).toggleClass('active');
+            if($(this).hasClass('active')){
+                    $.ajax({
+                        url: "<?php echo URL::to('/mywishlist');?>",
+                        type: "POST",
+                        data: { video_id : $(this).data('videoid'), _token: '<?= csrf_token(); ?>'},
+                        dataType: "html",
+                        success: function(data) {
+                          if(data == "Added To Wishlist"){
+                            
+                            $('#'+video_id).text('') ;
+                            $('#'+video_id).text('Remove From Wishlist');
+                            $("body").append('<div class="add_watch" style="z-index: 100; position: fixed; top: 73px; margin: 0 auto; left: 81%; right: 0; text-align: center; width: 225px; padding: 11px; background: #38742f; color: white;">Media added to wishlist</div>');
+                          setTimeout(function() {
+                            $('.add_watch').slideUp('fast');
+                          }, 3000);
+                          }else{
+                            
+                            $('#'+video_id).text('') ;
+                            $('#'+video_id).text('Add To Wishlist');
+                            $("body").append('<div class="remove_watch" style="z-index: 100; position: fixed; top: 73px; margin: 0 auto; left: 81%; text-align: center; right: 0; width: 225px; padding: 11px; background: hsl(11deg 68% 50%); color: white;">Media removed from wishlist</div>');
+                          setTimeout(function() {
+                          $('.remove_watch').slideUp('fast');
+                          }, 3000);
+                          }               
+                    }
+                });
+            }                
+        } else {
+          window.location = '<?= URL::to('login') ?>';
+      }
+  });
+
+</script>
