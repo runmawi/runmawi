83eb1e72d233008626ac68be77116fdaa02a9394
Webnexs Manikandan <manikandan@webnexs.in>
2022-02-18 22:20:46 +0530

    reels

--- public/themes/default/views/partials/home/Reels-video.blade.php
+++ public/themes/default/views/partials/home/Reels-video.blade.php
@@ -16,16 +16,14 @@
                                 </a>
                             </div>
                                 
-                            <div class="block-description">
-                                <a  href="<?php echo URL::to('Reals_videos') ?><?= '/videos/' . $reel->slug ?>">
-                                     <h6><?php echo __($reel->title); ?></h6>
-                                </a>
+                            <div class="block-description" >
+                                <a  class="text-white"  data-toggle="modal" data-target="#Reels">
+                                     <h6><?php echo __($reel->title); ?></h6></a>
                                 
                                <div class="hover-buttons">
-                                   <a class="text-white" href="<?php echo URL::to('Reals_videos') ?><?= '/videos/' . $reel->slug ?>" >
-                                         <i class="fa fa-play mr-1" aria-hidden="true"></i> Watch Now
-                                  </a>
-                            </div>
+                                   <a class="text-white"  data-toggle="modal" data-target="#Reels"  >
+                                         <i class="fa fa-play mr-1" aria-hidden="true"></i> Watch Now </a>
+                               </div>
                         </div>
                     </div>
                 </a>
@@ -33,3 +31,52 @@
                      <?php endforeach; endif; ?>
     </ul>
 </div>
+
+
+
+
+<!-- Reels Modal -->
+
+<div class="modal fade" id="Reels" tabindex="-1" role="dialog" aria-labelledby="Reels" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+            <div class="modal-body">
+                <video id="ReelsVideos" class="" poster="<?= URL::to('/') . '/public/uploads/images/' . $video->image ?>"
+                    controls data-setup='{"controls": true, "aspectRatio":"16:9", "fluid": true}'>
+                    <source src="<?php echo URL::to('public/uploads/reelsVideos').'/'.$video->reelvideo;?>" type="video/mp4" label='720p' res='720'/> 
+                </video>
+            </div>
+      </div>
+    </div>
+</div>
+
+
+
+<script src="https://cdn.plyr.io/3.6.3/plyr.polyfilled.js"></script>
+
+<script>
+
+
+// for Reels videos
+const player = new Plyr('#ReelsVideos',{
+          controls: [
+
+      'play-large',
+			'restart',
+			'rewind',
+			'play',
+			'fast-forward',
+			'progress',
+			'current-time',
+			'mute',
+			'volume',
+			'captions',
+			'settings',
+			'pip',
+			'airplay',
+			'fullscreen',
+			'capture'
+		],
+});
+    </script>
+ 
