0b4bb3d8a2c7895d55c2fddf897e15e9f11fe23f
Webnexs Manikandan <manikandan@webnexs.in>
2022-02-28 15:11:07 +0530

    payment

--- database/migrations/2020_06_19_144931_create_users_table.php
+++ database/migrations/2020_06_19_144931_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
                 $table->string('coupon_expired')->nullable();
                 $table->string('email')->unique();
                 $table->timestamp('email_verified_at')->nullable();
-                $table->timestamp('subscription_ends_at')->nullable();
+                $table->string('subscription_ends_at')->nullable();
                 $table->longText('paypal_end_at')->nullable();
                 $table->string('password')->nullable();
                 $table->string('avatar')->nullable();
