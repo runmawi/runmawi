cb116ca2d74c8e189570f55b7e157a7e14070135
unknown <illvarasi@webnexs.com>
2022-03-08 12:57:15 +0530

    api changes

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -405,7 +405,7 @@ class ApiAuthController extends Controller
   {
 
     $settings = Setting::first();
-    $mobile = $request->user_ip;
+    $userIp = $request->user_ip;
     $device_name = $request->device_name;
     $email = $request->email;
     $token = $request->token;
