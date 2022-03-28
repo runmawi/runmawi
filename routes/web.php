9a2deefa18622f4a6acdbbc75156a2a343fdc036
unknown <illvarasi@webnexs.com>
2022-03-28 21:26:41 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ Route::post('/moderatoruser/create', 'ModeratorsUserController@store');
 Route::post('/Dashboard_Revenue', 'ModeratorsUserController@Dashboard_Revenue');
 Route::post('/upgadeSubscription', 'PaymentController@UpgadeSubscription');
 Route::get('/admin/filemanager', 'FileManagerController@index');
+Route::post('/admin/exportCsv', 'AdminUsersController@exportCsv');
+Route::post('/admin/start_date_url', 'AdminUsersController@StartDateRecord');
+Route::post('/admin/end_date_url', 'AdminUsersController@StartEndDateRecord');
 
 Route::post('/register1', 'HomeController@PostcreateStep1');
 Route::get('/verify-request', 'HomeController@VerifyRequest');
