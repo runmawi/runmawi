455a9a32b06a096f6336d2511b4a3d1c46bffc72
Barath <barath@webnexs.in>
2022-04-22 14:36:44 +0530

    changes

--- public/themes/default/views/video.php
+++ public/themes/default/views/video.php
@@ -707,7 +707,7 @@ Auth::user()->role == 'admin' && $video->type != "" || Auth::user()->role =="sub
 <div class="text-white">
     <p class="trending-dec w-100 mb-0 text-white mt-2"><?php echo __($video->description); ?></p>
     <p class="trending-dec w-100 mb-0 text-white mt-2">Starring :</p>
-    <p class="trending-dec w-100 mb-0 text-white mt-2">Genres :</p>
+    <p class="trending-dec w-100 mb-0 text-white mt-2">Genres :<span></span></p>
     <p class="trending-dec w-100 mb-0 text-white mt-2">This Movie is :</p>
     <p class="trending-dec w-100 mb-0 text-white mt-2">Subtitles :</p>
     <p class="trending-dec w-100 mb-0 text-white mt-2">Audio Languages :</p>
