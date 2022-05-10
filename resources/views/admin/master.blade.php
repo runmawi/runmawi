a5f86026b807c2862659728cf9fb9164271adeb5
Manikandan <manikandan@webnexs.in>
2022-05-10 19:48:46 +0530

    live player size

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -571,7 +571,13 @@ if($package == "Basic" && auth()->user()->role == "subscriber" || $package == "B
                 <li><a href="{{ URL::to('admin/ads_plans') }}" class="iq-waves-effect"><img class="" src="<?php echo  URL::to('/assets/img/icon/ad3.svg')?>"><span> Ads Plans</span></a></li>
 
                 <li><a href="{{ URL::to('admin/ads_revenue') }}" class="iq-waves-effect"><img class="" src="<?php echo  URL::to('/assets/img/icon/ana.svg')?>"><span> Ads Revenue</span></a></li>
+
+                <li><a href="{{ URL::to('admin/calendar-event') }}" class="iq-waves-effect"><img  height="40" width="40" class="" src="<?php echo  URL::to('/assets/img/icon/ads-rev.svg')?>"><span> Calendar Events</span></a></li>
+                
+                <li><a href="{{ URL::to('admin/ad_campaign') }}" class="iq-waves-effect"><img  height="40" width="40" class="" src="<?php echo  URL::to('/assets/img/icon/ads-rev.svg')?>"><span> Ad Campaigns</span></a></li>
+
                 @endif
+
 
                     {{-- Geo Fencing --}}
                <p class="lnk">Geo Fencing</p>
@@ -791,6 +797,11 @@ if($package == "Basic" && auth()->user()->role == "subscriber" || $package == "B
                 <li><a href="{{ URL::to('admin/ads_plans') }}" class="iq-waves-effect"><img  height="40" width="40" class="" src="<?php echo  URL::to('/assets/img/icon/ads-plan.svg')?>"><span> Ads Plans</span></a></li>
 
                 <li><a href="{{ URL::to('admin/ads_revenue') }}" class="iq-waves-effect"><img  height="40" width="40" class="" src="<?php echo  URL::to('/assets/img/icon/ads-rev.svg')?>"><span> Ads Revenue</span></a></li>
+
+                <li><a href="{{ URL::to('admin/calendar-event') }}" class="iq-waves-effect"><img  height="40" width="40" class="" src="<?php echo  URL::to('/assets/img/icon/ads-rev.svg')?>"><span> Calendar Events</span></a></li>
+                
+                <li><a href="{{ URL::to('admin/ad_campaign') }}" class="iq-waves-effect"><img  height="40" width="40" class="" src="<?php echo  URL::to('/assets/img/icon/ads-rev.svg')?>"><span> Ad Campaigns</span></a></li>
+
                 @endif
 
                 
@@ -1012,6 +1023,11 @@ if($package == "Basic" && auth()->user()->role == "subscriber" || $package == "B
                 <li><a href="{{ URL::to('admin/ads_plans') }}" class="iq-waves-effect"><img class="" src="<?php echo  URL::to('/assets/img/icon/ad3.svg')?>"><span> Ads Plans</span></a></li>
 
                 <li><a href="{{ URL::to('admin/ads_revenue') }}" class="iq-waves-effect"><img class="" src="<?php echo  URL::to('/assets/img/icon/ana.svg')?>"><span> Ads Revenue</span></a></li>
+
+                <li><a href="{{ URL::to('admin/calendar-event') }}" class="iq-waves-effect"><img  height="40" width="40" class="" src="<?php echo  URL::to('/assets/img/icon/ads-rev.svg')?>"><span> Calendar Events</span></a></li>
+                
+                <li><a href="{{ URL::to('admin/ad_campaign') }}" class="iq-waves-effect"><img  height="40" width="40" class="" src="<?php echo  URL::to('/assets/img/icon/ads-rev.svg')?>"><span> Ad Campaigns</span></a></li>
+
                 @endif
 
                        {{-- Geo Fencing --}}
