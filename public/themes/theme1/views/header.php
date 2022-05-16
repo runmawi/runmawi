6d8e62d1a67a6033eb48860bf23d3d9ca0a1624f
M sanjai Kumar <illvarasi@webnexs.com>
2022-05-16 22:39:34 +0530

    header issue

--- public/themes/theme1/views/header.php
+++ public/themes/theme1/views/header.php
@@ -25,9 +25,11 @@ if(!empty($data['password_hash']) && empty($uppercase) || empty($data['password_
       $created_at = date_format($date,"Y-m-d");
       $filldate = date('Y-m-d', strtotime($created_at. ' + 10 day'));
       $currentdate = date('Y-m-d');
+      $DOB = $users->DOB;
    }else{
       $currentdate = null ;
       $filldate = null ;
+      $DOB = null;
    }
 
 // exit();UA-42534483-14
@@ -634,12 +636,12 @@ $data = Session::all();
 $(document).ready(function(){
 var currentdate = "<?=  $currentdate ?>";
 var filldate = "<?= $filldate ?>";
-var DOB = "<?= $users->DOB ?>";
+var DOB = "<?= $DOB ?>";
 
 // console.log(DOB);
 // console.log(currentdate);
 
-if(filldate == currentdate &&  !empty(DOB)  &&  currentdate != null &&  filldate != null){       
+if(filldate == currentdate &&  DOB != null && !empty(DOB) &&  currentdate != null &&  filldate != null){       
 $("body").append('<div class="add_watch" style="z-index: 100; position: fixed; top: 73px; margin: 0 auto; left: 81%; right: 0; text-align: center; width: 225px; padding: 11px; background: #38742f; color: white;">Add Your DOB for Amazing video experience</div>');
 setTimeout(function() {
 $('.add_watch').slideUp('fast');
