999cd1094623d55b494a0703e35ecf0498f0d85c
manikandan <manikandan@webnexs.in>
2022-06-09 12:32:29 +0530

    search

--- public/themes/default/partials/category_header.blade.php
+++ public/themes/default/partials/category_header.blade.php
@@ -443,11 +443,11 @@ input:checked + .sliderk:before {
                         </div>
                         <div class="navbar-right menu-right">
                            <ul class="d-flex align-items-center list-inline m-0">
-                              <!-- <li class="nav-item nav-icon">
+                              <li class="nav-item nav-icon">
                                  <a href="<?php echo URL::to('/').'/searchResult';?>" class="search-toggle device-search">
-                                     
-                                 <i class="ri-search-line"></i>
+                                    <i class="ri-search-line"></i>
                                  </a>
+
                                  <div class="search-box iq-search-bar d-search">
                                     <form action="<?php echo URL::to('/').'/searchResult';?>" method="post" class="searchbox">
                                         <input name="_token" type="hidden" value="<?php echo csrf_token(); ?>">
@@ -462,7 +462,7 @@ input:checked + .sliderk:before {
                                        <div class="iq-card-body">
                                    <div id="search_list" class="search_list search-toggle device-search" >
                                            </div> </div></div>
-                              </li> -->
+                              </li>
                               <li class="nav-item nav-icon">
                                  <!--<a href="#" class="search-toggle" data-toggle="search-toggle">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22"
