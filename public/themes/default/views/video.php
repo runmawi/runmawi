a633d577483f9147b359a786c8186507b183dc62
manikandan <manikandan@webnexs.in>
2022-06-14 10:09:10 +0530

    Artist link

--- public/themes/default/views/video.php
+++ public/themes/default/views/video.php
@@ -805,14 +805,18 @@ $artists = [];
 }
  if(count($artists) > 0 ) { ?>
  <h4>Cast & crew</h4>
-         <?php
-           foreach($artists as $key => $artist){
-          //  foreach($artist as $key => $value){
-         ?>
-           <div class="mt-2 d-flex">
-           <img src="<?= URL::to('/') . '/public/uploads/artists/'.$artist->image ?>" alt=""width="50" height="60">
-           <p class="trending-dec w-100 mb-0 text-white mt-2" ><?php echo $artist->artist_name ; ?> </p></div>
-    <?php } }  ?>
+         <?php foreach($artists as $key => $artist){  ?>
+          
+          <div class="row">
+            <div class="mt-6 d-flex">
+              <a  href="<?php echo __(URL::to('/') . '/Artist/' . $artist->artist_name); ?>"  >
+                <img src="<?= URL::to('/') . '/public/uploads/artists/'.$artist->image ?>" alt=""width="50" height="60">
+                <p class="trending-dec w-100 mb-0 text-white mt-2" ><?php echo $artist->artist_name ; ?> </p>
+              </a>
+            </div>
+          </div>
+       
+        <?php } }  ?>
            
        <!-- <div class="text-white">
            <p class="trending-dec w-100 mb-0 text-white"><?php echo __($video->description); ?></p>
