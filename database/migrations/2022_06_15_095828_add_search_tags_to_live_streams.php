7f5c1ce8601f010ac68368e41fe61f8ccc1daee7
M sanjai Kumar <illvarasi@webnexs.com>
2022-06-15 19:43:55 +0530

    search tag

---
+++ database/migrations/2022_06_15_095828_add_search_tags_to_live_streams.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSearchTagsToLiveStreams extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('live_streams', function (Blueprint $table) {
+            //
+            $table->string('search_tags')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('live_streams', function (Blueprint $table) {
+            //
+            $table->dropColumn('live_streams');
+        });
+    }
+}
--- resources/views/admin/livestream/edit.blade.php
+++ resources/views/admin/livestream/edit.blade.php
@@ -15,6 +15,37 @@ border-radius: 0px 4px 4px 0px;
         color: rgba(66, 149, 210, 1);
 
     }
+
+    
+.tags-input-wrapper{
+    background: transparent;
+    padding: 10px;
+    border-radius: 4px;
+    max-width: 400px;
+    border: 1px solid #ccc
+}
+.tags-input-wrapper input{
+    border: none;
+    background: transparent;
+    outline: none;
+    width: 140px;
+    margin-left: 8px;
+}
+.tags-input-wrapper .tag{
+    display: inline-block;
+    background-color: #20222c;
+    color: white;
+    border-radius: 40px;
+    padding: 0px 3px 0px 7px;
+    margin-right: 5px;
+    margin-bottom:5px;
+    box-shadow: 0 5px 15px -2px rgba(250 , 14 , 126 , .7)
+}
+.tags-input-wrapper .tag a {
+    margin: 0 7px 3px;
+    display: inline-block;
+    cursor: pointer;
+} */
 </style>
 @section('css')
 	<link rel="stylesheet" href="{{ URL::to('/assets/js/tagsinput/jquery.tagsinput.css') }}" />
@@ -249,7 +280,17 @@ border-radius: 0px 4px 4px 0px;
                 </div>
             @endif
 
+            <div class="row mt-3">
+                <div class="col-sm-12">
+                    <label class="m-0">Search Tags</label>
 
+                    <div class="panel-body">
+                    <!-- <input type="text" id="#inputTag" value="" data-role="tagsinput">  -->
+                    <input type="text" id="tag-input1" name="searchtags" >
+
+                </div>
+                </div>
+            </div>
             <div class="row mt-3">
                 <div class="col-sm-12">
                     <label class="m-0">Short Description</label>
@@ -426,7 +467,7 @@ border-radius: 0px 4px 4px 0px;
 			<input type="submit" value="{{ $button_text }}" class="btn btn-primary pull-right" />
 
 		</form>
-
+<input type="hidden" name="search_tag" id="search_tag" value="{{ $video->search_tags }}">
 		<div class="clear"></div>
 <!-- This is where now -->
 </div>
@@ -454,6 +495,8 @@ border-radius: 0px 4px 4px 0px;
 <script type="text/javascript">
    $ = jQuery;
    $(document).ready(function($){
+// alert($("#search_tag").val())
+
     $("#duration").mask("00:00:00");
    });
 
@@ -684,6 +727,205 @@ $(document).ready(function(){
 	}
 
 	</script>
+
+<script>
+	$(document).ready(function(){
+
+    var tagsdata = '<?= $video->search_tags ?>';
+    // alert(tagsdata);
+
+    });
+
+    var datasearch = $("#search_tag").val();
+// alert($("#search_tag").val())
+    (function(){
+
+"use strict"
+
+
+// Plugin Constructor
+var TagsInput = function(opts){
+    this.options = Object.assign(TagsInput.defaults , opts);
+    this.init();
+}
+
+// Initialize the plugin
+TagsInput.prototype.init = function(opts){
+    this.options = opts ? Object.assign(this.options, opts) : this.options;
+
+    if(this.initialized)
+        this.destroy();
+        
+    if(!(this.orignal_input = document.getElementById(this.options.selector)) ){
+        console.error("tags-input couldn't find an element with the specified ID");
+        return this;
+    }
+
+    this.arr = [];
+    this.wrapper = document.createElement('div');
+    this.input = document.createElement('input');
+    init(this);
+    initEvents(this);
+
+    this.initialized =  true;
+    return this;
+}
+
+// Add Tags
+TagsInput.prototype.addTag = function(string){
+
+    if(this.anyErrors(string))
+        return ;
+
+    this.arr.push(string);
+    var tagInput = this;
+
+    var tag = document.createElement('span');
+    tag.className = this.options.tagClass;
+    tag.innerText = string;
+
+    var closeIcon = document.createElement('a');
+    closeIcon.innerHTML = '&times;';
+    
+    // delete the tag when icon is clicked
+    closeIcon.addEventListener('click' , function(e){
+        e.preventDefault();
+        var tag = this.parentNode;
+
+        for(var i =0 ;i < tagInput.wrapper.childNodes.length ; i++){
+            if(tagInput.wrapper.childNodes[i] == tag)
+                tagInput.deleteTag(tag , i);
+        }
+    })
+
+
+    tag.appendChild(closeIcon);
+    this.wrapper.insertBefore(tag , this.input);
+    this.orignal_input.value = this.arr.join(',');
+
+    return this;
+}
+
+// Delete Tags
+TagsInput.prototype.deleteTag = function(tag , i){
+    tag.remove();
+    this.arr.splice( i , 1);
+    this.orignal_input.value =  this.arr.join(',');
+    return this;
+}
+
+// Make sure input string have no error with the plugin
+TagsInput.prototype.anyErrors = function(string){
+    if( this.options.max != null && this.arr.length >= this.options.max ){
+        console.log('max tags limit reached');
+        return true;
+    }
+    
+    if(!this.options.duplicate && this.arr.indexOf(string) != -1 ){
+        console.log('duplicate found " '+string+' " ')
+        return true;
+    }
+
+    return false;
+}
+
+// Add tags programmatically 
+TagsInput.prototype.addData = function(array){
+    var plugin = this;
+    
+    array.forEach(function(string){
+        plugin.addTag(string);
+    })
+    return this;
+}
+
+// Get the Input String
+TagsInput.prototype.getInputString = function(){
+    return this.arr.join(',');
+}
+
+
+// destroy the plugin
+TagsInput.prototype.destroy = function(){
+    this.orignal_input.removeAttribute('hidden');
+
+    delete this.orignal_input;
+    var self = this;
+    
+    Object.keys(this).forEach(function(key){
+        if(self[key] instanceof HTMLElement)
+            self[key].remove();
+        
+        if(key != 'options')
+            delete self[key];
+    });
+
+    this.initialized = false;
+}
+
+// Private function to initialize the tag input plugin
+function init(tags){
+    tags.wrapper.append(tags.input);
+    tags.wrapper.classList.add(tags.options.wrapperClass);
+    tags.orignal_input.setAttribute('hidden' , 'true');
+    tags.orignal_input.parentNode.insertBefore(tags.wrapper , tags.orignal_input);
+}
+
+// initialize the Events
+function initEvents(tags){
+    tags.wrapper.addEventListener('click' ,function(){
+        tags.input.focus();           
+    });
+    
+
+    tags.input.addEventListener('keydown' , function(e){
+        var str = tags.input.value.trim(); 
+
+        if( !!(~[9 , 13 , 188].indexOf( e.keyCode ))  )
+        {
+            e.preventDefault();
+            tags.input.value = "";
+            if(str != "")
+                tags.addTag(str);
+        }
+
+    });
+}
+
+
+// Set All the Default Values
+TagsInput.defaults = {
+    selector : '',
+    wrapperClass : 'tags-input-wrapper',
+    tagClass : 'tag',
+    max : null,
+    duplicate: false
+}
+
+window.TagsInput = TagsInput;
+
+})();
+
+var datasearch = $("#search_tags").val();
+var tagsdata = '<?= $video->search_tags ?>';
+
+// alert(tagsdata)
+var tagInput1 = new TagsInput({
+        selector: 'tag-input1',
+        duplicate : false,
+        max : 10
+    });
+// tagInput1.addData([tagsdata])
+
+    if(tagsdata == ""){
+// alert(tagsdata);
+tagInput1.addData([])
+        }else{
+    tagInput1.addData(['<?= $video->search_tags ?>'])
+            // tagInput1.addData([])
+        }
+
+</script>
 <script>
     $(document).ready(function(){
         // $('#message').fadeOut(120);
