6e9cb010909382aef7ff30849c1b11e692a08d4f
Manikandan <manikandan@webnexs.in>
2022-06-21 21:48:04 +0530

    trailer embede in default

--- public/themes/default/views/video.php
+++ public/themes/default/views/video.php
@@ -586,13 +586,35 @@ Auth::user()->role == 'admin' && $video->type != "" || Auth::user()->role =="sub
 </div>
 <?php  }?>
 
- <div class=" page-height">
-    <div id="watch_trailer" class="fitvid" atyle="z-index: 9999;">
-                <video  id="videoPlayers" class=""  
-                controls data-setup='{"controls": true, "aspectRatio":"16:9", "fluid": true}'  
-                type="video/mp4" src="<?php echo $video->trailer;?>">
-                </video>
-    </div>
+
+
+          <div class=" page-height">
+              <div id="watch_trailer" class="fitvid" atyle="z-index: 9999;">
+               
+                <?php  if($video->trailer_type !=null && $video->trailer_type == "video_mp4" || $video->trailer_type == "mp4_url"  ){ ?>
+
+                    <video  class="videoPlayers" 
+                          controls data-setup='{"controls": true, "aspectRatio":"16:9", "fluid": true}'  
+                          type="video/mp4" src="<?php echo $video->trailer;?>">
+                    </video>
+
+                <?php }elseif($video->trailer_type !=null && $video->trailer_type == "m3u8_url" ){ ?>
+
+                    <video  class="videoPlayers" 
+                          controls data-setup='{"controls": true, "aspectRatio":"16:9", "fluid": true}'  
+                          type="video/mp4" src="<?php echo $video->trailer;?>">
+                    </video>
+
+                <?php }elseif($video->trailer_type !=null && $video->trailer_type == "embed_url" ){ ?>
+
+                        <div class="videoPlayers" id="">
+                          <iframe src="<?php echo $video->trailer ?>" allowfullscreen allowtransparency
+                            allow="autoplay">
+                          </iframe>
+                        </div>
+
+                <?php  } ?>
+              </div>
 
 
 
@@ -1070,7 +1092,7 @@ $artists = [];
           $('#videoplay').click(function(){
             // alert('test');
               $('#video_container').hide();
-              const player = new Plyr('#videoPlayers');
+              const player = new Plyr('.videoPlayers');
               $('#watch_trailer').show();
               $('#videoplay').hide();
               $('#close_trailer').show();
@@ -1082,7 +1104,7 @@ $artists = [];
           $('#close_trailer').click(function(){
             // alert('test');
             $('#watch_trailer').hide();
-            const player = new Plyr('#videoPlayers');
+            const player = new Plyr('.videoPlayers');
             $('#video_container').show();
             $('#videoplay').show();            
             $('#close_trailer').hide();
