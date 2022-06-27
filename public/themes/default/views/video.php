0b64ef2d10db580f359a6ec48c259e21a64ba170
M sanjai Kumar <illvarasi@webnexs.com>
2022-06-27 20:20:34 +0530

    publish later on process

--- public/themes/default/views/video.php
+++ public/themes/default/views/video.php
@@ -1731,16 +1731,17 @@ $(document).ready(function(){
 // Set the date we're counting down to
 var date = "<?= $new_date ?>";
 var countDownDate = new Date(date).getTime();
-// alert(date)
+// alert(countDownDate)
 // Update the count down every 1 second
 var x = setInterval(function() {
 
   // Get today's date and time
   var now = new Date().getTime();
+// alert(now)
     
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+  // alert(distance)
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -1754,7 +1755,7 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    // document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
 </script>
