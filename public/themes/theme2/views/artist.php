d9be6a7eef6ec49131104fae1d078f16b1fa8f77
Manikandan <manikandan@webnexs.in>
2022-07-15 13:41:36 +0530

    changes

--- public/themes/theme2/views/artist.php
+++ public/themes/theme2/views/artist.php
@@ -1,4 +1,4 @@
-<?php include('header.php'); ?>
+<?php  include(public_path('themes/theme1/views/header.php')); ?>
 
 <div class="aud" style="background-image:url(<?php echo URL::to('/').'/public/uploads/artists/'.$artist->image;?>)">
         <h2 class="font-weight-bold"><?php echo $artist->artist_name;?></h2>
@@ -29,143 +29,175 @@
             </div>
         </div>
     </div>
-    <div class="m-4 mt-3">
-        <h2>Lastest Release</h2>
-    </div>
-    <div class="container">
-        <div class="row mb-5">
-        	<?php foreach ($latest_audios as $key => $latest_audio) { 
-        		?>
-        		
-            <div class="col-sm-4">
-                <a href="<?php echo URL::to('/').'/audio/'.$latest_audio[0]['slug'];?>">
-                <div class="bg">
-                    <img src="<?php echo URL::to('/').'/public/uploads/images/'.$latest_audio[0]['image'];?>" alt="" width="300">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <p class="dc"><?php echo $latest_audio[0]['title'];?></p>
-                            <p><?php echo $latest_audio[0]['year'];?></p>
-                        </div>
-                        <div>
-                            <i class="fa fa-eye" aria-hidden="true"></i>
 
-                        </div>
-                    </div>
-                </div>
-                </a>
+       <!-- Latest Videos -->
+
+       <?php if(count($latest_audios) > 0) { ?>
+
+            <div class="m-4 mt-3">
+                <h2>Lastest Release</h2>
             </div>
+
+            <div class="container">
+                <div class="row mb-5">
+                    <?php foreach ($latest_audios as $key => $latest_audio) { 
+                        ?>
+                        
+                    <div class="col-sm-4">
+                        <a href="<?php echo URL::to('/').'/audio/'.$latest_audio[0]['slug'];?>">
+                        <div class="bg">
+                            <img src="<?php echo URL::to('/').'/public/uploads/images/'.$latest_audio[0]['image'];?>" alt="" width="300">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <p class="dc"><?php echo $latest_audio[0]['title'];?></p>
+                                    <p><?php echo $latest_audio[0]['year'];?></p>
+                                </div>
+                                <div>
+                                    <i class="fa fa-eye" aria-hidden="true"></i>
+
+                                </div>
+                            </div>
+                        </div>
+                        </a>
+                    </div>
+                <?php } ?>
+                </div>
+            </div>
+
         <?php } ?>
-        </div>
-    </div>
-    <div class="m-4 mt-3">
-        <h2>Album</h2>
-    </div>
-    <div class="container">
-        <div class="row mb-5">
-        	<?php foreach ($albums as $key => $album) { ?>
-        		
-            <div class="col-sm-4">
-                <a href="<?php echo URL::to('/').'/album/'.$album->slug;?>">
-                <div class="bg">
-                    <img src="<?php echo URL::to('/').'/public/uploads/albums/'.$album->album;?>" alt="" width="300">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <p class="dc"><?php echo $album->albumname;?></p>
-                        </div>
-                        <div>
-                            <i class="fa fa-eye" aria-hidden="true"></i>
 
-                        </div>
-                    </div>
-                </div>
-            </a>
+        <?php if(count($albums) > 0) { ?>
+
+            <div class="m-4 mt-3">
+                <h2>Album</h2>
             </div>
-            <?php } ?>
-        </div>
-    </div>
+
+            <div class="container">
+                <div class="row mb-5">
+                    <?php foreach ($albums as $key => $album) { ?>
+                        
+                    <div class="col-sm-4">
+                        <a href="<?php echo URL::to('/').'/album/'.$album->slug;?>">
+                        <div class="bg">
+                            <img src="<?php echo URL::to('/').'/public/uploads/albums/'.$album->album;?>" alt="" width="300">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <p class="dc"><?php echo $album->albumname;?></p>
+                                </div>
+                                <div>
+                                    <i class="fa fa-eye" aria-hidden="true"></i>
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+                    <?php } ?>
+                </div>
+            </div>
+
+        <?php } ?>
   
-    <div class="m-4 mt-3">
-        <h2>Audio</h2>
-    </div>
-    <div class="container">
-        <div class="row mb-5">
-        	<?php  foreach ($artist_audios as $key => $artist_audio) { 
-        		?>
-        		
-            <div class="col-sm-4">
-                <a href="<?php echo URL::to('/').'/audio/'.$artist_audio->slug;?>">
-                <div class="bg">
-                    <img src="<?php echo URL::to('/').'/public/uploads/images/'.$artist_audio->image;?>" alt="" width="300">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <p class="dc"><?php echo $artist_audio->title;?></p>
-                            <p><?php echo $artist_audio->year;?></p>
-                        </div>
-                        <div>
-                            <i class="fa fa-eye" aria-hidden="true"></i>
-
-                        </div>
-                    </div>
-                </div>
-            </a>
+        <?php if(count($artist_audios) > 0) { ?>
+            <div class="m-4 mt-3">
+                <h2>Audio</h2>
             </div>
-        <?php } ?>
-        </div>
-    </div>
-    <div class="m-4 mt-3">
-        <h2>Series</h2>
-    </div>
-    <div class="container">
-        <div class="row mb-5">
-        	<?php  foreach ($artist_series as $key => $artist_serie) { 
-        		?>
-        		
-            <div class="col-sm-4">
-                <a href="<?php echo URL::to('/').'/play_series/'.$artist_serie->id;?>">
-                <div class="bg">
-                    <img src="<?php echo URL::to('/').'/public/uploads/images/'.$artist_serie->image;?>" alt="" width="300">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <p class="dc"><?php echo $artist_serie->title;?></p>
-                            <p><?php echo $artist_serie->year;?></p>
-                        </div>
-                        <div>
-                            <i class="fa fa-eye" aria-hidden="true"></i>
 
-                        </div>
-                    </div>
-                </div>
-            </a>
-            </div>
-        <?php } ?>
-        </div>
-    </div>
-    <div class="m-4 mt-3">
-        <h2>Videos</h2>
-    </div>
-    <div class="container">
-        <div class="row mb-5">
-            <?php  foreach ($artist_videos as $key => $artist_video) { 
-                ?>
-                
-            <div class="col-sm-4">
-                <a href="<?php echo URL::to('/').'/category/videos/'.$artist_video->slug;?>">
-                <div class="bg">
-                    <img src="<?php echo URL::to('/').'/public/uploads/images/'.$artist_video->image;?>" alt="" width="300">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <p class="dc"><?php echo $artist_video->title;?></p>
-                            <p><?php echo $artist_video->year;?></p>
-                        </div>
-                        <div>
-                            <i class="fa fa-eye" aria-hidden="true"></i>
+            <div class="container">
+                <div class="row mb-5">
+                    <?php  foreach ($artist_audios as $key => $artist_audio) { 
+                        ?>
+                        
+                    <div class="col-sm-4">
+                        <a href="<?php echo URL::to('/').'/audio/'.$artist_audio->slug;?>">
+                        <div class="bg">
+                            <img src="<?php echo URL::to('/').'/public/uploads/images/'.$artist_audio->image;?>" alt="" width="300">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <p class="dc"><?php echo $artist_audio->title;?></p>
+                                    <p><?php echo $artist_audio->year;?></p>
+                                </div>
+                                <div>
+                                    <i class="fa fa-eye" aria-hidden="true"></i>
 
+                                </div>
+                            </div>
                         </div>
+                    </a>
                     </div>
+                <?php } ?>
                 </div>
-            </a>
             </div>
+
+        <?php } ?>c
+        
+        <?php if(count($artist_series) > 0) { ?>
+
+            <div class="m-4 mt-3">
+                <h2>Series</h2>
+            </div>
+
+            <div class="container">
+                <div class="row mb-5">
+                    <?php  foreach ($artist_series as $key => $artist_serie) { 
+                        ?>
+                        
+                    <div class="col-sm-4">
+                        <a href="<?php echo URL::to('/').'/play_series/'.$artist_serie->id;?>">
+                        <div class="bg">
+                            <img src="<?php echo URL::to('/').'/public/uploads/images/'.$artist_serie->image;?>" alt="" width="300">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <p class="dc"><?php echo $artist_serie->title;?></p>
+                                    <p><?php echo $artist_serie->year;?></p>
+                                </div>
+                                <div>
+                                    <i class="fa fa-eye" aria-hidden="true"></i>
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+                <?php } ?>
+                </div>
+            </div>
+
         <?php } ?>
-        </div>
-    </div>
-<?php include('footer.blade.php'); ?>
+
+        <?php if(count($artist_videos) > 0) { ?>
+
+            <div class="m-4 mt-3">
+                <h2>Videos</h2>
+            </div>
+
+            <div class="container">
+                <div class="row mb-5">
+                    <?php  foreach ($artist_videos as $key => $artist_video) { 
+                        ?>
+                        
+                    <div class="col-sm-4">
+                        <a href="<?php echo URL::to('/').'/category/videos/'.$artist_video->slug;?>">
+                        <div class="bg">
+                            <img src="<?php echo URL::to('/').'/public/uploads/images/'.$artist_video->image;?>" alt="" width="300">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <p class="dc"><?php echo $artist_video->title;?></p>
+                                    <p><?php echo $artist_video->year;?></p>
+                                </div>
+                                <div>
+                                    <i class="fa fa-eye" aria-hidden="true"></i>
+
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+                <?php } ?>
+                </div>
+            </div>
+
+        <?php } ?>
+        
+
+<?php  include(public_path('themes/theme2/views/footer.blade.php'));  ?>
