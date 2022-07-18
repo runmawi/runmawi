cfa01206c14c2dd2a711f465e03f224bbdaf8b08
Manikandan <manikandan@webnexs.in>
2022-07-18 15:44:27 +0530

    in app purchase admin

---
+++ resources/views/admin/Inapp_purchase/edit.blade.php
@@ -0,0 +1,45 @@
+@extends('admin.master')
+
+@section('content')
+
+    <div id="content-page" class="content-page">
+        <div class="container-fluid">
+            <div class="iq-card">
+                <h4><i class="entypo-archive"></i>  Manage In App Purchase Plans </h4>
+                <div class="modal-body">
+	                <form id="update-cat-form" accept-charset="UTF-8" action="{{ route('inapp_purchase_update') }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group ">
+                            <label> Plan Price :</label>
+                            <input type="text" id="plan_price" name="plan_price" value="{{ $Inapp_Purchase->plan_price }}" class="form-control" placeholder="Enter Plan Price">
+                        </div>
+
+                        <div class="form-group">
+                            <label> Product ID :</label>
+                            <input type="text" id="product_id" name="product_id" value="{{ $Inapp_Purchase->product_id }}" class="form-control" placeholder="Enter Product ID">
+                        </div>
+                        <input type="hidden" name="id" id="id" value="{{ $Inapp_Purchase->id }}" />
+                </form>
+            </div>
+
+            <div class="modal-footer">
+                <a type="button" class="btn btn-danger" data-dismiss="modal" href="{{ route('inapp_purchase') }}">Close</a>
+                <button type="button" class="btn btn-primary" id="submit-update-cat">Update</button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+@section('javascript')
+
+<script>
+	$(document).ready(function(){
+		$('#submit-update-cat').click(function(){
+			$('#update-cat-form').submit();
+		});
+	});
+</script>
+
+@stop
+@stop
