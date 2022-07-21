ca09de774585a7974719557273b19cc706079635
Manikandan <manikandan@webnexs.in>
2022-07-21 17:57:38 +0530

    live stream trailer disable - slider

--- public/themes/default/views/partials/home/slider.php
+++ public/themes/default/views/partials/home/slider.php
@@ -84,26 +84,24 @@ endif; ?>
                         </div>
                         </div>
                         </div>
-                        <div class="trailor-video">
-                            <a href="<?= URL::to('/') ?><?= '/live'.'/'. $slider_video->slug ?>"
-                                class="video-open playbtn">
-                                <svg class="gt" version="1.1" xmlns="http://www.w3.org/2000/svg"
-                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="80px" height="80px"
-                                viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7" xml:space="preserve">
-                                    <style type="text/css">
-                                        .gt{
-                                            height: 60px!important;
-                                        }
-                            </style>
-                                    <polygon class='triangle' fill="none" stroke-width="7" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-miterlimit="10"
-                                    points="73.5,62.5 148.5,105.8 73.5,149.1 " />
-                                    <circle class='circle' fill="none" stroke-width="7" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8" r="103.3" />
-                                </svg>
-                                <span class="w-trailor">Watch Trailer</span>
-                            </a>
-                            </div>
+                            <!-- <div class="trailor-video">
+                                <a href="<?= URL::to('/') ?><?= '/live'.'/'. $slider_video->slug ?>" class="video-open playbtn">
+                                    <svg class="gt" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="80px" height="80px"
+                                            viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7" xml:space="preserve">
+                                            <style type="text/css">
+                                                .gt{  height: 60px!important;  }
+                                            </style>
+                                        <polygon class='triangle' fill="none" stroke-width="7" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-miterlimit="10"
+                                        points="73.5,62.5 148.5,105.8 73.5,149.1 " />
+                                        <circle class='circle' fill="none" stroke-width="7" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8" r="103.3" />
+                                    </svg>
+                                    <span class="w-trailor">Watch Trailer</span>
+                                </a>
+                            </div> -->
+
                             </div>
                             </div>
                             </div>
