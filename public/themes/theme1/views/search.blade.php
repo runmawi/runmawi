0b3385164a410f1d4ab5efc5fecdd6643e3a7811
Manikandan <manikandan@webnexs.in>
2022-07-21 14:27:45 +0530

    Search result

--- public/themes/theme1/views/search.blade.php
+++ public/themes/theme1/views/search.blade.php
@@ -7,11 +7,13 @@
     <div class="container-fluid" style="padding: 0px 40px!important;background: linear-gradient(135.05deg, rgba(136, 136, 136, 0.48) 1.85%, rgba(64, 32, 32, 0.13) 38.53%, rgba(81, 57, 57, 0.12) 97.89%);">
  
 
-            {{-- All Video - Videos,Episode,livestream,audio --}}
+            {{-- Videos --}}
         <div class="row">
             <div class="col-sm-12 page-height">
 
-                <div class="iq-main-header align-items-center justify-content-between"> </div>
+                <div class="iq-main-header align-items-center justify-content-between"> 
+                    <h3 class="vid-title"> Showing  videos for "{{  $search_value }}"</h3>              
+                </div>
                     <div class="favorites-contens">
                         <ul class="category-page list-inline row p-0 mb-0">
                             @if(isset($all_videos)) 
@@ -102,73 +104,21 @@
                                     </li>
                                 @endforeach
                             @endif
-
-                                 {{-- Live stream --}}
-
-                            @if(isset($Search_livestreams)) 
-                                @foreach($Search_livestreams as $livestream_search)
-
-                                    <li class="slide-item col-sm-2 col-md-2 col-xs-12">
-                                        <a href="<?php echo URL::to('home') ?>">
-                                            <div class="block-images position-relative">
-                                                <div class="img-box">
-                                                    <img loading="lazy" data-src="<?php echo URL::to('/').'/public/uploads/images/'.$livestream_search->image;  ?>" class="img-fluid" alt="">
-                                                </div>
-
-                                                <div class="block-description">
-                                                    <div class="hover-buttons">
-                                                        <a  href="{{  URL::to('live') .'/' .$livestream_search->slug }} ">	
-                                                            <img class="ply" src="<?php echo URL::to('/').'/assets/img/play.svg';  ?>">                                  
-                                                        </a>
-                                                    <div>
-                                                </div>
-                                            </div> </div> </div>
-            
-                                            <div class="">
-                                                <div class="mt-2 d-flex justify-content-between p-0">
-                                                    @if($ThumbnailSetting->title == 1) 
-                                                        <h6><?php  echo (strlen($livestream_search->title) > 17) ? substr($livestream_search->title,0,18).'...' : $livestream_search->title; ?></h6>
-                                                    @endif
-                                                </div>
-
-                                                <div class="movie-time my-2"> 
-                                                    @if($ThumbnailSetting->duration == 1) <!-- Duration -->
-                                                        <span class="text-white">
-                                                                <i class="fa fa-clock-o"></i>
-                                                                {{ gmdate('H:i:s', $livestream_search->duration)}}
-                                                        </span>
-                                                    @endif
                             
-                                                    @if($ThumbnailSetting->rating == 1 && $livestream_search->rating != null)  <!-- Rating -->
-                                                        <span class="text-white">
-                                                            <i class="fa fa-star-half-o" aria-hidden="true"></i>
-                                                            {{ $livestream_search->rating }}
-                                                        </span>
-                                                    @endif
-                            
-                                                    @if($ThumbnailSetting->featured == 1 && $livestream_search->featured == 1)  <!-- Featured -->
-                                                        <span class="text-white">
-                                                            <i class="fa fa-flag" aria-hidden="true"></i>
-                                                        </span>
-                                                    @endif
-                                                </div>
-                                            
-                                                <div class="movie-time my-2">                   
-                                                    @if ( ($ThumbnailSetting->published_year == 1) && ( $livestream_search->year != null ) )  <!-- published_year -->
-                                                        <span class="text-white">
-                                                            <i class="fa fa-calendar" aria-hidden="true"></i>
-                                                            {{ $livestream_search->year }}
-                                                        </span>
-                                                    @endif
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>
-                                @endforeach
-                            @endif
+                        </ul>
+                    </div>
+                </div>
+        </div>
 
-                                 {{-- Episode --}}
+            {{-- Episode  --}}
+        <div class="row">
+            <div class="col-sm-12 page-height">
 
+                <div class="iq-main-header align-items-center justify-content-between"> 
+                    <h3 class="vid-title"> Showing  Episode for "{{  $search_value }}"</h3>              
+                </div>
+                    <div class="favorites-contens">
+                        <ul class="category-page list-inline row p-0 mb-0">
                             @if(isset($Search_Episode)) 
                                 @foreach($Search_Episode as $episode_search)
                                     @php
@@ -239,9 +189,20 @@
                                     </li>
                                 @endforeach
                             @endif
+                        </ul>
+                    </div>
+                </div>
+        </div>
 
-                                {{-- Series --}}
+             {{-- Series  --}}
+        <div class="row">
+            <div class="col-sm-12 page-height">
 
+                <div class="iq-main-header align-items-center justify-content-between"> 
+                    <h3 class="vid-title"> Showing  Series for "{{  $search_value }}"</h3>              
+                </div>
+                    <div class="favorites-contens">
+                        <ul class="category-page list-inline row p-0 mb-0">
                                 @if(isset($Search_Series)) 
                                     @foreach($Search_Series as $Series_search)
 
@@ -303,9 +264,98 @@
                                         </li>
                                     @endforeach
                                 @endif
+                        </ul>
+                    </div>
+                </div>
+        </div>
 
-                                {{-- Audio --}}
+            {{-- Live Stream --}}
+        <div class="row">
+            <div class="col-sm-12 page-height">
 
+                <div class="iq-main-header align-items-center justify-content-between"> 
+                    <h3 class="vid-title"> Showing  Live Stream for "{{  $search_value }}"</h3>              
+                </div>
+                    <div class="favorites-contens">
+                        <ul class="category-page list-inline row p-0 mb-0">
+                            
+                            @if(isset($Search_livestreams)) 
+                                @foreach($Search_livestreams as $livestream_search)
+
+                                    <li class="slide-item col-sm-2 col-md-2 col-xs-12">
+                                        <a href="<?php echo URL::to('home') ?>">
+                                            <div class="block-images position-relative">
+                                                <div class="img-box">
+                                                    <img loading="lazy" data-src="<?php echo URL::to('/').'/public/uploads/images/'.$livestream_search->image;  ?>" class="img-fluid" alt="">
+                                                </div>
+
+                                                <div class="block-description">
+                                                    <div class="hover-buttons">
+                                                        <a  href="{{  URL::to('live') .'/' .$livestream_search->slug }} ">	
+                                                            <img class="ply" src="<?php echo URL::to('/').'/assets/img/play.svg';  ?>">                                  
+                                                        </a>
+                                                    <div>
+                                                </div>
+                                            </div> </div> </div>
+            
+                                            <div class="">
+                                                <div class="mt-2 d-flex justify-content-between p-0">
+                                                    @if($ThumbnailSetting->title == 1) 
+                                                        <h6><?php  echo (strlen($livestream_search->title) > 17) ? substr($livestream_search->title,0,18).'...' : $livestream_search->title; ?></h6>
+                                                    @endif
+                                                </div>
+
+                                                <div class="movie-time my-2"> 
+                                                    @if($ThumbnailSetting->duration == 1) <!-- Duration -->
+                                                        <span class="text-white">
+                                                                <i class="fa fa-clock-o"></i>
+                                                                {{ gmdate('H:i:s', $livestream_search->duration)}}
+                                                        </span>
+                                                    @endif
+                            
+                                                    @if($ThumbnailSetting->rating == 1 && $livestream_search->rating != null)  <!-- Rating -->
+                                                        <span class="text-white">
+                                                            <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                                                            {{ $livestream_search->rating }}
+                                                        </span>
+                                                    @endif
+                            
+                                                    @if($ThumbnailSetting->featured == 1 && $livestream_search->featured == 1)  <!-- Featured -->
+                                                        <span class="text-white">
+                                                            <i class="fa fa-flag" aria-hidden="true"></i>
+                                                        </span>
+                                                    @endif
+                                                </div>
+                                            
+                                                <div class="movie-time my-2">                   
+                                                    @if ( ($ThumbnailSetting->published_year == 1) && ( $livestream_search->year != null ) )  <!-- published_year -->
+                                                        <span class="text-white">
+                                                            <i class="fa fa-calendar" aria-hidden="true"></i>
+                                                            {{ $livestream_search->year }}
+                                                        </span>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </li>
+                                @endforeach
+                            @endif
+                            
+                        </ul>
+                    </div>
+                </div>
+        </div>
+
+            {{-- Audios --}}
+        <div class="row">
+            <div class="col-sm-12 page-height">
+
+                <div class="iq-main-header align-items-center justify-content-between"> 
+                    <h3 class="vid-title"> Showing  Audios for "{{  $search_value }}"</h3>              
+                </div>
+                    <div class="favorites-contens">
+                        <ul class="category-page list-inline row p-0 mb-0">
+                            
                             @if(isset($Search_audio)) 
                                 @foreach($Search_audio as $latest_video)
 
@@ -372,6 +422,7 @@
                     </div>
                 </div>
         </div>
+
     </div>
 </section>
 @php
