d469824c7b97028c84044d482461dba0c3d971bd
M sanjai Kumar <illvarasi@webnexs.com>
2022-07-22 18:00:30 +0530

    episode page watching seasona and episode number shown . logs for video add , upload ,meta data,category , artist, language for video done

--- app/LogActivity.php
+++ app/LogActivity.php
@@ -10,10 +10,31 @@ class LogActivity extends Model
     protected $table = 'log_activity';
 
     protected $fillable = [
-        'subject', 'url', 'method', 'ip', 'agent', 'user_id'
+        'subject', 'url', 'method', 'ip', 'agent', 'user_id', 'video_id', 'audio_id', 'live_id', 'series_id'
+        , 'season_id', 'episode_id', 'video_category_id', 'audio_category_id', 'live_category_id', 'series_category_id'
+        , 'episode_category_id', 'approved_video_id', 'approved_live_id', 'album_id' , 'video_language_id'
+        , 'video_artist_id', 'live_language_id', 'live_artist_id' , 'audio_language_id', 'audio_artist_id'
+        , 'series_language_id', 'series_artist_id'
+
     ];
 
     public function username(){
         return $this->belongsTo('App\User','user_id','id');
+    }
+
+    public function video_title(){
+        return $this->belongsTo('App\Video','video_id','id');
+    }
+
+    public function video_category(){
+        return $this->belongsTo('App\VideoCategory','video_category_id','id');
+    }
+
+    public function video_language(){
+        return $this->belongsTo('App\LanguageVideo','video_language_id','id');
+    }
+
+    public function video_cast(){
+        return $this->belongsTo('App\Videoartist','video_artist_id','id');
     }
 }
--- resources/views/admin/logActivity.blade.php
+++ resources/views/admin/logActivity.blade.php
@@ -1,40 +1,68 @@
 @extends('admin.master')
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+
 @section('content')
 <div id="content-page" class="content-page">
     <div class="container-fluid">
     	<h1>Log Activity Lists</h1>
 			<div class="container">
 				<!-- <h1>Log Activity Lists</h1> -->
-				<table class="table table-bordered">
-					<tr>
-						<th>No</th>
-						<th>Subject</th>
-						<th>URL</th>
-						<!-- <th>Method</th> -->
-						<th>Ip</th>
-						<th width="300px">User Agent</th>
-						<!-- <th>User Id</th> -->
-						<th width="300px">User Name</th>
-						<!-- <th>Action</th> -->
-					</tr>
-					@if($logs->count())
-						@foreach($logs as $key => $log)
-						<tr>
-							<td>{{ ++$key }}</td>
-							<td>{{ $log->subject }}</td>
-							<td class="text-success">{{ $log->url }}</td>
-							<!-- <td><label class="label label-info">{{ $log->method }}</label></td> -->
-							<td class="text-warning">{{ $log->ip }}</td>
-							<td class="text-danger">{{ $log->agent }}</td>
-							<!-- <td>{{ $log->user_id }}</td> -->
-							<td>{{ $log->username->username }}</td>
-							<!-- <td><button class="btn btn-danger btn-sm">Delete</button></td> -->
-						</tr>
-						@endforeach
-					@endif
-				</table>
+				<div class="iq-card-body table-responsive p-0">
+                        <div class="table-view">
+						<table class="table text-center table-striped table-bordered table movie_table iq-card " style="width:100%" id="logtable">
+							<thead>
+								<tr>
+									<th>No</th>
+									<th>Subject</th>
+									<!-- <th>URL</th> -->
+									<!-- <th>Method</th> -->
+									<!-- <th>Ip</th> -->
+									<th width="300px">User Agent</th>
+									<!-- <th>User Id</th> -->
+									<th width="300px">User Name</th>
+									<th width="300px">Video Title</th>
+									<th width="300px">Video Category</th>
+									<th width="300px">Video Language</th>
+									<th width="300px">Video Cast Crew</th>
+									<th width="300px">Created</th>
+									<!-- <th width="300px">Updated</th> -->
+
+									<!-- <th>Action</th> -->
+								</tr>
+							</thead>
+							<tbody>
+							@if($logs->count())
+								@foreach($logs as $key => $log)
+								<tr>
+									<td>{{ ++$key }}</td>
+									<td>{{ $log->subject }}</td>
+									<!-- <td class="text-success">{{ $log->url }}</td> -->
+									<!-- <td><label class="label label-info">{{ $log->method }}</label></td> -->
+									<!-- <td class="text-warning">{{ $log->ip }}</td> -->
+									<td class="text-danger">{{ $log->agent }}</td>
+									<!-- <td>{{ $log->user_id }}</td> -->
+									<td>{{ $log->username->username }}</td>
+									<td>@if(!empty($log->video_title->title)){{ @$log->video_title->title }} @else No Title @endif</td>
+									<td>@if(!empty($log->video_category->name)){{ @$log->video_category->name }} @else No Category @endif</td>
+									<td>@if(!empty($log->video_language->name)){{ @$log->video_language->name }} @else No Language @endif</td>
+									<td>@if(!empty($log->video_cast->artist_name)){{ @$log->video_cast->artist_name }} @else No Cast @endif</td>
+									<td >{{ $log->created_at }}</td>
+									<!-- <td >{{ $log->updated_at }}</td> -->
+
+									<!-- <td><button class="btn btn-danger btn-sm">Delete</button></td> -->
+								</tr>
+								@endforeach
+							@endif
+						</tbody>
+						</table>
+					</div>
+				</div>
+
 			</div>
 	</div>
 </div>
@@ -42,3 +70,13 @@
 
 
 @stop
+<script>
+    $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+    });
+     $(document).ready(function(){
+        $('#logtable').DataTable();
+    });
+</script>
