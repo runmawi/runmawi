bad511e431bd55e422653e643ac5674587a4289d
Manikandan <manikandan@webnexs.in>
2022-09-07 19:16:57 +0530

    changes

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -331,6 +331,7 @@ class ApiAuthController extends Controller
                               $response = array(
                                 'status' => 'false',
                                 'message' => 'Subscription Not done',
+                                'error_message' =>   $th,
                                 'user_id' =>  $userid
                             );
 
