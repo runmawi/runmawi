191daa2b23e2039f12ce72c8d1373e9332f9b09f
M sanjai Kumar <illvarasi@webnexs.com>
2022-09-23 10:18:14 +0530

    upload file

--- resources/views/admin/test_server_videos/index.blade.php
+++ resources/views/admin/test_server_videos/index.blade.php
@@ -21,21 +21,41 @@
                 </div>
             </div>
 
+            @if (Session::has('message'))
+               <div id="successMessage" class="alert alert-info">{{ Session::get('message') }}</div>
+            @endif
+            @if(count($errors) > 0)
+            @foreach( $errors->all() as $message )
+               <div class="alert alert-danger display-hide" id="successMessage" >
+                  <button id="successMessage" class="close" data-close="alert"></button>
+                  <span>{{ $message }}</span>
+               </div>
+            @endforeach
+            @endif
+
             <div class="row">
                     <div class="col-md-12">
                     <!-- Video upload -->   
-                        <div id="video_upload" style="">
+                        <!-- <div id="video_upload" style="">
                             <div class='content file'>
-                                <h3 class="card-title upload-ui font-weight-bold">Upload Full Video Here</h4>
+                                <h3 class="card-title upload-ui font-weight-bold">Upload Full Video Here</h4> -->
                                 <!-- Dropzone -->
-                                <form action="{{URL::to('admin/test/server/videoupload/')}}" method= "post" class='dropzone' ></form>
+                                <!-- <form action="{{URL::to('admin/test/server/videoupload/')}}" method= "post" class='dropzone' ></form>
                                 <div class="row justify-content-center">
                                     <div class="col-md-9 text-center">
                                     <p class="c1" style="margin-left: 25%;">Trailers Can Be Uploaded From Video Edit Screen</p>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
+                        <form action="{{URL::to('admin/test/server/videoupload/')}}" method= "post" accept-charset="UTF-8" enctype="multipart/form-data" >
+
+                            <input type="file" accept="video/mp4,video/x-m4v,video/*" name="file" id="file">
+
+                            <input type="submit" value="Update Settings" class="btn btn-primary " />
+                            <input type="hidden" name="_token" value="<?= csrf_token() ?>" />
+                        </form>
+
                     </div>
                 </div>
             
@@ -48,9 +68,8 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="<?= URL::to('/assets/js/jquery.mask.min.js');?>"></script>
         
-        <script type="text/javascript">
+        <!-- <script type="text/javascript">
 
             var CSRF_TOKEN = document.querySelector('meta[name="csrf-token"]').getAttribute("content");
 
@@ -66,7 +85,16 @@
                     // console.log(value.video_title);
                 });
             }); 
-        </script>
+        </script> -->
+
+        <script>
+   $(document).ready(function(){
+       // $('#message').fadeOut(120);
+       setTimeout(function() {
+           $('#successMessage').fadeOut('fast');
+       }, 3000);
+   })
+</script>
 
     @stop
 
