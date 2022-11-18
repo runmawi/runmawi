4b2ba027e8c5bca1b151491e62dccc797e6ffe9c
M sanjai Kumar <illvarasi@webnexs.com>
2022-11-18 18:22:25 +0530

    aws storage for livesteram store and  upload and audio store and upload

--- routes/web.php
+++ routes/web.php
@@ -871,6 +871,7 @@ Route::get('/episode/filedelete/{id}', 'AdminSeriesController@filedelete');
     
     Route::post('/uploadAudio',  'AdminAudioController@uploadAudio');
     Route::post('/Audiofile',  'AdminAudioController@Audiofile');
+    Route::post('/AWSUploadAudio',  'AdminAudioController@AWSUploadAudio');
 
     /*Moderators*/
 
