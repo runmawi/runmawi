4049a8721d4e6b1a1fba54570061442a72f36109
Barath <barath@webnexs.in>
2023-01-11 12:06:37 +0530

    header perload css

--- public/themes/default/views/header.php
+++ public/themes/default/views/header.php
@@ -131,7 +131,8 @@
    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
    <link rel="shortcut icon" type="image/png" href="<?= URL::to('/'). '/public/uploads/settings/'. $settings->favicon; ?>" />
    <!-- Bootstrap CSS -->
-   <link rel="stylesheet" href="<?= URL::to('/'). '/assets/css/bootstrap.min.css';?>" />
+     <link rel="preload" href="<?= URL::to('/'). '/assets/css/bootstrap.min.css';?>" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link rel="stylesheet" href="<?= URL::to('/'). '/assets/css/bootstrap.min.css';?>"></noscript>
    <!-- Typography CSS -->
    <link rel="stylesheet" href="<?= typography_link();?>" />
    <!-- Style -->
