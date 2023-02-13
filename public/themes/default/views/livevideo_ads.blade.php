2b100b73789e2fa24b6bf00b237b1da06b27cdc0
Manikandan <manikandan@webnexs.in>
2023-02-13 20:30:18 +0530

    ads live

--- public/themes/default/views/livevideo_ads.blade.php
+++ public/themes/default/views/livevideo_ads.blade.php
@@ -7,24 +7,10 @@ if(  plans_ads_enable() == 1 ){
         // ->whereTime('start', '<=', $current_time)
         // ->whereTime('end', '>=', $current_time)
         ->where('ads_events.status',1)
-        ->where('advertisements.status',1);
-
-        if( $video->pre_ads != null){
-            $live_ads = $live_ads->where('ads_position','pre')
-                        ->where('advertisements.id',$video->pre_ads);
-        }
-        elseif ( $video->mid_ads != null ){
-            $live_ads = $live_ads->where('ads_position','mid')
-                        ->where('advertisements.id',$video->mid_ads);
-        }
-        elseif( $video->post_ads != null ){
-            $live_ads = $live_ads->where('ads_position','post')
-                        ->where('advertisements.id',$video->post_ads);
-        }else{
-            $live_ads = $live_ads->where('ads_position','null');
-        }
-
-        $live_ads = $live_ads->pluck('ads_path')->first();
+        ->where('advertisements.status',1)
+        ->where('advertisements.id',$video->live_ads)
+        ->where('advertisements.ads_position',$video->ads_position)
+        ->pluck('ads_path')->first();
   }
   else
   {
