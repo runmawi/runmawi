827eede58312d047802efac557540ee85a1c81a4
barathbk <barath@webnexs.in>
2023-02-28 10:30:09 +0530

    recom

--- public/themes/theme5-nemisha/views/video_before_login.php
+++ public/themes/theme5-nemisha/views/video_before_login.php
@@ -565,7 +565,7 @@ if(!empty($request_url)){
       
 
     <div class="video-list you-may-like overflow-hidden">
-            <h4 class="Continue Watching" style="color:#fffff;"><?php echo __('Recomended Videos');?></h4>
+            <h4 class="Continue Watching" style="color:#fffff;"><?php echo __('Recommended Videos');?></h4>
                 <div class="slider" data-slick='{"slidesToShow": 4, "slidesToScroll": 4, "autoplay": false}'>   
                 <?php include('partials/video-loop.php');?>
                 </div>
