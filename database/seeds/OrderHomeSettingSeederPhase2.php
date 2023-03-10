60569bff9a82b5890a13076f91395c0162a9aa19
Manikandan <manikandan@webnexs.in>
2023-03-10 15:35:27 +0530

    OrderHomeSettingSeeder

---
+++ database/seeds/OrderHomeSettingSeederPhase2.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OrderHomeSettingSeederPhase2 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $Menu = [
+
+            [   
+                'user_id' => 1, 
+                'order_id' => 16,
+                'video_name' => 'latest_viewed_Videos' ,
+                'header_name' => 'Latest Viewed Videos', 
+                'url' => '',
+                'created_at' => Carbon::now(),
+                'updated_at' => null,
+            ],
+
+            [   
+                'user_id' => 1, 
+                'order_id' => 17,
+                'video_name' => 'latest_viewed_Livestream' ,
+                'header_name' => 'Latest Viewed Livestream', 
+                'url' => '',
+                'created_at' => Carbon::now(),
+                'updated_at' => null,
+            ],
+            [   
+                'user_id' => 1, 
+                'order_id' => 18,
+                'video_name' => 'latest_viewed_Audios' ,
+                'header_name' => 'Latest Viewed Audios', 
+                'url' => '',
+                'created_at' => Carbon::now(),
+                'updated_at' => null,
+            ],
+            
+            [   
+                'user_id' => 1, 
+                'order_id' => 19,
+                'video_name' => 'latest_viewed_Episode' ,
+                'header_name' => 'Latest Viewed Episode', 
+                'url' => '',
+                'created_at' => Carbon::now(),
+                'updated_at' => null,
+            ],
+
+        ];
+
+        OrderHomeSetting::insert($Menu);
+    }
+}
