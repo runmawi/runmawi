930016278b65023cda84b1385c3b681f2603c070
Manikandan <manikandan@webnexs.in>
2023-03-13 19:46:33 +0530

    theme5

--- public/themes/theme5-nemisha/views/partials/home/latest_viewed_Episode.php
+++ public/themes/theme5-nemisha/views/partials/home/latest_viewed_Episode.php
@@ -32,44 +32,71 @@ if (Auth::guest() != true) {
     </h4>
 </div>
 
-
 <div class="favorites-contens">
-    <ul class="favorites-slider list-inline  row p-0 mb-0">
+    <ul class="favorites-slider list-inline row p-0 mb-0">
         <?php  if(isset($latest_view_episodes)) :
-    					 foreach($latest_view_episodes as $key => $latest_view_episodes) {
-?>
+                foreach($latest_view_episodes as $key => $latest_view_episode):  ?>
+
         <li class="slide-item">
-            <a class="text-white "
-                href="<?= URL::to('/episode' . '/' . $latest_view_episodes->series_slug . '/' . $latest_view_episodes->episode_slug) ?> ">
-                <!-- block-images -->
+            <a href="<?= URL::to('/episode' . '/' . $latest_view_episode->series_slug . '/' . $latest_view_episode->episode_slug); ?>">
                 <div class="block-images position-relative">
+
+                    <!-- block-images -->
                     <div class="img-box">
-                        <img src="<?php echo URL::to('/') . '/public/uploads/images/' . $latest_view_episodes->image; ?>" class="img-fluid w-100" alt="">
+                        <img loading="lazy" data-src="<?php echo URL::to('/') . '/public/uploads/images/' . $latest_view_episode->image; ?>" class="img-fluid lazyload w-100"
+                            alt="">
                     </div>
                 </div>
+
                 <div class="block-description">
-                    <div class="hover-buttons d-flex">
-                        <a class="text-white "
-                            href="<?= URL::to('/episode' . '/' . $latest_view_episodes->series_slug . '/' . $latest_view_episodes->episode_slug) ?> ">
-                            <img class="ply" src="<?php echo URL::to('/') . '/assets/img/play.svg'; ?>">
-                        </a>
+                    <div class="hover-buttons text-white">
+                        <a class="" href="<?= URL::to('/episode' . '/' . $latest_view_episode->series_slug . '/' . $latest_view_episode->episode_slug) ?>"> <img
+                                class="ply" src="<?php echo URL::to('/') . '/assets/img/default_play_buttons.svg'; ?>" /> </a>
+                        <div></div>
                     </div>
                 </div>
-                <div>
-                    <div class="movie-time d-flex align-items-center justify-content-between my-2">
-                        <a class="text-white "
-                            href="<?= URL::to('/episode' . '/' . $latest_view_episodes->series_slug . '/' . $latest_view_episodes->episode_slug) ?> ">
-                            <h6><?php echo __($latest_view_episodes->title); ?></h6>
-                        </a>
-                        <div class="badge badge-secondary p-1 mr-2"><?php echo $latest_view_episodes->age_restrict . ' ' . '+'; ?></div>
-                    </div>
-                    <span class="text-white"><i class="fa fa-clock-o"></i>
-                        <?= gmdate('H:i:s', $latest_view_episodes->duration) ?></span>
+
+                <div class="mt-2 d-flex justify-content-between p-0">
+                    <?php if($ThumbnailSetting->title == 1) { ?>
+                        <h6><?php echo strlen($latest_view_episode->title) > 17 ? substr($latest_view_episode->title, 0, 18) . '...' : $latest_view_episode->title; ?></h6>
+                    <?php } ?>
+
+                    <?php if($ThumbnailSetting->age == 1) { ?>
+                        <div class="badge badge-secondary"><?php echo $latest_view_episode->age_restrict . ' ' . '+'; ?></div>
+                    <?php } ?>
+                </div>
+
+                <div class="movie-time my-2">
+
+                    <!-- Duration -->
+                    <?php if($ThumbnailSetting->duration == 1) { ?>
+                        <span class="text-white">
+                            <i class="fa fa-clock-o"></i>
+                            <?= gmdate('H:i:s', $latest_view_episode->duration) ?>
+                        </span>
+                    <?php } ?>
+
+                    <!-- Rating -->
+
+                    <?php if($ThumbnailSetting->rating == 1 && $latest_view_episode->rating != null) { ?>
+                        <span class="text-white">
+                            <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                            <?php echo __($latest_view_episode->rating); ?>
+                        </span>
+                    <?php } ?>
+
+                    <?php if($ThumbnailSetting->featured == 1 && $latest_view_episode->featured == 1) { ?>
+                    <!-- Featured -->
+                        <span class="text-white">
+                            <i class="fa fa-flag" aria-hidden="true"></i>
+                        </span>
+                    <?php }?>
                 </div>
             </a>
         </li>
-        <?php  } 
-      // }
-    endif; ?>
+        <?php                     
+            endforeach; 
+            endif; 
+        ?>
     </ul>
 </div>
