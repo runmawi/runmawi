460ed9b296d2c353ad653638380da012e7eb35b1
barath <barath@webnexs.in>
2023-03-24 16:31:17 +0530

    cap

--- resources/views/admin/videos/videoapproval/approval_index.blade.php
+++ resources/views/admin/videos/videoapproval/approval_index.blade.php
@@ -35,9 +35,9 @@ border-radius: 0px 4px 4px 0px;
          <div class="container-fluid p-0">
             <div class="row">
                <div class="col-sm-12">
-                  <div class="iq-card">
+                  <div class="iq-card"> <h4>CPP Uploaded Videos</h4>
                      <div class="iq-card-header d-flex justify-content-between">
-                       
+                        
                      <div class="col-md-12">
                   <div class="row">
                      <div class="col-md-6">
@@ -48,7 +48,7 @@ border-radius: 0px 4px 4px 0px;
                      </div>
                   </div>
                </div>
-                        <h4>CPP Uploaded Videos</h4>
+                      
                          <div class="iq-card-header-toolbar d-flex align-items-baseline">
                              <div class="form-group mr-2">
                     <!-- <input type="text" name="search" id="search" class="form-control" placeholder="Search Data" /> -->
