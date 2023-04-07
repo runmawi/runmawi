6371955859cdf88e0d8a98dd58516ff1171d7293
barath <barath@webnexs.in>
2023-04-07 17:07:18 +0530

    see all

--- public/themes/theme5-nemisha/views/partials/home/latest-series.php
+++ public/themes/theme5-nemisha/views/partials/home/latest-series.php
@@ -10,7 +10,10 @@
     <a href="<?php if ($order_settings_list[4]->header_name) { echo URL::to('/').'/'.$order_settings_list[4]->url ;} else { echo "" ; } ?>">
       <?php if ($order_settings_list[4]->header_name) { echo $order_settings_list[4]->header_name ;} else { echo "" ; } ?>
     </a>
-  </h4>                      
+  </h4>  
+     <a class="see" href="<?php if ($order_settings_list[4]->header_name) { echo URL::to('/').'/'.$order_settings_list[4]->url ;} else { echo "" ; } ?>">See All
+    
+    </a>
 </div>
 <div class="favorites-contens">
   <ul class="favorites-slider list-inline  row p-0 mb-0">
