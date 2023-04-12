d0fb145184556ae5d5d9f14e6ac54c72678913eb
Manikandan <manikandan@webnexs.in>
2023-04-12 20:39:40 +0530

    Episode_videos learn

--- public/themes/theme5-nemisha/views/All-Videos/learn.blade.php
+++ public/themes/theme5-nemisha/views/All-Videos/learn.blade.php
@@ -18,108 +18,67 @@ include public_path('themes/theme5-nemisha/views/header.php');
                     </div>
                     <div class="favorites-contens">
                         <ul class="category-page list-inline row p-0 mb-0">
+
                             @if (isset($respond_data['videos']))
+                                @foreach ($respond_data['videos'] as $key => $videos)
+                                    <li class="slide-item">
+                                        <div class="block-images position-relative">
+                                            <!-- block-images -->
+                                            <a
+                                                href="{{  URL::to('episode/'.@$videos->series_slug.'/'.$videos->slug ) }}">
+                                                <img loading="lazy" data-src="{{ URL::to('public/uploads/images/'.$videos->image)  }} " class="img-fluid"
+                                                    alt="">
+                                            </a>
+                                        </div>
 
-                                @foreach ($respond_data['videos'] as $key => $video)
-                                    <li class="slide-item col-sm-2 col-md-2 col-xs-12">
-                                        <a href="<?php echo URL::to('home'); ?>">
-                                            <div class="block-images position-relative">
-                                                <div class="img-box">
-                                                    <img loading="lazy"
-                                                        data-src="{{ URL::to('public/uploads/images/' . $video->image) }}"
-                                                        class="img-fluid" alt="">
-                                                </div>
-
-                                                <div class="block-description">
-                                                    <div class="hover-buttons">
-                                                        <a href="{{ URL::to('category/videos/' . $video->slug) }}">
-                                                            <img class="ply"
-                                                                src="{{ URL::to('assets/img/play.svg') }} ">
-                                                        </a>
-                                                        <div>
-                                                            <!-- <a   href="" class="text-white mt-4"><i class="fa fa-plus" aria-hidden="true"></i> Add to Watchlist</a> -->
-                                                        </div>
-                                                    </div>
-                                                </div>
+                                        <div class="block-description">
+                                            <div class="hover-buttons">
+                                                <a type="button" class="text-white btn-cl"
+                                                    href="{{  URL::to('episode/'.@$videos->series_slug.'/'.$videos->slug ) }}">
+                                                    <img class="ply" src="{{ URL::to('assets/img/default_play_buttons.svg') }}  " />
+                                                </a>
                                             </div>
-                                            <div>
+                                        </div>
 
-                                                <div class="mt-2 d-flex justify-content-between p-0">
-                                                    @if ($respond_data['ThumbnailSetting']->title == 1)
-                                                        <h6>{{ strlen($video->title) > 17 ? substr($video->title, 0, 18) . '...' : $video->title }}
-                                                        </h6>
-                                                    @endif
+                                        <div class="mt-2 d-flex justify-content-between p-0">
+                                            @if ($respond_data['ThumbnailSetting']->title == 1)
+                                                <h6>{{ strlen($videos->title) > 17 ? substr($videos->title, 0, 18) . '...' : $videos->title }}
+                                                </h6>
+                                            @endif
 
-                                                    @if ($respond_data['ThumbnailSetting']->age == 1)
-                                                        <div class="badge badge-secondary">
-                                                            {{ $video->age_restrict . ' ' . '+' }}
-                                                        </div>
-                                                    @endif
-                                                </div>
+                                            @if ($respond_data['ThumbnailSetting']->age == 1)
+                                                <div class="badge badge-secondary">
+                                                    {{ $videos->age_restrict . ' ' . '+' }} </div>
+                                            @endif
+                                        </div>
 
+                                        <div class="movie-time my-2">
 
-                                                <div class="movie-time my-2">
+                                            <!-- Duration -->
 
-                                                    <!-- Duration -->
+                                            @if ($respond_data['ThumbnailSetting']->duration == 1)
+                                                <span class="text-white">
+                                                    <i class="fa fa-clock-o"></i>
+                                                    {{ gmdate('H:i:s', $videos->duration) }}
+                                                </span>
+                                            @endif
 
-                                                    @if ($respond_data['ThumbnailSetting']->duration == 1)
-                                                        <span class="text-white">
-                                                            <i class="fa fa-clock-o"></i>
-                                                            <?= gmdate('H:i:s', $video->duration) ?>
-                                                        </span>
-                                                    @endif
+                                            <!-- Rating -->
 
-                                                    <!-- Rating -->
+                                            @if ($respond_data['ThumbnailSetting']->rating == 1 && $videos->rating != null)
+                                                <span class="text-white">
+                                                    <i class="fa fa-star-half-o" aria-hidden="true"></i>
+                                                    {{ $videos->rating }}
+                                                </span>
+                                            @endif
 
-                                                    @if ($respond_data['ThumbnailSetting']->rating == 1 && $video->rating != null)
-                                                        <span class="text-white">
-                                                            <i class="fa fa-star-half-o" aria-hidden="true"></i>
-                                                            <?php echo __($video->rating); ?>
-                                                        </span>
-                                                    @endif
-
-                                                    @if ($respond_data['ThumbnailSetting']->featured == 1 && $video->featured == 1)
-                                                        <!-- Featured -->
-                                                        <span class="text-white">
-                                                            <i class="fa fa-flag" aria-hidden="true"></i>
-                                                        </span>
-                                                    @endif
-                                                </div>
-
-                                                <div class="movie-time my-2">
-                                                    <!-- published_year -->
-
-                                                    @if ($respond_data['ThumbnailSetting']->published_year == 1 && $video->year != null)
-                                                        <span class="text-white">
-                                                            <i class="fa fa-calendar" aria-hidden="true"></i>
-                                                            <?php echo __($video->year); ?>
-                                                        </span>
-                                                    @endif
-                                                </div>
-
-                                                <div class="movie-time my-2">
-                                                    <!-- Category Thumbnail  setting -->
-                                                    <?php
-                                                    $CategoryThumbnail_setting = App\CategoryVideo::join('video_categories', 'video_categories.id', '=', 'categoryvideos.category_id')
-                                                        ->where('categoryvideos.video_id', $video->id)
-                                                        ->pluck('video_categories.name');
-                                                    ?>
-
-                                                    @if ($respond_data['ThumbnailSetting']->category == 1 && count($CategoryThumbnail_setting) > 0)
-                                                        <span class="text-white">
-                                                            <i class="fa fa-list-alt" aria-hidden="true"></i>
-                                                            <?php
-                                                            $Category_Thumbnail = [];
-                                                            foreach ($CategoryThumbnail_setting as $key => $CategoryThumbnail) {
-                                                                $Category_Thumbnail[] = $CategoryThumbnail;
-                                                            }
-                                                            echo implode(',' . ' ', $Category_Thumbnail);
-                                                            ?>
-                                                        </span>
-                                                    @endif
-                                                </div>
-                                            </div>
-                                        </a>
+                                            @if ($respond_data['ThumbnailSetting']->featured == 1 && $videos->featured == 1)
+                                                <!-- Featured -->
+                                                <span class="text-white">
+                                                    <i class="fa fa-flag" aria-hidden="true"></i>
+                                                </span>
+                                            @endif
+                                        </div>
                                     </li>
                                 @endforeach
                             @endif
@@ -128,7 +87,6 @@ include public_path('themes/theme5-nemisha/views/header.php');
                         <div class="col-md-12 pagination justify-content-end">
                             {!! $respond_data['videos']->links() !!}
                         </div>
-
                     </div>
                 </div>
             </div>
