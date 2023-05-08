5ee83cc9420ed513bd60cbadddbf26269ce833b4
barath <barath@webnexs.in>
2023-05-08 17:10:32 +0530

    data src

--- public/themes/theme5-nemisha/views/All-Videos/learn.blade.php
+++ public/themes/theme5-nemisha/views/All-Videos/learn.blade.php
@@ -101,7 +101,7 @@
                                         <a href="{{ $series->redirect_url }}">
                                             <div class="block-images position-relative">
                                                 <div class="img-box">
-                                                    <img loading="lazy"  data-original="{{ $series->image_url }}" class="img-fluid lazyload w-100">
+                                                    <img loading="lazy"  data-src="{{ $series->image_url }}" class="img-fluid lazyload w-100">
                                                 </div>
                                             </div>
 
