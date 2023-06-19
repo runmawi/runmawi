5447ddc1429fdec27dfe803438f8969445c182b8
barath <barath@webnexs.in>
2023-06-19 15:44:40 +0530

    my profilwe

--- public/themes/theme5-nemisha/views/partials/home/trending-videoloop.php
+++ public/themes/theme5-nemisha/views/partials/home/trending-videoloop.php
@@ -3,7 +3,8 @@
 <a href="<?php if ($order_settings_list[0]->header_name) { echo URL::to('/').'/'.$order_settings_list[0]->url ;} else { echo "" ; } ?>">                   
   <?php if ($order_settings_list[0]->header_name) { echo $order_settings_list[0]->header_name ;} else { echo "" ; } ?>
                     <!-- Featured Movies -->
-                  </a></h4>    
+                  </a> 
+    </h4>    
 </div>
 <div class="favorites-contens">
     <ul class="favorites-slider list-inline row p-0 mb-0">
