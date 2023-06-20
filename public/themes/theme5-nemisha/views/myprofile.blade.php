5145c318e06afa2d653b53f3304ae373bbd39728
Sanjai Kumar M <sanjai@webnexs.in>
2023-06-20 17:40:16 +0530

    others changes

--- public/themes/theme5-nemisha/views/myprofile.blade.php
+++ public/themes/theme5-nemisha/views/myprofile.blade.php
@@ -394,7 +394,7 @@
                                         <option value="null" @if(!empty($user->gender) && $user->gender == "null" ){{ 'selected' }}@endif>   Gender </option>
                                         <option value="Male" @if(!empty($user->gender) && $user->gender == 'Male'){{ 'selected' }}@endif>  Male </option>
                                         <option value="Female" @if(!empty($user->gender) && $user->gender == 'Female'){{ 'selected' }}@endif> Female </option>
-                                        <option value="Others" @if(!empty($user->gender) && $user->gender == 'Others'){{ 'selected' }}@endif > Others </option>
+                                        <!-- <option value="Others" @if(!empty($user->gender) && $user->gender == 'Others'){{ 'selected' }}@endif > Others </option> -->
                                     </select>
                                 </span>
                             </p>
