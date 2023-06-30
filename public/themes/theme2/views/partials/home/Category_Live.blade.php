dc761e0e083f229fd7ca97b438186aed71974752
Manikandan <manikandan@webnexs.in>
2023-06-30 18:00:28 +0530

    theme2 - breadcrumbs

--- public/themes/theme2/views/partials/home/Category_Live.blade.php
+++ public/themes/theme2/views/partials/home/Category_Live.blade.php
@@ -2,6 +2,23 @@
 include public_path('themes/theme2/views/header.php');
 ?>
 
+<style>
+    /* <!-- BREADCRUMBS  */
+    .bc-icons-2 .breadcrumb-item+.breadcrumb-item::before {
+        content: none;
+    }
+
+    ol.breadcrumb {
+        color: white;
+        background-color: transparent !important;
+        font-size: revert;
+    }
+
+    .nav-div.container-fluid {
+        padding: 0;
+    }
+</style>
+
 <!-- MainContent -->
 <section id="iq-favorites">
     @if (isset($Live_Category) && count($Live_Category) > 0)
@@ -14,6 +31,29 @@ include public_path('themes/theme2/views/header.php');
                     <div class="iq-main-header align-items-center justify-content-between">
 
                     </div>
+
+                    <!-- BREADCRUMBS -->
+                    <div class="row d-flex">
+                        <div class="nav nav-tabs nav-fill container-fluid nav-div" id="nav-tab" role="tablist">
+                            <div class="bc-icons-2">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a class="black-text"
+                                            href="<?= route('liveList') ?>"><?= ucwords('live Stream') ?></a>
+                                        <i class="fa fa-angle-double-right mx-2" aria-hidden="true"></i>
+                                    </li>
+
+                                    <li class="breadcrumb-item"><a class="black-text"
+                                            href="<?= route('CategoryLive') ?>"><?= ucwords(' Live Category') ?></a>
+                                        <i class="fa fa-angle-double-right mx-2" aria-hidden="true"></i>
+                                    </li>
+
+                                    <li class="breadcrumb-item"><a class="black-text"><?php echo strlen($category_title) > 50 ? ucwords(substr($category_title, 0, 120) . '...') : ucwords($category_title); ?>
+                                        </a></li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+
                     <div class="favorites-contens">
                         <ul class="category-page list-inline row p-0 mb-0">
                             @if (isset($Live_Category))
