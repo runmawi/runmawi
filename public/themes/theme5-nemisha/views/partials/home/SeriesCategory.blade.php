d86336d4b448d6dabea606db53c2f096adde5d25
Manikandan <manikandan@webnexs.in>
2023-06-30 17:40:57 +0530

    Theme 5 breadcrumbs

--- public/themes/theme5-nemisha/views/partials/home/SeriesCategory.blade.php
+++ public/themes/theme5-nemisha/views/partials/home/SeriesCategory.blade.php
@@ -1,45 +1,85 @@
 <?php
 include public_path('themes/theme5-nemisha/views/header.php');
+
 ?>
+
+<style>
+    /* <!-- BREADCRUMBS  */
+    .bc-icons-2 .breadcrumb-item+.breadcrumb-item::before {
+        content: none;
+    }
+
+    ol.breadcrumb {
+        color: white;
+        background-color: transparent !important;
+        font-size: revert;
+    }
+
+    .nav-div.container-fluid {
+        padding: 0;
+    }
+</style>
+
 <section id="iq-favorites">
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12 page-height">
                 <div class="iq-main-header align-items-center justify-content-between">
-                    <h2 class="text-center  mb-3 mt-3"> <?php echo @$CategorySeries->name; ?></h2>
+                    <h2 class="text-center  mb-3 mt-3"> {{ @$CategorySeries->name }} </h2>
                 </div>
+
+                <!-- BREADCRUMBS -->
+                <div class="row d-flex">
+                    <div class="nav nav-tabs nav-fill container-fluid nav-div" id="nav-tab" role="tablist">
+                        <div class="bc-icons-2">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a class="black-text"
+                                        href="<?= route('series.tv-shows') ?>"><?= ucwords('Series') ?></a>
+                                    <i class="fa fa-angle-double-right mx-2" aria-hidden="true"></i>
+                                </li>
+
+                                <li class="breadcrumb-item"><a class="black-text"
+                                        href="<?= route('SeriescategoryList') ?>"><?= ucwords('category') ?></a>
+                                    <i class="fa fa-angle-double-right mx-2" aria-hidden="true"></i>
+                                </li>
+
+                                <li class="breadcrumb-item"><a class="black-text"><?php echo strlen($CategorySeries->name) > 50 ? ucwords(substr($CategorySeries->name, 0, 120) . '...') : ucwords($CategorySeries->name); ?> </a></li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="favorites-contens">
                     <ul class="category-page list-inline row p-0 mb-0">
-                        @if(isset($SeriesGenre)) 
+                        @if (isset($SeriesGenre))
                             @forelse($SeriesGenre as $Series_Genre)
                                 <li class="slide-item col-sm-2 col-md-2 col-xs-12">
-                                    <a href="<?php echo URL::to('/play_series/' . $Series_Genre->slug); ?>">
+                                    <a href=" {{ URL::to('play_series/' . $Series_Genre->slug) }}">
                                         <div class="block-images position-relative">
                                             <div class="img-box">
-                                                <img src="<?php echo URL::to('/') . '/public/uploads/images/' . @$Series_Genre->image; ?>" class="img-fluid w-100" alt="">
+                                                <img src="{{ URL::to('public/uploads/images' . @$Series_Genre->image) }}"
+                                                    class="img-fluid w-100" alt="">
                                             </div>
-  </div>
-                                    
-                                            <div class="block-description">    </div>
-                                                <a href="<?php echo URL::to('/play_series/') . '/' . $Series_Genre->slug; ?>">
-                                                    <h6><?php echo strlen(@$Series_Genre->title) > 17 ? substr(@$Series_Genre->title, 0, 18) . '...' : @$Series_Genre->title; ?></h6>
-                                                </a>
-                                                <div class="hover-buttons">
-                                                    <div>
-                                                    </div>
-                                                </div>
-                                                <div>
-                                                   <!-- <a class="text-white" href="<?php echo URL::to('/play_series' . '/' . $Series_Genre->slug); ?> ">
-                                                        <i class="fa fa-play mr-1" aria-hidden="true"></i>
-                                                        Visit Series
-                                                    </a>-->
-                                                </div>
-                                          
+                                        </div>
+
+                                        <div class="block-description"> </div>
+                                        <a href=" {{ URL::to('play_series/' . $Series_Genre->slug) }}">
+                                            <h6><?php echo strlen(@$Series_Genre->title) > 17 ? substr(@$Series_Genre->title, 0, 18) . '...' : @$Series_Genre->title; ?></h6>
+                                        </a>
+                                        <div class="hover-buttons">
+                                            <div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                        </div>
+
                                     </a>
                                 </li>
                             @empty
-                                <div class="col-md-12 text-center mt-4" style="background: url(<?=URL::to('/assets/img/watch.png') ?>);heigth: 500px;background-position:center;background-repeat: no-repeat;background-size:contain;height: 500px!important;">
-                                    <p ><h3 class="text-center"> {{  "No Series Available" }}</h3>
+                                <div class="col-md-12 text-center mt-4"
+                                    style="background: url(<?= URL::to('/assets/img/watch.png') ?>);heigth: 500px;background-position:center;background-repeat: no-repeat;background-size:contain;height: 500px!important;">
+                                    <p>
+                                    <h3 class="text-center"> {{ 'No Series Available' }}</h3>
                                 </div>
                             @endforelse
                         @endif
@@ -49,7 +89,6 @@ include public_path('themes/theme5-nemisha/views/header.php');
         </div>
     </div>
 </section>
-
 
 <?php
 include public_path('themes/theme5-nemisha/views/footer.blade.php');
