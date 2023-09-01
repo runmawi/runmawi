c88c6490acd0b9886182cef78e36fbb8b61e6cd5
ManojBalaji <manoj@webnexs.com>
2023-09-01 14:25:37 +0530

    Signup and CMS

--- public/themes/default/views/auth/login.blade.php
+++ public/themes/default/views/auth/login.blade.php
@@ -93,7 +93,7 @@ i.fa.fa-google-plus {
     </head>
 
 <body>
-<section class="sign-in-page" style="url('<?php echo URL::to('/').'/public/uploads/settings/'.$settings->login_content; ?>') no-repeat scroll 0 0;;background-size: cover;">
+<section class="sign-in-page" style="background:url('<?php echo URL::to('/').'/public/uploads/settings/'.$settings->login_content; ?>') no-repeat scroll 0 0;;background-size: cover;">
    <div class="container">
       <div class="row mb-4  align-items-center height-self-center">
           <div class="col-lg-7  col-12">
