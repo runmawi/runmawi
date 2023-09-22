9b3f4c223a03061675a07289bda56ffe7cef8b89
Sanjai Kumar M <sanjai@webnexs.in>
2023-09-22 18:27:23 +0530

    lable

--- public/themes/default/views/partials/Episode/Other_episodes_list.blade.php
+++ public/themes/default/views/partials/Episode/Other_episodes_list.blade.php
@@ -19,17 +19,18 @@
                         </div>
                         <div class="block-description">
                             <h6><?php echo strlen($episodes->title) > 15 ? substr($episodes->title, 0, 15) . '...' : $episodes->title; ?></h6>
-
-                            <p class="date" style="color:#fff;font-size:14px;">
-                                <?= date('F jS, Y', strtotime($episodes->created_at)) ?>
-                                <?php if($episodes->access == 'guest'): ?>
-                                <span class="label label-info">Free</span>
-                                <?php elseif($episodes->access == 'subscriber'): ?>
-                                <span class="label label-success">Subscribers Only</span>
-                                <?php elseif($episodes->access == 'registered'): ?>
-                                <span class="label label-warning">Registered Users</span>
-                                <?php endif; ?>
-                            </p>
+                            <?php if($ThumbnailSetting->free_or_cost_label == 1): ?> 
+                                <p class="date" style="color:#fff;font-size:14px;">
+                                    <?= date('F jS, Y', strtotime($episodes->created_at)) ?>
+                                    <?php if($episodes->access == 'guest'): ?>
+                                    <span class="label label-info">Free</span>
+                                    <?php elseif($episodes->access == 'subscriber'): ?>
+                                    <span class="label label-success">Subscribers Only</span>
+                                    <?php elseif($episodes->access == 'registered'): ?>
+                                    <span class="label label-warning">Registered Users</span>
+                                    <?php endif; ?>
+                                </p>
+                            <?php endif; ?>
 
                             <div class="hover-buttons">
                                 <a
--- public/themes/default/views/partials/Episode/Recommend_series_episode_page.blade.php
+++ public/themes/default/views/partials/Episode/Recommend_series_episode_page.blade.php
@@ -6,6 +6,7 @@
     <div class="favorites-contens ml-2">
         <ul class="favorites-slider list-inline row mb-0">
             <?php  
+                $ThumbnailSetting = App\ThumbnailSetting::first();
                 foreach($series_lists as $key => $series_list):
             ?>
             <li class="slide-item">
@@ -16,17 +17,18 @@
                         </div>
                         <div class="block-description">
                             <h6><?php echo strlen($series_list->title) > 15 ? substr($series_list->title, 0, 15) . '...' : $series_list->title; ?></h6>
-
-                            <p class="date" style="color:#fff;font-size:14px;">
-                                <?= date('F jS, Y', strtotime($series_list->created_at)) ?>
-                                <?php if($series_list->access == 'guest'): ?>
-                                <span class="label label-info">Free</span>
-                                <?php elseif($series_list->access == 'subscriber'): ?>
-                                <span class="label label-success">Subscribers Only</span>
-                                <?php elseif($series_list->access == 'registered'): ?>
-                                <span class="label label-warning">Registered Users</span>
-                                <?php endif; ?>
-                            </p>
+                            <?php if($ThumbnailSetting->free_or_cost_label == 1): ?> 
+                                <p class="date" style="color:#fff;font-size:14px;">
+                                    <?= date('F jS, Y', strtotime($series_list->created_at)) ?>
+                                    <?php if($series_list->access == 'guest'): ?>
+                                    <span class="label label-info">Free</span>
+                                    <?php elseif($series_list->access == 'subscriber'): ?>
+                                    <span class="label label-success">Subscribers Only</span>
+                                    <?php elseif($series_list->access == 'registered'): ?>
+                                    <span class="label label-warning">Registered Users</span>
+                                    <?php endif; ?>
+                                </p>
+                            <?php endif; ?>
 
                             <div class="movie-time d-flex align-items-center my-2">
                                 <div class="badge badge-secondary p-1 mr-2">
