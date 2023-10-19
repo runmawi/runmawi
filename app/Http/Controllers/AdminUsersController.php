060664ed8f8adee2a3021e5b4e3f5c092fdd0c62
Sanjai Kumar M <sanjai@webnexs.in>
2023-10-20 02:38:35 +0530

    export users

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4367,6 +4367,9 @@ class AdminUsersController extends Controller
                 "Contact Number",
                 "Current Package",
                 "User Type",
+                "User Country",
+                "User State",
+                "User City",
                 "Active",
 
             ]);
@@ -4392,6 +4395,9 @@ class AdminUsersController extends Controller
                         $each_user->mobile,
                         $current_plan,
                         $each_user->role,
+                        $each_user->country,
+                        $each_user->state,
+                        $each_user->city,
                         $Active,
                     ]);
                 }
