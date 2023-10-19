fe63d7773180e29b32bef4b0b04f7ea08c173777
Manikandan <manikandan@webnexs.in>
2023-10-19 13:54:22 +0530

    Category list

--- public/themes/theme6/views/languagevideo.blade.php
+++ public/themes/theme6/views/languagevideo.blade.php
@@ -2,172 +2,75 @@
     include public_path('themes/theme6/views/header.php');
 @endphp
 
-<!-- loader Start -->
-{{-- <div id="loading">
-         <div id="loading-center">
-         </div>
-      </div> --}}
-<!-- loader END -->
-
 <!-- Slider  -->
-<section id="home" class="iq-main-slider p-0">
-    <div id="home-slider" class="slider m-0 p-0">
-        <div class="slide slick-bg s-bg-1">
-            <div class="container-fluid position-relative h-100">
-                <div class="slider-inner h-100">
-                    <div class="row align-items-center  h-100">
-                        <div class="col-xl-6 col-lg-12 col-md-12">
-                            <h1 class="slider-text big-title title text-uppercase" data-animation-in="fadeInLeft"
-                                data-delay-in="0.6">bushland</h1>
-                            <div class="d-flex align-items-center" data-animation-in="fadeInUp" data-delay-in="1">
-                                <span class="badge  p-2"><img
-                                        src="https://localhost/flicknexs/public/themes/theme6/assets/images/Group 3 (1).png"></span>
-                                <span class="ml-3">4.5(Imdb)</span>
+@if (!empty($video_banners) && $video_banners->isNotEmpty())
+    <section id="home" class="iq-main-slider p-0">
+        <div id="home-slider" class="slider m-0 p-0">
+            @foreach ($video_banners as $item)
+                <div class="slide slick-bg s-bg-1" style="background: url('{{ URL::to('public/uploads/images/' . $item->player_image) }}')" >
+                    <div class="container-fluid position-relative h-100">
+                        <div class="slider-inner h-100">
+                            <div class="row align-items-center  h-100">
+                                <div class="col-xl-6 col-lg-12 col-md-12">
+                                    <h1 class="slider-text big-title title text-uppercase" data-animation-in="fadeInLeft" data-delay-in="0.6">{{ strlen($item->title) > 13 ? substr($item->title, 0, 14) . '...' : $item->title }}</h1>
+
+                                    <div class="d-flex align-items-center" data-animation-in="fadeInUp" data-delay-in="1">
+                                        <span class="badge  p-2">
+                                            <ul class="ratting-start p-0 m-0 list-inline text-primary d-flex align-items-center justify-content-left">
+                                                @php $rating = ($item->rating / 2) ; @endphp
+                                                @for ($i = 1; $i <= 5; $i++)
+                                                    @if ($rating >= $i)
+                                                        <li><i class="fa fa-star" aria-hidden="true"></i></a></li>
+                                                    @elseif ($rating + 0.5 == $i)
+                                                        <li><i class="fa fa-star-half-o" aria-hidden="true"></i></a></li>
+                                                    @else
+                                                        <li><i class="fa fa-star-o" aria-hidden="true"></i></a></li>
+                                                    @endif
+                                                @endfor
+                                            </ul>
+                                        </span>
+                                        <span class="ml-3"> {{ $item->rating ? ( $item->rating / 2 ) : " "  }} </span>
+                                    </div>
+
+                                    <br />
+                                    <div class="d-flex align-items-center" data-animation-in="fadeInUp" data-delay-in="1">
+                                        <span class="badge badge-secondary p-2"> {{ optional($item)->age_restrict.'+' }} </span>
+                                        <li class="ml-3">{{ $item->publish_type == "publish_now" ? Carbon\Carbon::createFromDate($item->created_at)->toFormattedDateString()  : Carbon\Carbon::createFromDate($item->publish_time)->toFormattedDateString() }}</li>
+                                    </div>
+                                    <p data-animation-in="fadeInUp" data-delay-in="1.2"> {!! html_entity_decode( optional($item)->description) !!}  </p>
+                                    <div class="d-flex align-items-center r-mb-23" data-animation-in="fadeInUp"
+                                        data-delay-in="1.2">
+                                        <a href="{{ URL::to('category/videos/'.$item->slug ) }}" class="btn btn-hover"><i class="fa fa-play mr-2"
+                                                aria-hidden="true"></i>Play Now</a>
+                                    </div>
+                                </div>
                             </div>
-                            <br />
-                            <div class="d-flex align-items-center" data-animation-in="fadeInUp" data-delay-in="1">
-                                <span class="badge badge-secondary p-2">18+</span>
-                                <li class="ml-3">2 Seasons</li>
-                                <li class="ml-3">Feb 2023</li>
-                            </div>
-                            <p data-animation-in="fadeInUp" data-delay-in="1.2">Lorem Ipsum is simply dummy text of the
-                                printing and typesetting industry. Lorem Ipsum has been the industry's standard
-                                dummy text ever since the 1500s.
-                            </p>
-                            <div class="d-flex align-items-center r-mb-23" data-animation-in="fadeInUp"
-                                data-delay-in="1.2">
-                                <a href="show-details.html" class="btn btn-hover"><i class="fa fa-play mr-2"
-                                        aria-hidden="true"></i>Play Now</a>
-                            </div>
+
+                            @if ( optional($item)->trailer)
+                                {{-- <div class="trailor-video">
+                                    <a href="video/trailer.mp4" class="video-open playbtn">
+                                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="80px" height="80px"
+                                            viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7" xml:space="preserve">
+                                            <polygon class='triangle' fill="none" stroke-width="7" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-miterlimit="10"
+                                                points="73.5,62.5 148.5,105.8 73.5,149.1 " />
+                                            <circle class='circle' fill="none" stroke-width="7" stroke-linecap="round"
+                                                stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8"
+                                                r="103.3" />
+                                        </svg>
+                                        <span class="w-trailor">Watch Trailer</span>
+                                    </a>
+                                </div> --}}
+                            @endif
+                                
                         </div>
                     </div>
-                    <div class="trailor-video">
-                        <a href="video/trailer.mp4" class="video-open playbtn">
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="80px" height="80px"
-                                viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7" xml:space="preserve">
-                                <polygon class='triangle' fill="none" stroke-width="7" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-miterlimit="10"
-                                    points="73.5,62.5 148.5,105.8 73.5,149.1 " />
-                                <circle class='circle' fill="none" stroke-width="7" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8"
-                                    r="103.3" />
-                            </svg>
-                            <span class="w-trailor">Watch Trailer</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
+                </div> 
+            @endforeach
         </div>
-        <div class="slide slick-bg s-bg-2">
-            <div class="container-fluid position-relative h-100">
-                <div class="slider-inner h-100">
-                    <div class="row align-items-center  h-100">
-                        <div class="col-xl-6 col-lg-12 col-md-12">
-                            <h1 class="slider-text big-title title text-uppercase" data-animation-in="fadeInLeft">sail
-                                coaster</h1>
-                            <div class="d-flex align-items-center" data-animation-in="fadeInUp" data-delay-in="1">
-                                <span class="badge  p-2"><img
-                                        src="https://localhost/flicknexs/public/themes/theme6/assets/images/Group 3 (1).png"></span>
-                                <span class="ml-3">4.5(Imdb)</span>
-                            </div>
-                            <br />
-                            <div class="d-flex align-items-center" data-animation-in="fadeInUp" data-delay-in="1">
-                                <span class="badge badge-secondary p-2">18+</span>
-                                <li class="ml-3">2 Seasons</li>
-                                <li class="ml-3">Feb 2023</li>
-                            </div>
-                            <p data-animation-in="fadeInUp" data-delay-in="0.7">Lorem Ipsum is simply dummy text of the
-                                printing and typesetting industry. Lorem Ipsum has been the industry's standard
-                                dummy text ever since the 1500s.
-                            </p>
-                            <div class="d-flex align-items-center r-mb-23" data-animation-in="fadeInUp"
-                                data-delay-in="1">
-                                <a href="movie-details.html" class="btn btn-hover"><i class="fa fa-play mr-2"
-                                        aria-hidden="true"></i>Play Now</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="trailor-video">
-                        <a href="video/trailer.mp4" class="video-open playbtn">
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="80px" height="80px"
-                                viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7" xml:space="preserve">
-                                <polygon class='triangle' fill="none" stroke-width="7" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-miterlimit="10"
-                                    points="73.5,62.5 148.5,105.8 73.5,149.1 " />
-                                <circle class='circle' fill="none" stroke-width="7" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8"
-                                    r="103.3" />
-                            </svg>
-                            <span class="w-trailor">Watch Trailer</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="slide slick-bg s-bg-3">
-            <div class="container-fluid position-relative h-100">
-                <div class="slider-inner h-100">
-                    <div class="row align-items-center  h-100">
-                        <div class="col-xl-6 col-lg-12 col-md-12">
-                            <!-- <a href="javascript:void(0);">
-                              <div class="channel-logo" data-animation-in="fadeInLeft">
-                                 <img src="https://localhost/flicknexs/public/themes/theme6/assets/images/logo.png" class="c-logo" alt="streamit">
-                              </div>
-                           </a> -->
-                            <h1 class="slider-text big-title title text-uppercase" data-animation-in="fadeInLeft">the
-                                army</h1>
-                            <div class="d-flex align-items-center" data-animation-in="fadeInUp" data-delay-in="1">
-                                <span class="badge  p-2"><img
-                                        src="https://localhost/flicknexs/public/themes/theme6/assets/images/Group 3 (1).png"></span>
-                                <span class="ml-3">4.5(Imdb)</span>
-                            </div>
-                            <br />
-                            <div class="d-flex align-items-center" data-animation-in="fadeInUp" data-delay-in="1">
-                                <span class="badge badge-secondary p-2">18+</span>
-                                <li class="ml-3">2 Seasons</li>
-                                <li class="ml-3">Feb 2023</li>
-                            </div>
-                            <p data-animation-in="fadeInUp" data-delay-in="0.7">Lorem Ipsum is simply dummy text of
-                                the printing and typesetting industry. Lorem Ipsum has been the industry's standard
-                                dummy text ever since the 1500s.
-                            </p>
-                            <div class="d-flex align-items-center r-mb-23" data-animation-in="fadeInUp"
-                                data-delay-in="1">
-                                <a href="movie-details.html" class="btn btn-hover"><i class="fa fa-play mr-2"
-                                        aria-hidden="true"></i>Play Now</a>
-                                <!-- <a href="movie-details.html" class="btn btn-link">More details</a> -->
-                            </div>
-                        </div>
-                    </div>
-                    <div class="trailor-video">
-                        <a href="video/trailer.mp4" class="video-open playbtn">
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-                                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="80px"
-                                height="80px" viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7"
-                                xml:space="preserve">
-                                <polygon class='triangle' fill="none" stroke-width="7" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-miterlimit="10"
-                                    points="73.5,62.5 148.5,105.8 73.5,149.1 " />
-                                <circle class='circle' fill="none" stroke-width="7" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-miterlimit="10" cx="106.8" cy="106.8"
-                                    r="103.3" />
-                            </svg>
-                            <span class="w-trailor">Watch Trailer</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" width="44px" height="44px" id="circle"
-            fill="none" stroke="currentColor">
-            <circle r="20" cy="22" cx="22" id="test"></circle>
-        </symbol>
-    </svg>
-</section>
+    </section>
+@endif
 
 <div class="main-content">
     @if (!empty($lang_videos) && $lang_videos->isNotEmpty())
