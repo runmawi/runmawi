2e3bf898dc711421d00ff10cb413bfa2262e89e1
Manikandan <manikandan@webnexs.in>
2023-10-20 11:13:49 +0530

    User Import

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -25,6 +25,7 @@ class UsersImport implements ToModel, WithHeadingRow
     */
     public function model(array $row)
     {
+
         $email_exist = User::where('email',$row['e_mail'])->first();
 
         if( empty($email_exist) &&  $row['e_mail'] != null && $row['role'] ){
@@ -34,6 +35,10 @@ class UsersImport implements ToModel, WithHeadingRow
                 'email'     => $row['e_mail'],
                 'password'  => Hash::make($row['e_mail']),
                 'active'    => $row['active_status'] ,
+                'mobile'    => $row['phone_number'] ,
+                'city'      => $row['city'] ,
+                'country'   => $row['country'] ,
+                'countryname' => $row['country']
             );
     
             if( $row['role'] == "subscriber" ){
@@ -64,18 +69,18 @@ class UsersImport implements ToModel, WithHeadingRow
                     'link'          =>  URL::to('password/reset'),
                 );
 
-                \Mail::send('emails.Import_users', $Email_data , function($message) use ($row,$heading) {
+                // \Mail::send('emails.Import_users', $Email_data , function($message) use ($row,$heading) {
 
-                    $message->from( AdminMail(),GetWebsiteName() );
-                    $message->to( $row['e_mail'] , $row['e_mail'] )->subject( $heading );
+                //     $message->from( AdminMail(),GetWebsiteName() );
+                //     $message->to( $row['e_mail'] , $row['e_mail'] )->subject( $heading );
                     
-                });
+                // });
 
-                $email_log      = 'Mail Sent Successfully from Welcome E-Mail - Import users';
-                $email_template = "41";
-                $user_id        = Auth::user()->id;
+                // $email_log      = 'Mail Sent Successfully from Welcome E-Mail - Import users';
+                // $email_template = "41";
+                // $user_id        = Auth::user()->id;
     
-                Email_sent_log( $user_id,$email_log,$email_template );
+                // Email_sent_log( $user_id,$email_log,$email_template );
     
             } 
             catch (\Throwable $th) {
