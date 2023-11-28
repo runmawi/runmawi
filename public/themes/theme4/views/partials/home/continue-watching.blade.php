7e800f51ccee05652c8f8568d4aed3bf8c81c7dd
manivel <manivel@webnexs.in>
2023-11-28 11:18:46 +0530

    font style changes and continue watching

--- public/themes/theme4/views/partials/home/continue-watching.blade.php
+++ public/themes/theme4/views/partials/home/continue-watching.blade.php
@@ -10,45 +10,53 @@
                         <h4 class="main-title"><a href="#">{{ ucwords('continue watching') }}</a></h4>
                     </div>
 
-                    <div class="favorites-contens">
-                        <ul class="favorites-slider list-inline  row p-0 mb-0">
+                    <div class="trending-contens">
+                        <ul id="trending-slider-nav" class="latest-videos-slider-nav list-inline p-0 mb-0 row align-items-center">
                             @foreach ($data as $key => $video_details)
-                                <li class="slide-item">
-                                    <a href="{{ URL::to('category/videos/'.$video_details->slug ) }}">
-                                        <div class="block-images position-relative">
-                                            <div class="img-box">
-                                                <img src="{{ $video_details->image ?  URL::to('public/uploads/images/'.$video_details->image) : default_vertical_image_url() }}" class="img-fluid" alt="">
-                                            </div>
-                                            <div class="block-description">
-                                                <h6> {{ strlen($video_details->title) > 17 ? substr($video_details->title, 0, 18) . '...' : $video_details->title }}
-                                                </h6>
-                                                <div class="movie-time d-flex align-items-center my-2">
-
-                                                    <div class="badge badge-secondary p-1 mr-2">
-                                                        {{ optional($video_details)->age_restrict.'+' }}
-                                                    </div>
-
-                                                    <span class="text-white">
-                                                        {{ $video_details->duration != null ? gmdate('H:i:s', $video_details->duration) : null }}
-                                                    </span>
-                                                </div>
-
-                                                <div class="hover-buttons">
-                                                    <span class="btn btn-hover">
-                                                        <i class="fa fa-play mr-1" aria-hidden="true"></i>
-                                                        Play Now
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <div class="block-social-info">
-                                                <ul class="list-inline p-0 m-0 music-play-lists">
-                                                    {{-- <li><span><i class="ri-volume-mute-fill"></i></span></li> --}}
-                                                    <li><span><i class="ri-heart-fill"></i></span></li>
-                                                    <li><span><i class="ri-add-line"></i></span></li>
-                                                </ul>
-                                            </div>
+                                <li>
+                                    <a href="javascript:void(0);">
+                                        <div class="movie-slick position-relative">
+                                            <img src="{{ $video_details->image ?  URL::to('public/uploads/images/'.$video_details->image) : default_vertical_image_url() }}" class="img-fluid" >
                                         </div>
                                     </a>
+                                </li>
+                            @endforeach
+                        </ul>
+
+                        <ul id="trending-slider latest-videos-slider" class="list-inline p-0 m-0 align-items-center latest-videos-slider">
+                            @foreach ($data as $key => $video_details )
+                                <li>
+                                    <div class="tranding-block position-relative" style="background-image: url({{ $video_details->player_image ?  URL::to('public/uploads/images/'.$video_details->player_image) : default_horizontal_image_url() }});">
+                                        <button class="home-page-close-button">×</button>
+
+                                        <div class="trending-custom-tab">
+                                            <div class="trending-content">
+                                                <div id="" class="overview-tab tab-pane fade active show">
+                                                    <div class="trending-info align-items-center w-100 animated fadeInUp">
+
+                                                        <h1 class="trending-text big-title text-uppercase">{{ optional($video_details)->title }}</h1>
+
+                                                        @if ( $video_details->year != null && $video_details->year != 0)
+                                                            <div class="d-flex align-items-center text-white text-detail">
+                                                                <span class="trending">{{ ($video_details->year != null && $video_details->year != 0) ? $video_details->year : null   }}</span>
+                                                            </div>
+                                                        @endif
+
+                                                        @if (optional($video_details)->description)
+                                                            <p class="trending-dec">{!! html_entity_decode( optional($video_details)->description) !!}</p>
+                                                        @endif
+
+                                                        <div class="p-btns">
+                                                            <div class="d-flex align-items-center p-0">
+                                                                <a href="{{ URL::to('category/videos/'.$video_details->slug) }}" class="btn btn-hover mr-2" tabindex="0"><i class="fa fa-play mr-2" aria-hidden="true"></i> Play Now </a>
+                                                                <a href="#" class="btn btn-hover mr-2" tabindex="0"><i class="fa fa-play mr-2" aria-hidden="true"></i> More Info </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </li>
                             @endforeach
                         </ul>
@@ -58,3 +66,61 @@
         </div>
     </section>
 @endif
+
+
+
+<script>
+    
+    $( window ).on("load", function() {
+        $('.latest-videos-slider').hide();
+    });
+
+    $(document).ready(function() {
+
+        $('.latest-videos-slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            fade: true,
+            draggable: false,
+            asNavFor: '.latest-videos-slider-nav',
+        });
+
+        $('.latest-videos-slider-nav').slick({
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            asNavFor: '.latest-videos-slider',
+            dots: false,
+            arrows: true,
+            nextArrow: '<a href="#" class="slick-arrow slick-next"></a>',
+            prevArrow: '<a href="#" class="slick-arrow slick-prev"></a>',
+            infinite: false,
+            focusOnSelect: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    },
+                },
+            ],
+        });
+
+        $('.latest-videos-slider-nav').on('click', function() {
+            $( ".home-page-close-button" ).trigger( "click" );
+            $('.latest-videos-slider').show();
+        });
+
+        $('body').on('click', '.home-page-close-button', function() {
+            $('.latest-videos-slider').hide();
+        });
+    });
+</script>
