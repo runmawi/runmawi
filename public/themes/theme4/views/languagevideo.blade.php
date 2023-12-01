6b32e2f1a0f9faaa8c0e7904d190ee055125a86c
manivel <manivel@webnexs.in>
2023-12-01 19:19:16 +0530

    theme4 dropdown image design

--- public/themes/theme4/views/languagevideo.blade.php
+++ public/themes/theme4/views/languagevideo.blade.php
@@ -32,15 +32,16 @@
                         <ul id="trending-slider latest-videos-slider" class="list-inline p-0 m-0 align-items-center latest-videos-slider">
                             @foreach ($lang_videos as $key => $latest_video )
                                 <li>
-                                    <div class="tranding-block position-relative trending-thumbnail-image" style="background-image: url({{ $latest_video->player_image ?  URL::to('public/uploads/images/'.$latest_video->player_image) : default_horizontal_image_url() }}); background-repeat: no-repeat;background-size: cover;">
-                                        <button class="close_btn">×</button>
+                                    <div class="tranding-block position-relative trending-thumbnail-image" >
+                                        <button class="drp-close">×</button>
 
                                         <div class="trending-custom-tab">
                                             <div class="trending-content">
                                                 <div id="" class="overview-tab tab-pane fade active show">
                                                     <div class="trending-info align-items-center w-100 animated fadeInUp">
 
-                                                        <h2 class="trending-text big-title text-uppercase">{{ optional($latest_video)->title }}</h2>
+                                                    <div class="caption">
+                                                            <h2 class="caption-h2">{{ optional($latest_video)->title }}</h2>
 
                                                         {{-- @if ( $latest_video->year != null && $latest_video->year != 0)
                                                             <div class="d-flex align-items-center text-white text-detail">
@@ -58,6 +59,12 @@
                                                                 <a href="#" class="btn btn-hover button-groups mr-2" tabindex="0"><i class="fa fa-play mr-2" aria-hidden="true"></i> More Info </a>
                                                             </div>
                                                         </div>
+                                                        </div>
+
+                                                        <div class="dropdown_thumbnail">
+                                                            <img  src="{{  $latest_video->player_image ?  URL::to('public/uploads/images/'.$latest_video->player_image) : default_horizontal_image_url() }}" alt="">
+                                                        </div>
+                                                    </div>
                                                     </div>
                                                 </div>
                                             </div>
@@ -86,7 +93,7 @@
     $(document).ready(function() {
 
         $('.latest-videos-slider').slick({
-            slidesToShow: 6,
+            slidesToShow: 1,
             slidesToScroll: 1,
             arrows: true,
             fade: true,
@@ -130,11 +137,11 @@
         });
 
         $('.latest-videos-slider-nav').on('click', function() {
-            $( ".close_btn" ).trigger( "click" );
+            $( ".drp-close" ).trigger( "click" );
             $('.latest-videos-slider').show();
         });
 
-        $('body').on('click', '.close_btn', function() {
+        $('body').on('click', '.drp-close', function() {
             $('.latest-videos-slider').hide();
         });
     });
