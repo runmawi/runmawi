00c2c42e5f20b28c9752c5f9a145abc7ddc78b7d
Manikandan <manikandan@webnexs.in>
2023-12-11 18:02:05 +0530

    Series

--- resources/views/admin/series/index.blade.php
+++ resources/views/admin/series/index.blade.php
@@ -1,4 +1,5 @@
 @extends('admin.master')
+
 <style type="text/css">
 
     .has-switch .switch-on label {
@@ -47,7 +48,6 @@
     <div class="admin-section-title">
         <div id="content-page" class="content-page">
             <div class="container-fluid">
-
                 <div class="d-flex">
                     <a class="black" style="background:#fafafa!important;color: #006AFF!important;" href="{{ URL::to('admin/series-list') }}"> TV Shows List</a>
                     <a class="black" href="{{ URL::to('admin/series/create') }}"> Add TV Shows Series</a>
@@ -55,169 +55,161 @@
 
                 <div class="iq-card">
                     <div class="row align-items-center p-2">
-                        <div class="col-md-5">
-                            <h4><i class="entypo-movie"></i> TV Shows</h4>
-                        </div>
-                        <div class="col-md-5 mt-3">
-                            {{-- <form method="get" role="form" class="search-form-full">
-                                <div class="form-group"> <input type="text" class="form-control" value="" name="s" id="search-input" placeholder="Search..."> <i class="entypo-search"></i> </div>
-                            </form> --}}
-                        </div>
-
-                        <div class="col-md-2 mb-2">
-                            <a href="{{ URL::to('admin/series/create') }}" class="btn btn-primary mt-2"><i class="fa fa-plus-circle"></i> Add New</a>
-                        </div>
+                        <div class="col-md-10"> <h4><i class="entypo-movie"></i> TV Shows</h4></div>
+                        <div class="col-md-2 mb-2"><a href="{{ URL::to('admin/series/create') }}" class="btn btn-primary mt-2"><i class="fa fa-plus-circle"></i> Add New</a></div>
                     </div>
 
                     <div class="clear"></div>
 
-                    <div class="gallery-env">
-                        <div class="row mt-3 p-2">
-                            <table class="table text-center iq-card" id="Series-Table">
-                                <tr class="table-header r1">
-                                    <th><label>S.No</label></th>
-                                    <th><label>Image</label></th>
-                                    <th><label>Title</label></th>
+					<div class="row">
+						<div class="col-md-12">
+							<table class="table text-center" id="sitemeta_table" style="width:100%">
+								<thead>
+									<tr class="r1">
+										<th><label>S.No</label></th>
+										<th><label>Image</label></th>
+										<th><label>Title</label></th>
+	
+										@if (Series_Networks_Status() == 1)
+											<th><label>Networks</label></th>
+										@endif
+	
+										<th><label>Slider</label></th>
+										<th><label>Operation</label></th>
+									</tr>
+								</thead>
 
-                                    @if (Series_Networks_Status() == 1)
-                                        <th><label>Networks</label></th>
-                                    @endif
+								<tbody>
+									@foreach ($series as $key => $series_value)
+										<tr>
+											<td>{{ $key + 1 }}</td>
+											<td><img src="{{ URL::to('public/uploads/images/' . $series_value->image) }}" width="100"></td>
+											<td valign="bottom"> <p>{{ $series_value->title }}</p> </td>
 
-                                    <th><label>Slider</label></th>
-                                    <th><label>Operation</label></th>
-                                    @foreach ($series as $key => $series_value)
-                                <tr>
+											@if (Series_Networks_Status() == 1)
+												<td valign="bottom">
+													@php
+														if (!empty($series_value->network_id) && !is_null($series_value->network_id)):
+															$SeriesNetwork = App\SeriesNetwork::WhereIn('id', json_decode($series_value->network_id))->pluck('name');
+															$Series_Network_name = implode(', ', $SeriesNetwork->toArray());
+														else:
+															$Series_Network_name = '-';
+														endif;
+													@endphp
+													{{ $Series_Network_name }}
+													</p>
+												</td>
+											@endif
 
-                                    <td>{{ $key + 1 }}</td>
-                                    <td><img src="{{ URL::to('public/uploads/images/' . $series_value->image) }}" width="100"></td>
-                                    <td valign="bottom"> <p>{{ $series_value->title }}</p> </td>
+											<td valign="bottom">
+												<div class="mt-1">
+													<label class="switch">
+														<input name="video_status" class="video_status" id="{{ 'video_' . $series_value->id }}" type="checkbox"
+															@if ($series_value->banner == '1') checked @endif
+																data-video-id={{ $series_value->id }} data-type="video" onchange="update_series_banner(this)">
+														<span class="slider round"></span>
+													</label>
+												</div>
+											</td>
+											<td>
+												<div class=" align-items-center">
+													<a href="{{ URL::to('play_series/'.$series_value->slug )   }}"
+														class="iq-bg-warning"><img class="ply" src="{{ URL::to('assets/img/icon/view.svg') }}">
+														<!--Visit Site-->
+													</a>
+													<a href="{{ URL::to('admin/series/edit/'.$series_value->id )  }}"
+														class="iq-bg-success ml-2"><img class="ply" src="{{ URL::to('assets/img/icon/edit.svg') }} ">
+														<!--Edit--></a>
+													<a href="{{ URL::to('admin/series/delete/'. $series_value->id ) }}"
+														class="iq-bg-danger ml-2" onclick="return confirm('Are you sure?')"><img
+															class="ply" src="{{ URL::to('assets/img/icon/delete.svg') }}"><i></i> </a>
+												</div>
+											</td>
+										</tr>
+									@endforeach
+								</tbody>
+					  	 	</table>
+						</div>
+            		</div>
+        		</div>
+    		</div>
 
-                                    @if (Series_Networks_Status() == 1)
-                                        <td valign="bottom">
-                                            @php
-                                                if (!empty($series_value->network_id) && !is_null($series_value->network_id)):
-                                                    $SeriesNetwork = App\SeriesNetwork::WhereIn('id', json_decode($series_value->network_id))->pluck('name');
-                                                    $Series_Network_name = implode(', ', $SeriesNetwork->toArray());
-                                                else:
-                                                    $Series_Network_name = '-';
-                                                endif;
-                                            @endphp
-                                            {{ $Series_Network_name }}
-                                            </p>
-                                        </td>
-                                    @endif
-
-                                    <td valign="bottom">
-                                        <div class="mt-1">
-                                            <label class="switch">
-                                                <input name="video_status" class="video_status" id="{{ 'video_' . $series_value->id }}" type="checkbox"
-                                                    @if ($series_value->banner == '1') checked @endif
-														data-video-id={{ $series_value->id }} data-type="video" onchange="update_series_banner(this)">
-                                                <span class="slider round"></span>
-                                            </label>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div class=" align-items-center">
-                                            <a href="{{ URL::to('play_series/'.$series_value->slug )   }}"
-                                                class="iq-bg-warning"><img class="ply" src="{{ URL::to('assets/img/icon/view.svg') }}">
-                                                <!--Visit Site-->
-											</a>
-                                            <a href="{{ URL::to('admin/series/edit/'.$series_value->id )  }}"
-                                                class="iq-bg-success ml-2"><img class="ply" src="{{ URL::to('assets/img/icon/edit.svg') }} ">
-                                                <!--Edit--></a>
-                                            <a href="{{ URL::to('admin/series/delete/'. $series_value->id ) }}"
-                                                class="iq-bg-danger ml-2" onclick="return confirm('Are you sure?')"><img
-                                                    class="ply" src="{{ URL::to('assets/img/icon/delete.svg') }}"><i></i> </a>
-                                        </div>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </table>
-                            <div class="clear"></div>
-                            <div class="pagination-outter"><?= $series->appends(Request::only('s'))->render() ?></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    @section('javascript')
+    	@section('javascript')
       
+			<script>
 
-        <script>
+				$(document).ready(function() {
 
-            $(document).ready(function() {
+					$('#sitemeta_table').DataTable();
 
-                var delete_link = '';
+					var delete_link = '';
 
-                $('.delete').click(function(e) {
-                    e.preventDefault();
-                    delete_link = $(this).attr('href');
-                    swal({
-                        title: "Are you sure?",
-                        text: "Do you want to permanantly delete this series?",
-                        type: "warning",
-                        showCancelButton: true,
-                        confirmButtonColor: "#DD6B55",
-                        confirmButtonText: "Yes, delete it!",
-                        closeOnConfirm: false
-                    }, function() {
-                        window.location = delete_link
-                    });
-                    return false;
-                });
-            });
-        </script>
+					$('.delete').click(function(e) {
+						e.preventDefault();
+						delete_link = $(this).attr('href');
+						swal({
+							title: "Are you sure?",
+							text: "Do you want to permanantly delete this series?",
+							type: "warning",
+							showCancelButton: true,
+							confirmButtonColor: "#DD6B55",
+							confirmButtonText: "Yes, delete it!",
+							closeOnConfirm: false
+						}, function() {
+							window.location = delete_link
+						});
+						return false;
+					});
+				});
+		
+				function update_series_banner(ele) {
 
-        <script>
-            function update_series_banner(ele) {
+					var video_id = $(ele).attr('data-video-id');
+					var status = '#video_' + video_id;
+					var video_Status = $(status).prop("checked");
 
-                var video_id = $(ele).attr('data-video-id');
-                var status = '#video_' + video_id;
-                var video_Status = $(status).prop("checked");
+					if (video_Status == true) {
+						var banner_status = '1';
+						var check = confirm("Are you sure you want to active this slider?");
 
-                if (video_Status == true) {
-                    var banner_status = '1';
-                    var check = confirm("Are you sure you want to active this slider?");
-
-                } else {
-                    var banner_status = '0';
-                    var check = confirm("Are you sure you want to remove this slider?");
-                }
+					} else {
+						var banner_status = '0';
+						var check = confirm("Are you sure you want to remove this slider?");
+					}
 
 
-                if (check == true) {
+					if (check == true) {
 
-                    $.ajax({
-                        type: "POST",
-                        dataType: "json",
-                        url: "{{ url('admin/series_slider_update') }}",
-                        data: {
-                            _token: "{{ csrf_token() }}",
-                            video_id: video_id,
-                            banner_status: banner_status,
-                        },
-                        success: function(data) {
-                            if (data.message == 'true') {
-                                //  location.reload();
-                            } else if (data.message == 'false') {
-                                swal.fire({
-                                    title: 'Oops',
-                                    text: 'Something went wrong!',
-                                    allowOutsideClick: false,
-                                    icon: 'error',
-                                    title: 'Oops...',
-                                }).then(function() {
-                                    location.href = '{{ URL::to('admin/ActiveSlider') }}';
-                                });
-                            }
-                        },
-                    });
-                } else if (check == false) {
-                    $(status).prop('checked', true);
+						$.ajax({
+							type: "POST",
+							dataType: "json",
+							url: "{{ url('admin/series_slider_update') }}",
+							data: {
+								_token: "{{ csrf_token() }}",
+								video_id: video_id,
+								banner_status: banner_status,
+							},
+							success: function(data) {
+								if (data.message == 'true') {
+									//  location.reload();
+								} else if (data.message == 'false') {
+									swal.fire({
+										title: 'Oops',
+										text: 'Something went wrong!',
+										allowOutsideClick: false,
+										icon: 'error',
+										title: 'Oops...',
+									}).then(function() {
+										location.href = '{{ URL::to('admin/ActiveSlider') }}';
+									});
+								}
+							},
+						});
+					} else if (check == false) {
+						$(status).prop('checked', true);
 
-                }
-            }
-        </script>
-    @stop
+					}
+				}
+			</script>
+   		@stop
 @stop
