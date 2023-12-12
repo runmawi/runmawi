d740353ad0b7274ac65fe95bc6e6dd5991be1f4e
Manikandan <manikandan@webnexs.in>
2023-12-12 13:53:01 +0530

    AdminNetworkController

--- app/Http/Controllers/AdminNetworkController.php
+++ app/Http/Controllers/AdminNetworkController.php
@@ -162,18 +162,19 @@ class AdminNetworkController extends Controller
     
                 if(compress_image_enable() == 1){
     
-                    $filename   = 'series-Network-'.time().'.'.compress_image_format();
+                    $filename   = 'series-Network-banner-'.time().'.'.compress_image_format();
                     Image::make($file)->save(base_path().'/public/uploads/seriesNetwork/'.$filename ,compress_image_resolution() );
     
                 }else{
     
-                    $filename   = 'series-Network-'.time().'.'.$file->getClientOriginalExtension();
+                    $filename   = 'series-Network-banner-'.time().'.'.$file->getClientOriginalExtension();
                     Image::make($file)->save(base_path().'/public/uploads/seriesNetwork/'.$filename );
                 }
     
                 $inputs +=  ['banner_image' => $filename ];
             }
     
+            dd($inputs);
             $SeriesNetwork = SeriesNetwork::find($id)->update($inputs);
     
             return back()->with('message', 'TV-Shows Network Updated successfully.');
