46a1303605afef3bd35aee807b267f7b63add824
manivel <manivel@webnexs.in>
2024-01-04 16:17:14 +0530

    theme1 UI alignments fixed

--- public/themes/theme1/views/partials/home/SeriesCategory.php
+++ public/themes/theme1/views/partials/home/SeriesCategory.php
@@ -23,13 +23,11 @@ ol.breadcrumb {
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12 page-height">
-                <div class="iq-main-header align-items-center justify-content-between">
-                    <h4 class="movie-title"> <?php echo @$CategorySeries->name ?></h4>
-                </div>
+                
 
                 <!-- BREADCRUMBS -->
                 <div class="row d-flex">
-                    <div class="nav nav-tabs nav-fill container-fluid nav-div" id="nav-tab" role="tablist">
+                    <div class="nav-fill container-fluid nav-div" id="nav-tab" role="tablist">
                         <div class="bc-icons-2">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a class="black-text"
@@ -50,8 +48,12 @@ ol.breadcrumb {
                     </div>
                 </div>
 
+                <div class="iq-main-header align-items-center justify-content-between">
+                    <h4 class="movie-title"> <?php echo @$CategorySeries->name ?></h4>
+                </div>
+
                 <div class="favorites-contens">
-                    <ul class="category-page list-inline row p-0 mb-0">
+                    <ul class="category-page seriescategf list-inline row p-0 mb-0">
                         <?php if(isset($SeriesGenre)) {
                         foreach($SeriesGenre as $Series_Genre){ ?>
                         <li class="slide-item col-sm-2 col-md-2 col-xs-12">
@@ -63,14 +65,14 @@ ol.breadcrumb {
                                     </div>
 
                                     <div class="block-description">
-                                        <a href="<?php echo URL::to('/play_series/').'/'.$Series_Genre->slug  ?>">
+                                        <!-- <a href="<?php echo URL::to('/play_series/').'/'.$Series_Genre->slug  ?>">
                                             <h6><?php  echo (strlen(@$Series_Genre->title) > 17) ? substr(@$Series_Genre->title,0,18).'...' : @$Series_Genre->title; ?>
                                             </h6>
                                         </a>
                                         <div class="hover-buttons">
                                             <div>
                                             </div>
-                                        </div>
+                                        </div> -->
                                         <div>
                                             <a class="text-white"
                                                 href="<?php echo URL::to('/play_series'.'/'.$Series_Genre->slug  ) ?> ">
@@ -90,6 +92,35 @@ ol.breadcrumb {
         </div>
     </div>
 </section>
+
+<script>
+   $(document).ready(function(){
+      $('.seriescategf').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        speed: 300,
+        infinite: true,
+        autoplaySpeed: 5000,
+        autoplay: true,
+        responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 534,
+        settings: {
+          slidesToShow: 2,
+        }
+      }
+    ]
+      });
+    });
+</script>
 
 
 <?php
