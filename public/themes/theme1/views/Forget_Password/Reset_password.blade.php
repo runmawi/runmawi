0686e857eb7594317d32f553a6d0401c4b9bbfd6
Sanjai Kumar M <sanjai@webnexs.in>
2024-01-16 14:31:09 +0530

    changes

--- public/themes/theme1/views/Forget_Password/Reset_password.blade.php
+++ public/themes/theme1/views/Forget_Password/Reset_password.blade.php
@@ -2,6 +2,49 @@
     $settings = App\Setting::first();
     $theme_mode = App\SiteTheme::pluck('theme_mode')->first();
     $theme = App\SiteTheme::first();
+
+    
+    $translate_checkout = App\SiteTheme::pluck('translate_checkout')->first();
+
+    @$translate_language = App\Setting::pluck('translate_language')->first();
+
+        if(Auth::guest()){
+            $geoip = new \Victorybiz\GeoIPLocation\GeoIPLocation();
+            $userIp = $geoip->getip();
+            $UserTranslation = App\UserTranslation::where('ip_address',$userIp)->first();
+
+            if(!empty($UserTranslation)){
+                $translate_language = $UserTranslation->translate_language;
+            }else{
+                $translate_language = 'en';
+            }
+        }else if(!Auth::guest()){
+
+            $subuser_id=Session::get('subuser_id');
+            if($subuser_id != ''){
+                $Subuserranslation = App\UserTranslation::where('multiuser_id',$subuser_id)->first();
+                if(!empty($Subuserranslation)){
+                    $translate_language = $Subuserranslation->translate_language;
+                }else{
+                    $translate_language = 'en';
+                }
+            }else if(Auth::user()->id != ''){
+                $UserTranslation = App\UserTranslation::where('user_id',Auth::user()->id)->first();
+                if(!empty($UserTranslation)){
+                    $translate_language = $UserTranslation->translate_language;
+                }else{
+                    $translate_language = 'en';
+                }
+            }else{
+                $translate_language = 'en';
+            }
+
+        }else{
+            $translate_language = 'en';
+        }
+
+    \App::setLocale(@$translate_language);
+
 ?>
 
 <html>
@@ -142,8 +185,7 @@
                                             </span>
                                         @enderror
 
-                                        <p class="reset-help text-center">{{ __('We will send you an email with instructions on
-                                            how to reset your password') }}.</p>
+                                        <p class="reset-help text-center">{{ __('We will send you an email with instructions on how to reset your password') }}.</p>
 
                                         <button type="submit" class="btn btn-primary">
                                             {{ __('Send Password Reset Link') }}
