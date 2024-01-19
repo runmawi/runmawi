045ad123cfb791062c334a07c9b72da985e8969d
Manikandan <manikandan@webnexs.in>
2024-01-19 23:24:13 +0530

    Search Option

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2708,38 +2708,29 @@ class HomeController extends Controller
         if ($request->ajax())
         {
 
-            // $videos = Video::Select('videos.*','categoryvideos.category_id','categoryvideos.video_id','video_categories.name as category_name')
-            //                ->Join('categoryvideos','categoryvideos.video_id','=','videos.id')
-            //                ->Join('video_categories','video_categories.id','=','categoryvideos.category_id')
-            //                ->orwhere('videos.search_tags', 'LIKE', '%' . $request->country . '%')
-            //                ->orwhere('videos.title', 'LIKE', '%' . $request->country . '%')
-            //                ->orwhere('video_categories.name', 'LIKE', '%' . $request->country . '%')
-            //                ->where('active', '=', '1')
-            //                ->where('status', '=', '1')
-            //                ->where('draft', '=', '1')
-            //                ->orderBy('created_at', 'desc')
-            //                ->groupBy('videos.id')
-            //                ->limit('10');
+            $videos = Video::Select('videos.*','categoryvideos.category_id','categoryvideos.video_id','video_categories.name as category_name')
+                           ->leftJoin('categoryvideos','categoryvideos.video_id','=','videos.id')
+                           ->leftJoin('video_categories','video_categories.id','=','categoryvideos.category_id')
+                           ->orwhere('videos.search_tags', 'LIKE', '%' . $request->country . '%')
+                           ->orwhere('videos.title', 'LIKE', '%' . $request->country . '%')
+                           ->orwhere('video_categories.name', 'LIKE', '%' . $request->country . '%')
+                           ->where('active', '=', '1')
+                           ->where('status', '=', '1')
+                           ->where('draft', '=', '1')
+                           ->orderBy('created_at', 'desc')
+                           ->groupBy('videos.id')
+                           ->limit('10');
 
-                            $videos = Video::Select('videos.*')
-                            ->where('videos.search_tags', 'LIKE', '%' . $request->country . '%')
-                            ->orwhere('videos.title', 'LIKE', '%' . $request->country . '%')
-                            ->where('active', '=', '1')
-                            ->where('status', '=', '1')
-                            ->where('draft', '=', '1')
-                            ->orderBy('created_at', 'desc')
-                            ->groupBy('videos.id')
-                            ->limit('10');
 
                            if(Geofencing() !=null && Geofencing()->geofencing == 'ON'){
                                 $videos = $videos  ->whereNotIn('videos.id',Block_videos());
                             }
 
-                           $videos = $videos->get();
+            $videos = $videos->get();
 
             $livestream = LiveStream::Select('live_streams.*','livecategories.live_id','live_categories.name')
-                            ->Join('livecategories','livecategories.live_id','=','live_streams.id')
-                            ->Join('live_categories','live_categories.id','=','livecategories.category_id')
+                            ->leftJoin('livecategories','livecategories.live_id','=','live_streams.id')
+                            ->leftJoin('live_categories','live_categories.id','=','livecategories.category_id')
                             ->orwhere('live_streams.search_tags', 'LIKE', '%' . $request->country . '%')
                             ->orwhere('live_streams.title', 'LIKE', '%' . $request->country . '%')
                             ->orwhere('live_categories.name', 'LIKE', '%' . $request->country . '%')           
@@ -2756,25 +2747,25 @@ class HomeController extends Controller
                             ->limit('10')
                             ->get();
 
-            $Episode = Episode::Select('episodes.*','series.id','series_categories.category_id','video_categories.name as Category_name')
-                            ->Join('series','series.id','=','episodes.series_id')
-                            ->Join('series_categories','series_categories.series_id','=','series.id')
-                            ->Join('video_categories','video_categories.id','=','series_categories.category_id')
+            $Episode = Episode::Select('episodes.*','series.id','series_categories.category_id')
+                            ->leftJoin('series','series.id','=','episodes.series_id')
+                            ->leftJoin('series_categories','series_categories.series_id','=','series.id')
+                            ->leftJoin('series_genre','series_genre.id','=','series_categories.category_id')
                             ->orwhere('episodes.search_tags', 'LIKE', '%' . $request->country . '%')
                             ->orwhere('episodes.title', 'LIKE', '%' . $request->country . '%')
-                            ->orwhere('video_categories.name', 'LIKE', '%' . $request->country . '%')           
+                            ->orwhere('series_genre.name', 'LIKE', '%' . $request->country . '%')           
                             ->where('episodes.active', '=', '1')
                             ->where('episodes.status', '=', '1')
                             ->groupBy('episodes.id')
                             ->limit('10')
                             ->get(); 
                             
-            $Series = Series::Select('series.*','series_categories.category_id','video_categories.name as Category_name')
-                            ->Join('series_categories','series_categories.series_id','=','series.id')
-                            ->Join('video_categories','video_categories.id','=','series_categories.category_id')
+            $Series = Series::Select('series.*','series_categories.category_id')
+                            ->leftJoin('series_categories','series_categories.series_id','=','series.id')
+                            ->leftJoin('series_genre','series_genre.id','=','series_categories.category_id')
                             ->orwhere('series.search_tag', 'LIKE', '%' . $request->country . '%')
                             ->orwhere('series.title', 'LIKE', '%' . $request->country . '%')
-                            ->orwhere('video_categories.name', 'LIKE', '%' . $request->country . '%')           
+                            ->orwhere('series_genre.name', 'LIKE', '%' . $request->country . '%')           
                             ->where('series.active', '=', '1')
                             ->groupBy('series.id')
                             ->limit('10')
