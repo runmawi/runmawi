2d777aab67cdf6162f83ee4058d30346dccae8ac
Sanjai Kumar M <sanjai@webnexs.in>
2024-01-29 16:26:58 +0530

    changes

--- public/themes/theme1/views/partials/home/Category_Live.blade.php
+++ public/themes/theme1/views/partials/home/Category_Live.blade.php
@@ -44,7 +44,7 @@ include public_path('themes/theme1/views/header.php');
     </div>
 
     @if (isset($Live_Category) && count($Live_Category) > 0)
-        <h3 class="vid-title text-center mt-4 mb-5">{{ __('Live Stream videos') }}</h3>
+        <h3 class="vid-title text-center mt-4 mb-5">{{ __($category_title) }}</h3>
         <div class="container-fluid"
             style="padding: 0px 40px!important;background: linear-gradient(135.05deg, rgba(136, 136, 136, 0.48) 1.85%, rgba(64, 32, 32, 0.13) 38.53%, rgba(81, 57, 57, 0.12) 97.89%);">
             <div class="row">
