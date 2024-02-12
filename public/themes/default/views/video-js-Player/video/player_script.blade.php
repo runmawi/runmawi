8af249239c42f4f5bf72c245cbd099e77b0bab99
Manikandan <manikandan@webnexs.in>
2024-02-12 15:50:01 +0530

    Flixcave - Mid Sequence

--- public/themes/default/views/video-js-Player/video/player_script.blade.php
+++ public/themes/default/views/video-js-Player/video/player_script.blade.php
@@ -37,6 +37,54 @@
             }
         });
 
+        // Skip Intro & Skip Recap 
+
+        player.on("loadedmetadata", function() {
+
+            const player_duration_Seconds        =  player.duration();
+            const video_skip_intro_seconds       = '<?= $videodetail->video_skip_intro_seconds ?>' ;
+            const video_intro_start_time_seconds = '<?= $videodetail->video_intro_start_time_seconds ?>' ;
+            const video_intro_end_time_seconds   = '<?= $videodetail->video_intro_end_time_seconds ?>' ;
+
+            const video_skip_recap_seconds       = '<?= $videodetail->video_skip_recap_seconds ?>' ;
+            const video_recap_start_time_seconds = '<?= $videodetail->video_recap_start_time_seconds ?>'  ;
+            const video_recap_end_time_seconds   = '<?= $videodetail->video_recap_end_time_seconds ?>'  ;
+
+            if( player_duration_Seconds != "Infinity" && !!video_skip_intro_seconds && !!video_intro_start_time_seconds && !!video_intro_end_time_seconds ){
+                player.skipButton({
+                    text: "Skip Intro",
+                    from: video_intro_start_time_seconds,
+                    to: video_skip_intro_seconds,
+                    position: "bottom-right",
+                    offsetH: 46,
+                    offsetV: 96
+                });
+
+                player.on("timeupdate", function() {
+                    if(video_intro_end_time_seconds <= player.currentTime() ){
+                        $(".vjs-fg-skip-button").removeAttr("style").hide();
+                    }
+                });
+            }
+
+            if(  player_duration_Seconds != "Infinity" &&  !!video_skip_recap_seconds && !!video_recap_start_time_seconds && !!video_recap_end_time_seconds ){
+                player.skipButton({
+                    text: "Skip Recap",
+                    from: video_recap_start_time_seconds,
+                    to: video_skip_recap_seconds,
+                    position: "bottom-right",
+                    offsetH: 46,
+                    offsetV: 96
+                });
+
+                player.on("timeupdate", function() {
+                    if(video_recap_end_time_seconds <= player.currentTime() ){
+                        $(".vjs-fg-skip-button").removeAttr("style").hide();
+                    }
+                });
+            }
+        });
+
         // Ads Marker
 
         player.on("loadedmetadata", function() {
@@ -85,24 +133,27 @@
         });
 
         // Back Button 
-        const Back_button = videojs.dom.createEl('button', {
-            className: '',
-            innerHTML: '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
-            title: 'Back Button',
-        });
+        // const Back_button = videojs.dom.createEl('button', {
+        //     className: '',
+        //     innerHTML: '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+        //     title: 'Back Button',
+        // });
 
-        player.controlBar.el().appendChild(Back_button);
+        // player.controlBar.el().appendChild(Back_button);
 
-        Back_button.addEventListener('click', function() {
-            history.back();
-        });
+        // Back_button.addEventListener('click', function() {
+        //     history.back();
+        // });
 
+        // Hls Quality Selector - M3U8 
 
-        player.hlsQualitySelector({ // Hls Quality Selector - M3U8 
+        player.hlsQualitySelector({ 
             displayCurrentQuality: true,
         });
 
-        var vastTagPreroll  = '<?= $pre_advertisement ?>'; // Advertisement
+        // Advertisement
+
+        var vastTagPreroll  = '<?= $pre_advertisement ?>'; 
         var vastTagPostroll = '<?= $post_advertisement ?>';
 
         var prerollTriggered = false;
@@ -204,60 +255,8 @@
             player.play();
 
         });
-
-        // player.endcard({
-        //     getRelatedContent: getRelatedContent,
-        //     // getNextVid: getNextVid, 
-        //     count: 20
-        // });
-
     });
 
-    // function createRelatedContent(title, url) {
-
-    //     var sdiv = document.createElement('div');
-    //     sdiv.setAttribute('class', 'col-lg-12');
-
-    //     var div = document.createElement('div');
-    //     div.setAttribute('class', 'col-4 col-sm-4 col-md-4 col-lg-4');
-
-    //     var a = document.createElement('a');
-    //     var p = document.createElement('p');
-
-    //     p.innerHTML = title;
-    //     a.href = url;
-    //     a.appendChild(p);
-    //     div.appendChild(a);
-
-    //     return div;
-    // }
-
-    // // Creating related content boxes
-    // var rel_content_1 = createRelatedContent("Video JS Website, For All Your HTML5 Needs.... AND MORE!", "http://www.videojs.com/");
-    // var rel_content_2 = createRelatedContent("This Man Found a LinkBait LinkBait. You Won't Believe What the LinkBait Did Next!", "http://www.youtube.com/watch?v=6k3--GPk-l4");
-
-    // // Asynchronous function to get related content
-    // function getRelatedContent(callback) {
-    //     var list = [rel_content_1, rel_content_2];
-
-    //     setTimeout(function () {
-    //         callback(list);
-    //     }, 0);
-    // }
-
-    // var next_video = document.createElement('div');
-    // var a3 = document.createElement('a');
-    // var p3 = document.createElement('p');
-    // p3.innerHTML = "ABOUT TO GO HERE!!";
-    // a3.href = "http://www.youtube.com/watch?v=KAv500Q6bfA";
-    // a3.appendChild(p3);
-    // next_video.appendChild(a3);
-
-    // function getNextVid(callback) {
-    //     setTimeout(function(){
-    //         callback(next_video);
-    //     }, 0);
-    // }
 </script>
 
 <style>
