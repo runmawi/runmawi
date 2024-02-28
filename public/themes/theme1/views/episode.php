55ca4864afb62431036f71d492b0092c944137f0
manivel <manivel@webnexs.in>
2024-02-28 09:39:47 +0530

    theme1 changes

--- public/themes/theme1/views/episode.php
+++ public/themes/theme1/views/episode.php
@@ -237,9 +237,9 @@ $SeriesSeason = App\SeriesSeason::where('id', $episode->season_id)->first();
     value="<?= $episode->genre_id ?>">
 <br>
 
-    <div class="row-flex">
+    <div class="container-fluid">
         <div class="bc-icons-2">
-                <ol class="breadcrumb">
+                <ol class="breadcrumb p-0">
                     <li class="breadcrumb-item"><a class="black-text"
                             href="<?= route('series.tv-shows') ?>"><?= ucwords (__('Series')) ?></a>
                         <i class="fa fa-angle-double-right mx-2" aria-hidden="true"></i>
@@ -398,8 +398,8 @@ $SeriesSeason = App\SeriesSeason::where('id', $episode->season_id)->first();
                 </ul>
             </div>
 
-
-            <h2 id="tags"><?= __('Tags') ?>:
+            <span class="text-white" style="font-size: 129%;font-weight: 500;">
+                <?= __('Tags') ?>:
                 <?php if(isset($episode->tags)) {
 		foreach($episode->tags as $key => $tag): ?>
 
@@ -408,8 +408,8 @@ $SeriesSeason = App\SeriesSeason::where('id', $episode->season_id)->first();
 
                 <?php endforeach; }
 		?>
-
-            </h2>
+            </span>
+            
         </div>
 
 
@@ -443,7 +443,7 @@ $SeriesSeason = App\SeriesSeason::where('id', $episode->season_id)->first();
     </div>
 </div>
 <div class="clear">
-    <h2 id="tags">
+    <span class="text-white" style="font-size: 129%;font-weight: 500;">
         <?php if(isset($episode->tags)) {
 		foreach($episode->tags as $key => $tag): ?>
 
@@ -452,7 +452,7 @@ $SeriesSeason = App\SeriesSeason::where('id', $episode->season_id)->first();
 
         <?php endforeach; }
 		?>
-    </h2>
+    </span>
 
     <div class="clear"></div>
     <div id="social_share">
