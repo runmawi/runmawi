b0b53d47e3815c59262c2c44e3804b5888ef756c
Manikandan <manikandan@webnexs.in>
2024-03-06 10:58:57 +0530

    API - video details videojs Ads

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -99,6 +99,7 @@ use App\LoggedDevice;
 use Razorpay\Api\Api;
 use App\AdsVideo;
 use App\AdvertisementView;
+use App\Advertisement;
 use App\OrderHomeSetting;
 use App\MobileHomeSetting;
 use App\SiteTheme;
@@ -1624,8 +1625,10 @@ public function verifyandupdatepassword(Request $request)
       $videoid = $request->videoid;
 
       $current_date = date('Y-m-d h:i:s a', time());
+
+      $choose_player = SiteTheme::pluck('choose_player')->first();
   
-      $videodetail = Video::where('id',$videoid)->orderBy('created_at', 'desc')->get()->map(function ($item) use ($request){
+      $videodetail = Video::where('id',$videoid)->orderBy('created_at', 'desc')->get()->map(function ($item) use ($request,$choose_player){
           $item['details']        = strip_tags($item->details);
           $item['description']    = strip_tags($item->description);
           $item['image_url']      = URL::to('public/uploads/images/'.$item->image );
@@ -1659,6 +1662,7 @@ public function verifyandupdatepassword(Request $request)
           if($item->access == 'ppv' && $request->user_id != '' || $item->access == 'subscriber' && $request->user_id != ''){
             $item['rent_url']   = URL::to('category/videos').'/'.$item->slug;
           }
+
           // Videos URL 
 
           switch (true) {
@@ -1695,6 +1699,107 @@ public function verifyandupdatepassword(Request $request)
               $item['videos_url']    = null ;
               break;
           }
+         
+          // video js Player Ads
+
+          $current_time = Carbon::now()->format('H:i:s');
+          $advertisement_plays_24hrs = Setting::pluck('ads_play_unlimited_period')->first();
+
+          $video_js_mid_advertisement_sequence_time = $item->video_js_mid_advertisement_sequence_time != null ? Carbon::parse( $item->video_js_mid_advertisement_sequence_time )->secondsSinceMidnight()  : '300';
+
+          $item['video_js_pre_position_ads_url']  = null ;
+          $item['video_js_mid_position_ads_urls']  = array() ;
+          $item['video_js_post_position_ads_url'] = null ;
+          $item['video_js_mid_advertisement_sequence_time_second'] = null ;
+
+          $plans_ads_enable_status = $this->plans_ads_enable($request->user_id) ;
+
+          if( $plans_ads_enable_status  == 1  && $choose_player == 1 ){
+
+                  // Pre-advertisement 
+
+                $item['video_js_pre_position_ads_url']  = Advertisement::select('advertisements.*','ads_events.ads_id','ads_events.status','ads_events.end','ads_events.start')
+                                              ->join('ads_events', 'ads_events.ads_id', '=', 'advertisements.id')
+                                              ->where('advertisements.status', 1)
+
+                                              ->when( $item->video_js_pre_position_ads == 'Random', function ($query) {
+
+                                                  return $query->inRandomOrder();
+
+                                              }, function ($query) use ($item) {
+
+                                                  return $query->where('advertisements.id', $item->video_js_pre_position_ads );
+
+                                              })
+
+                                              ->when( $advertisement_plays_24hrs == 0, function ($query) use ($current_time) {
+
+                                                  return $query->where('ads_events.status', 1)
+                                                      ->whereTime('ads_events.start', '<=', $current_time)
+                                                      ->whereTime('ads_events.end', '>=', $current_time);
+                                              })
+
+                                              ->groupBy('advertisements.id')
+                                              ->pluck('ads_path')
+                                              ->first();
+
+                  // Mid-advertisement 
+
+                $item['video_js_mid_position_ads_urls'] = Advertisement::select('advertisements.*', 'ads_events.ads_id', 'ads_events.status', 'ads_events.end', 'ads_events.start')
+                                          ->join('ads_events', 'ads_events.ads_id', '=', 'advertisements.id')
+                                          ->where('advertisements.status', 1)
+                                          ->groupBy('advertisements.id')
+
+                                          ->when( $item->video_js_mid_position_ads_category == 'random_category', function ($query) {
+
+                                                  return $query ;
+
+                                              }, function ($query) use ($item) {
+
+                                                  return $query->where('advertisements.ads_category', $item->video_js_mid_position_ads_category);
+
+                                              })
+
+                                              ->when( $advertisement_plays_24hrs == 0 , function ($query) use ($current_time) {
+
+                                                  return $query->where('ads_events.status', 1)
+                                                      ->whereTime('ads_events.start', '<=', $current_time)
+                                                      ->whereTime('ads_events.end', '>=', $current_time);
+                                                  })
+                                          
+                                          ->pluck('ads_path');
+
+                  // Post-advertisement 
+
+                $item['video_js_post_position_ads_url'] = Advertisement::select('advertisements.*','ads_events.ads_id','ads_events.status','ads_events.end','ads_events.start')
+                                              ->join('ads_events','ads_events.ads_id','=','advertisements.id')
+                                              ->where('advertisements.status', 1 )
+
+                                              ->when( $item->video_js_pre_position_ads == 'Random', function ($query) {
+
+                                                  return $query->inRandomOrder();
+
+                                                  }, function ($query) use ($item) {
+
+                                                  return $query->where('advertisements.id', $item->video_js_post_position_ads);
+
+                                                  })
+
+                                              ->when( $advertisement_plays_24hrs == 0, function ($query) use ($current_time) {
+
+                                                  return $query->where('ads_events.status', 1)
+                                                      ->whereTime('ads_events.start', '<=', $current_time)
+                                                      ->whereTime('ads_events.end', '>=', $current_time);
+                                                  })
+
+                                              ->groupBy('advertisements.id')
+                                              ->pluck('ads_path')
+                                              ->first();
+
+              $item['video_js_mid_advertisement_sequence_time_second'] = $video_js_mid_advertisement_sequence_time ; 
+
+          }
+
           return $item;
         });
 
@@ -1970,8 +2075,7 @@ public function verifyandupdatepassword(Request $request)
 
         $plans_ads_enable = $this->plans_ads_enable($request->user_id);
        
-
-        if($plans_ads_enable == 1){
+        if($plans_ads_enable == 1 && $choose_player == 0 ){
 
           $current_time = Carbon::now()->format('H:i:s');
 
