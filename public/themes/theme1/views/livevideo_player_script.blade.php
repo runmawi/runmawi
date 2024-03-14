4976c41e35aadf228493351aba517cffa56278bb
Manikandan <manikandan@webnexs.in>
2024-03-15 01:16:33 +0530

    fanway plyr.io Player live stream & old videojs comit - 82eed02cce3d8631d1a347a0b54dbde5e80c48df

--- public/themes/theme1/views/livevideo_player_script.blade.php
+++ public/themes/theme1/views/livevideo_player_script.blade.php
@@ -1,50 +1,121 @@
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
 
-        var player = videojs('live-stream-player', { // Video Js Player 
-            aspectRatio: '16:9',
-            fill: true,
-            playbackRates: [0.5, 1, 1.5, 2, 3, 4],
-            fluid: true,
-            autoplay: true,
+    let live_ads = <?php echo json_encode( $live_ads ); ?> ;
 
+    document.addEventListener('DOMContentLoaded', () => { 
 
-            controlBar: {
+    const player = new Plyr('#live_player_mp4',{
 
-                volumePanel: {
-                    inline: false
-                },
+           controls: [   'play-large','restart','rewind','play','fast-forward','progress',
+                        'current-time','mute','volume','captions','settings','pip','airplay',
+                        'fullscreen'
+		            ],
 
-                children: {
-                    'playToggle': {},
-                    'currentTimeDisplay': {},
-                    'timeDivider': {},
-                    'durationDisplay': {},
-                    'liveDisplay': {},
-
-                    'flexibleWidthSpacer': {},
-                    'progressControl': {},
-
-                    'fullscreenToggle': {},
-                },
-
+            ads:{ 
+                  enabled: true, 
+                  tagUrl: live_ads 
             }
         });
+    });
 
 
-        player.hlsQualitySelector({ // Hls Quality Selector - M3U8 
-            displayCurrentQuality: true,
+    document.addEventListener('DOMContentLoaded', () => { 
+
+    const player = new Plyr('#acc_audio',{
+
+        controls: [   'play-large','play','progress',
+                        'current-time','mute','volume','captions','settings','airplay',
+                        'fullscreen'
+                    ],
+
+            ads:{ 
+                enabled: true, 
+                tagUrl: live_ads 
+            },
         });
+    });
 
-        var Advertisement  =  '<?= $live_ads ?>' ;  // Advertisement
+    document.addEventListener('DOMContentLoaded', () => { 
 
-        if (!!Advertisement) {
+    const player = new Plyr('#Embed_player',{
 
-            player.ima({
-                adTagUrl: Advertisement,
-                showControlsForAds: true,
-                debug: false,
+          controls: [   'play-large','restart','rewind','play','fast-forward','progress',
+                        'current-time','mute','volume','captions','settings','pip','airplay',
+                        'fullscreen'
+		            ],
+        });
+    });
+
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const video = document.querySelector("#live_player");
+        const source = video.getElementsByTagName("source")[0].src;
+  
+        const defaultOptions = {};
+
+        if (!Hls.isSupported()) {
+
+            defaultOptions.ads = {
+                enabled: true, 
+                tagUrl: live_ads
+            }
+
+            video.src = source;
+            var player = new Plyr(video, defaultOptions);
+        } 
+        else
+        {
+            const hls = new Hls();
+            hls.loadSource(source);
+            
+                hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+                const availableQualities = hls.levels.map((l) => l.height)
+                availableQualities.unshift(0) 
+
+                defaultOptions.quality = {
+                    default: 0, //Default - AUTO
+                    options: availableQualities,
+                    forced: true,        
+                    onChange: (e) => updateQuality(e),
+        }
+
+          // Add Auto Label 
+            defaultOptions.i18n = {
+                qualityLabel: { 0: 'Auto', },
+            }
+
+            defaultOptions.ads = {
+                enabled: true, 
+                tagUrl: live_ads
+            }
+
+            hls.on(Hls.Events.LEVEL_SWITCHED, function (event, data) {
+                var span = document.querySelector(".plyr__menu__container [data-plyr='quality'][value='0'] span")
+                if (hls.autoLevelEnabled) {
+                    span.innerHTML = `AUTO (${hls.levels[data.level].height}p)`
+                } else {
+                    span.innerHTML = `AUTO`
+                }
+            })
+      
+            var player = new Plyr(video, defaultOptions);
+      });	
+
+        hls.attachMedia(video);
+            window.hls = hls;		 
+        }
+
+        function updateQuality(newQuality) {
+            if (newQuality === 0) {
+            window.hls.currentLevel = -1;
+            } else {
+            window.hls.levels.forEach((level, levelIndex) => {
+                if (level.height === newQuality) {
+                console.log("Found quality match with " + newQuality);
+                window.hls.currentLevel = levelIndex;
+                }
             });
+            }
         }
     });
 </script>
