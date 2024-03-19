be45a11f4c1fb377e5a9037b15f35fe9cbdc8e86
Sanjai Kumar M <sanjai@webnexs.in>
2024-03-19 09:36:02 +0530

    keywords meta

--- public/themes/default/views/header.php
+++ public/themes/default/views/header.php
@@ -131,7 +131,7 @@
       elseif(!empty($SiteMeta_page)){ echo $SiteMeta_page->meta_description .' | '. $settings->website_name ; }
       else{ echo $settings->website_description   ;} //echo $settings; ?>" />
       
- <meta name="keywords" content="<?php  $dynamic_page->meta_keywords ? $dynamic_page->meta_keywords : $dynamic_page->meta_keywords?>">
+ <meta name="keywords" content="<?php  @$dynamic_page->meta_keywords ? @$dynamic_page->meta_keywords : @$dynamic_page->meta_keywords?>">
 
 <!-- Schema.org markup for Google+ -->
 <meta itemprop="name" content="<?php
