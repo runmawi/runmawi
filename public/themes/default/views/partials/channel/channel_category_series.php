ae16fae372b7fa9e96883b60ff74799dfb0a7d83
manivel <manivel@webnexs.in>
2024-04-15 12:26:07 +0530

    channel tab issue

--- public/themes/default/views/partials/channel/channel_category_series.php
+++ public/themes/default/views/partials/channel/channel_category_series.php
@@ -11,68 +11,23 @@
                         foreach($SeriesCategory as $Series_Category){ ?>
                             <li class="slide-item">
                                 <div class="block-images position-relative">
-                                
                                                     <!-- block-images -->
                                     <div class="border-bg">
                                         <div class="img-box">
                                             <a class="playTrailer" href="<?php echo URL::to('/play_series'.'/'.$Series_Category->slug ) ?>">
-                                                <img class="img-fluid w-100" loading="lazy" data-src="<?php echo URL::to('/').'/public/uploads/images/'.@$Series_Category->image;  ?>"  alt="series">
+                                                <img class="img-fluid w-100" loading="lazy" data-src="<?php echo URL::to('/').'/public/uploads/images/'.$latest_serie->image;  ?>"  alt="series">
                                             </a>
-
-                                            <?php if($ThumbnailSetting->free_or_cost_label == 1) { ?> 
-                                                <?php if($Series_Category->access == 'subscriber' ){ ?>
-                                                    <p class="p-tag"> <i class="fas fa-crown" style='color:gold'></i> </p>
-                                                <?php }elseif($Series_Category->access == 'registered'){?>
-                                                    <p class="p-tag"><?php echo (__('Register Now')); ?></p>
-                                                <?php } elseif(!empty($Series_Category->ppv_status)){?>
-                                                    <p class="p-tag1"><?php echo $currency->symbol.' '.$settings->ppv_price; ?></p>
-                                                <?php }elseif(!empty($Series_Category->ppv_status || !empty($Series_Category->ppv_status) && $Series_Category->ppv_status == null)){ ?>
-                                                    <p class="p-tag1"><?php echo $currency->symbol.' '.$settings->ppv_status; ?></p>
-                                                <?php }elseif($Series_Category->ppv_status == null && $Series_Category->ppv_price == null ){ ?>
-                                                    <p class="p-tag"><?php echo (__('Free')); ?></p>
-                                                <?php } ?>
-                                            <?php } ?>
-                                        
                                         </div>
                                     </div>
 
                                     <div class="block-description">
                                         <a class="playTrailer" href="<?php echo URL::to('/play_series'.'/'.$Series_Category->slug ) ?>">
-                                            <img class="img-fluid w-100" loading="lazy" data-src="<?php echo URL::to('/').'/public/uploads/images/'.@$Series_Category->player_image;  ?>" alt="series">
-                                    
-
-                                            <?php if($ThumbnailSetting->free_or_cost_label == 1) { ?> 
-                                                <?php if($Series_Category->access == 'subscriber' ){ ?>
-                                                    <p class="p-tag"> <i class="fas fa-crown" style='color:gold'></i> </p>
-                                                <?php }elseif($Series_Category->access == 'registered'){?>
-                                                    <p class="p-tag"><?php echo (__('Register Now')); ?></p>
-                                                <?php } elseif(!empty($Series_Category->ppv_status)){?>
-                                                    <p class="p-tag1"><?php echo $currency->symbol.' '.$settings->ppv_price; ?></p>
-                                                <?php }elseif(!empty($Series_Category->ppv_status || !empty($Series_Category->ppv_status) && $Series_Category->ppv_status == null)){ ?>
-                                                    <p class="p-tag1"><?php echo $currency->symbol.' '.$settings->ppv_status; ?></p>
-                                                <?php }elseif($Series_Category->ppv_status == null && $Series_Category->ppv_price == null ){ ?>
-                                                    <p class="p-tag"><?php echo (__('Free')); ?></p>
-                                                <?php } ?>
-                                            <?php } ?>
+                                            <img class="img-fluid w-100" loading="lazy" data-src="<?php echo URL::to('/').'/public/uploads/images/'.$latest_serie->player_image;  ?>" alt="series">
                                         </a>
-                                        <?php if($ThumbnailSetting->free_or_cost_label == 1) { ?> 
-                                            <?php if($Series_Category->access == 'subscriber' ){ ?>
-                                                <p class="p-tag"> <i class="fas fa-crown" style='color:gold'></i> </p>
-                                            <?php }elseif($Series_Category->access == 'registered'){?>
-                                                <p class="p-tag"><?php echo (__('Register Now')); ?></p>
-                                            <?php } elseif(!empty($Series_Category->ppv_status)){?>
-                                                <p class="p-tag1"><?php echo $currency->symbol.' '.$settings->ppv_price; ?></p>
-                                            <?php }elseif(!empty($Series_Category->ppv_status || !empty($Series_Category->ppv_status) && $Series_Category->ppv_status == null)){ ?>
-                                                <p class="p-tag1"><?php echo $currency->symbol.' '.$settings->ppv_status; ?></p>
-                                            <?php }elseif($Series_Category->ppv_status == null && $Series_Category->ppv_price == null ){ ?>
-                                                <p class="p-tag"><?php echo (__('Free')); ?></p>
-                                            <?php } ?>
-                                        <?php } ?>
-
                                         <div class="hover-buttons text-white"> 
                                             <a class="text-white" href="<?php echo URL::to('/play_series'.'/'.$Series_Category->slug ) ?>" >
                                                 <p class="epi-name text-left m-0"><?php echo __($Series_Category->title); ?></p>
-                                    
+                                            
                                                 <div class="movie-time d-flex align-items-center my-2">
                                                     <p class="badge badge-secondary p-1 mr-2"><?php echo $Series_Category->age_restrict.' '.'+' ?></p>
                                                     <p class="badge badge-secondary p-1 mr-2"><?php 
@@ -83,19 +38,17 @@
                                                     $Episode = App\Episode::where('series_id',$Series_Category->id)->count(); 
                                                     echo $Episode.' '.'Episodes'
                                                     ?></p>
-
                                                 </div>
                                             </a>
 
-                                    
+                                
                                             <a class="epi-name mt-5 mb-0 btn" href="<?php echo URL::to('/play_series'.'/'.$Series_Category->slug ) ?>" >
-                                                <i class="fa fa-play mr-1" aria-hidden="true"></i> <?= ('Watch Series') ?>
+                                                <i class="fa fa-play mr-1" aria-hidden="true"></i> <?=  ('Watch Series') ?>
                                             </a>
                                         </div>
                                     </div>
                                 </div>
                             </li>
-
 
 
 
