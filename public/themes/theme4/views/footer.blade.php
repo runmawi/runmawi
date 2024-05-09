134ec68e965eaa507c23f6e13fdddc07b3c9bab4
manivel <manivel@webnexs.in>
2024-05-09 11:02:22 +0530

    scroll res optiz error script & infi loader

--- public/themes/theme4/views/footer.blade.php
+++ public/themes/theme4/views/footer.blade.php
@@ -331,4 +331,29 @@ if ($Prevent_inspect == 1) { ?>
             }
         });
     </script>
+
+<!-- scrolling performance -->
+<script>
+    jQuery.event.special.touchstart = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+    };
+    jQuery.event.special.touchmove = {
+        setup: function( _, ns, handle ) {
+            this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+        }
+    };
+    jQuery.event.special.wheel = {
+        setup: function( _, ns, handle ){
+            this.addEventListener("wheel", handle, { passive: true });
+        }
+    };
+    jQuery.event.special.mousewheel = {
+        setup: function( _, ns, handle ){
+            this.addEventListener("mousewheel", handle, { passive: true });
+        }
+    };
+</script>
+
 <?php } ?>
