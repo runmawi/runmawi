4e68dfb63edb672aab142fc1c1002cdcd4d01c81
Sanjai Kumar M <sanjai@webnexs.in>
2024-05-22 15:48:12 +0530

    channel_name and up_next

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -23854,13 +23854,13 @@ public function TV_login(Request $request)
             $item['Player_image_url'] = $item->player_image != null ?  URL::to('public/uploads/EPG-Channel/'.$item->player_image ) : default_horizontal_image_url();
             $item['Logo_url'] = $item->logo != null ?  URL::to('public/uploads/EPG-Channel/'.$item->logo ) : default_vertical_image_url();
             // $item['scheduled_videos'] = ChannelVideoScheduler::where('channe_id',$item->id)->where('choosed_date',$choosed_date)->get();
+  
             $scheduled_videos = ChannelVideoScheduler::where('channe_id', $item->id)->where('choosed_date', $choosed_date)->get();
-
-            // Add next video title to each scheduled video
-              $scheduled_videos->each(function ($video, $index) use ($scheduled_videos, $item) {
+            $scheduled_videos->each(function ($video, $index) use ($scheduled_videos, $item) {
                 $nextVideoTitle = $index + 1 < $scheduled_videos->count() ? $scheduled_videos[$index + 1]->socure_title : null;
-                $video->up_next = $nextVideoTitle ? $item->name : 0;
-            });
+                $video->channel_name = $nextVideoTitle ? $item->name : $item->name;
+                $video->up_next = $nextVideoTitle ? $nextVideoTitle : 0;
+              });
 
             $item['scheduled_videos'] = $scheduled_videos;
           return $item;
