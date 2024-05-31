25cef8386b447b1ca937f8f64e2bcb0b6bc2b35c
Sanjai Kumar M <sanjai@webnexs.in>
2024-05-31 12:38:09 +0530

    StorageSetting api

--- routes/api.php
+++ routes/api.php
@@ -676,4 +676,6 @@ Route::group(
             Route::post('time-zone', 'ApiAuthController@TimeZone'); 
             Route::post('channel-epg', 'ApiAuthController@channel_epg'); 
 
+            Route::post('storage-setting', 'ApiAuthController@StorageSetting'); 
+
         });
