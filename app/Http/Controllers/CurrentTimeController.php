2399fb5ca39be8bd0856928d6c93a16fd153952f
Manikandan <manikandan@webnexs.in>
2024-07-09 14:28:07 +0530

    CurrentTimeController

--- app/Http/Controllers/CurrentTimeController.php
+++ app/Http/Controllers/CurrentTimeController.php
@@ -10,13 +10,15 @@ class CurrentTimeController extends Controller
 {
     public function current_time(Request $request)
     {
+        $current_carbon = Carbon::now(current_timezone());
+
         $data = array(
-            'Get_hours' =>  Carbon::now()->hour ,
-            'Get_Minutes' => Carbon::now()->minute ,
-            'Get_Seconds' => Carbon::now()->second ,
-            'Get_Year' => Carbon::now()->year ,
-            'Get_Date' => Carbon::now()->day ,
-            'Get_Day' => substr(strtoupper(Carbon::now()->format('l')), 0, 3) ,
+            'Get_hours' =>  $current_carbon->hour ,
+            'Get_Minutes' => $current_carbon->minute ,
+            'Get_Seconds' => $current_carbon->second ,
+            'Get_Year' => $current_carbon->year ,
+            'Get_Date' => $current_carbon->day ,
+            'Get_Day' => substr(strtoupper($current_carbon->format('l')), 0, 3) ,
         );
 
         return view ('admin.current_time.index',compact('data'));
