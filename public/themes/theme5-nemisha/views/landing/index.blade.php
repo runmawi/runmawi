dff525d8144c30b1a034c874b37a452693a12700
Balaji <balaji.b@webnexs.in>
2024-07-10 17:34:50 +0530

    changes

--- public/themes/theme5-nemisha/views/landing/index.blade.php
+++ public/themes/theme5-nemisha/views/landing/index.blade.php
@@ -74,6 +74,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" defer></script>
 
     <!-- Include jQuery library -->
+    <link rel="preload" href="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" as="script">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
