6dc9880a7f6e6252f1ef89e1af8f11389b45def3
Manivel S <manivel@webnexs.in>
2024-08-01 10:34:53 +0530

    home-sections slider changes

--- public/themes/theme4/views/partials/home/ContentPartners.blade.php
+++ public/themes/theme4/views/partials/home/ContentPartners.blade.php
@@ -13,68 +13,50 @@
                     </div>
 
 
-                    <div class="tvthrillers-contens">
-                        <ul id="trending-slider-nav" class="cpp-portal-nav list-inline p-0 mar-left row align-items-center">
-                                @foreach ($data as $CPP_details)
-                                    <li class="slick-slide">
-                                        <a href="javascript:;">
-                                            <div class="movie-slick position-relative">
-                                                @if ( $multiple_compress_image == 1)
-                                                    <img class="img-fluid position-relative" alt="{{ $CPP_details->title }}" src="{{ $CPP_details->image ?  URL::to('public/uploads/images/'.$CPP_details->image) : $default_vertical_image_url }}"
-                                                        srcset="{{ URL::to('public/uploads/PCimages/'.$CPP_details->responsive_image.' 860w') }},
-                                                        {{ URL::to('public/uploads/Tabletimages/'.$CPP_details->responsive_image.' 640w') }},
-                                                        {{ URL::to('public/uploads/mobileimages/'.$CPP_details->responsive_image.' 420w') }}" >
-                                                @else
-                                                    <img src="{{ $CPP_details->picture ? URL::to('public/uploads/moderator_albums/'.$CPP_details->picture ) : $default_vertical_image_url }}" class="img-fluid w-100" alt="content">
-                                                @endif 
-                                            </div>
-                                        </a>
-                                    </li>
+                    <div class="channels-list">
+                        <div class="channel-row">
+                            <div id="trending-slider-nav" class="video-list cpp-video">
+                                @foreach ($data as $key => $CPP_details)
+                                    <div class="item" data-index="{{ $key }}">
+                                        <div>
+                                            @if ( $multiple_compress_image == 1)
+                                                <img class="flickity-lazyloaded" alt="{{ $CPP_details->title }}" src="{{ $CPP_details->image ?  URL::to('public/uploads/images/'.$CPP_details->image) : $default_vertical_image_url }}"
+                                                    srcset="{{ URL::to('public/uploads/PCimages/'.$CPP_details->responsive_image.' 860w') }},
+                                                    {{ URL::to('public/uploads/Tabletimages/'.$CPP_details->responsive_image.' 640w') }},
+                                                    {{ URL::to('public/uploads/mobileimages/'.$CPP_details->responsive_image.' 420w') }}" >
+                                            @else
+                                                <img src="{{ $CPP_details->picture ?$CPP_details->picture : $default_vertical_image_url }}" class="flickity-lazyloaded" alt="{{ $CPP_details->title }}">
+                                            @endif 
+                                        </div>
+                                    </div>
                                 @endforeach
-                            </ul>
+                            </div>
+                        </div>
 
-                            <ul id="trending-slider cpp-portal" class="list-inline p-0 m-0 align-items-center cpp-portal theme4-slider" style="display:none;">
-                                @foreach ($data as $CPP_details)
-                                    <li class="slick-slide">
-                                        <div class="tranding-block position-relative trending-thumbnail-image" >
-                                            <button class="drp-close">×</button>
+                        <div id="videoInfo" class="cpp-video-dropdown" style="display:none;">
+                            <button class="drp-close">×</button>
+                            <div class="vib" style="display:flex;">
+                                @foreach ($data as $key => $CPP_details)
+                                    <div class="caption" data-index="{{ $key }}">
+                                        <h2 class="caption-h2">{{ optional($CPP_details)->username }}</h2>
 
-                                            <div class="trending-custom-tab">
-                                                <div class="trending-content">
-                                                    <div id="" class="overview-tab tab-pane fade active show h-100">
-                                                        <div class="trending-info align-items-center w-100 animated fadeInUp">
+                                        @if (optional($CPP_details)->description)
+                                            <div class="trending-dec">{{ \Illuminate\Support\Str::limit(strip_tags(html_entity_decode(optional($CPP_details)->description)), 500) }}</div>
+                                        @endif
 
-                                                            <div class="caption pl-4">
-                                                                <h2 class="caption-h2">{{ ucwords(optional($CPP_details)->username)  }}</h2>
-
-                                                                <div class="p-btns">
-                                                                    <div class="d-flex align-items-center p-0">
-                                                                        <a href="{{ URL::to('contentpartner/' . $CPP_details->slug ) }}" class="button-groups btn btn-hover  mr-2" tabindex="0"><i class="fa fa-play mr-2" aria-hidden="true"></i> Visit </a>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-
-                                                            <div class="dropdown_thumbnail">
-                                                                @if ( $multiple_compress_image == 1)
-                                                                    <img  alt="latest_series" src="{{$CPP_details->player_image ?  URL::to('public/uploads/images/'.$CPP_details->player_image) : $default_horizontal_image_url }}"
-                                                                        srcset="{{ URL::to('public/uploads/PCimages/'.$CPP_details->responsive_player_image.' 860w') }},
-                                                                        {{ URL::to('public/uploads/Tabletimages/'.$CPP_details->responsive_player_image.' 640w') }},
-                                                                        {{ URL::to('public/uploads/mobileimages/'.$CPP_details->responsive_player_image.' 420w') }}" >
-                                                                @else
-                                                                    <img  src="{{ $CPP_details->picture ? URL::to('public/uploads/moderator_albums/'.$CPP_details->picture ) : $default_vertical_image_url }}" alt="cpp">
-                                                                @endif 
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
+                                        <div class="p-btns">
+                                            <div class="d-flex align-items-center p-0">
+                                                <a href="{{ URL::to('contentpartner/' . $CPP_details->slug ) }}" class="button-groups btn btn-hover  mr-2" tabindex="0"><i class="fa fa-play mr-2" aria-hidden="true"></i> Play Now </a>
                                             </div>
                                         </div>
-                                    </li>
+                                    </div>
+                                    <div class="thumbnail" data-index="{{ $key }}">
+                                        <img src="{{ $CPP_details->player_image ?  URL::to('public/uploads/images/'.$CPP_details->player_image) : $default_vertical_image_url }}" class="flickity-lazyloaded" alt="latest_series" width="300" height="200">
+                                    </div>
                                 @endforeach
-                            </ul>
+                            </div>
+                        </div>
 
-
-                        
                     </div>
                 </div>
             </div>
@@ -84,64 +66,48 @@
 
 
 <script>
-    
-    $( window ).on("load", function() {
-        $('.cpp-portal').hide();
+
+    var elem = document.querySelector('.cpp-video');
+    var flkty = new Flickity(elem, {
+        cellAlign: 'left',
+        contain: true,
+        groupCells: true,
+        pageDots: false,
+        draggable: true,
+        freeScroll: true,
+        imagesLoaded: true,
+        lazyload:true,
+    });
+    document.querySelectorAll('.cpp-video .item').forEach(function(item) {
+        item.addEventListener('click', function() {
+            document.querySelectorAll('.cpp-video .item').forEach(function(item) {
+                item.classList.remove('current');
+            });
+
+            item.classList.add('current');
+
+            var index = item.getAttribute('data-index');
+
+            document.querySelectorAll('.cpp-video-dropdown .caption').forEach(function(caption) {
+                caption.style.display = 'none';
+            });
+            document.querySelectorAll('.cpp-video-dropdown .thumbnail').forEach(function(thumbnail) {
+                thumbnail.style.display = 'none';
+            });
+
+            var selectedCaption = document.querySelector('.cpp-video-dropdown .caption[data-index="' + index + '"]');
+            var selectedThumbnail = document.querySelector('.cpp-video-dropdown .thumbnail[data-index="' + index + '"]');
+            if (selectedCaption && selectedThumbnail) {
+                selectedCaption.style.display = 'block';
+                selectedThumbnail.style.display = 'block';
+            }
+
+            document.getElementsByClassName('cpp-video-dropdown')[0].style.display = 'flex';
+        });
     });
 
-    $(document).ready(function() {
 
-        $('.cpp-portal').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            fade: true,
-            draggable: false,
-            asNavFor: '.cpp-portal-nav',
-        });
-
-        $('.cpp-portal-nav').slick({
-            slidesToShow: 6,
-            slidesToScroll: 6,
-            asNavFor: '.cpp-portal',
-            dots: false,
-            arrows: true,
-            prevArrow: '<a href="#" class="slick-arrow slick-prev" aria-label="Previous" type="button">Previous</a>',
-            nextArrow: '<a href="#" class="slick-arrow slick-next" aria-label="Next" type="button">Next</a>',
-            infinite: false,
-            focusOnSelect: true,
-            responsive: [
-                {
-                    breakpoint: 1200,
-                    settings: {
-                        slidesToShow: 6,
-                        slidesToScroll: 1,
-                    },
-                },
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 5,
-                        slidesToScroll: 1,
-                    },
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                    },
-                },
-            ],
-        });
-
-        $('.cpp-portal-nav').on('click', function() {
-            $( ".drp-close" ).trigger( "click" );
-            $('.cpp-portal').show();
-        });
-
-        $('body').on('click', '.drp-close', function() {
-            $('.cpp-portal').hide();
-        });
+    $('body').on('click', '.drp-close', function() {
+        $('.cpp-video-dropdown').hide();
     });
 </script>
