114c6df57677d43d6df710b79b8333f0dd188e87
Manikandan <manikandan@webnexs.in>
2024-08-21 19:29:08 +0530

    Admin OTP option for 24*7

--- resources/views/admin/OTP/index.blade.php
+++ resources/views/admin/OTP/index.blade.php
@@ -100,7 +100,7 @@
                         <br>
 
                         <div class="row md-12">
-                            <div class="col-md-5">
+                            <div class="col-md-6">
                                 <div class="panel panel-primary" data-collapsed="0">
                                     <div class="panel-heading">
                                         <div class="panel-title"><label>OTP Through</label></div>
@@ -108,7 +108,7 @@
                                             <a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a>
                                         </div>
                                     </div>
-                                    <div class="panel-body" style="display: block;">
+                                    <div class="panel-body" >
                                        <select  class="form-control otp_vai"  name="otp_vai" >
                                           <option value=" " > Select the OTP Through</option>
                                           <option value="fast2sms" {{ @$AdminOTPCredentials->otp_vai == "fast2sms" ? 'selected' : null }}  > fast2sms </option>
@@ -118,7 +118,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-7 fast2sms_div">
+                            <div class="col-md-6 fast2sms_div" style="{{ @$AdminOTPCredentials->otp_vai == 'fast2sms' ? 'display=none !important;' : 'display: none !important' }} ">
                                 <div class="panel panel-primary" data-collapsed="0">
                                     <div class="panel-heading">
                                         <div class="panel-title"><label> API KEY - fast2sms </label></div>
@@ -132,7 +132,8 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-7 24x7sms_div">
+                                    {{-- 24x7sms div --}}
+                            <div class="col-md-6 24x7sms_div" style="{{ @$AdminOTPCredentials->otp_vai == '24x7sms' ? 'display=none !important;' : 'display: none !important' }} ">
                                 <div class="panel panel-primary" data-collapsed="0">
                                     <div class="panel-heading">
                                         <div class="panel-title"><label> API KEY - 24x7sms </label></div>
@@ -141,12 +142,88 @@
                                         </div>
                                     </div>
                                     <div class="panel-body" style="display: block;">
-                                        <input type="text" class="form-control" name="otp_24x7sms_api_key" id="otp_24x7sms_api_key" value="{{ !empty($AdminOTPCredentials->otp_24x7sms_api_key) ? $AdminOTPCredentials->otp_24x7sms_api_key : null }}" placeholder="24x7sms API Key" />
+                                        <input type="text" class="form-control" name="otp_24x7sms_api_key" id="otp_24x7sms_api_key" value="{{ !empty($AdminOTPCredentials->otp_24x7sms_api_key) ? $AdminOTPCredentials->otp_24x7sms_api_key : null }}" placeholder="xxxxxxxxxxxxxx" />
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <br>
+
+                                    {{-- 24x7sms div --}}
+
+                        <div class="row col-md-12 24x7sms_div" style="{{ @$AdminOTPCredentials->otp_vai == '24x7sms' ? 'display=none !important;' : 'display: none !important' }} ">
+                            <div class="col-md-6">
+                                <div class="panel panel-primary" data-collapsed="0">
+                                    <div class="panel-heading">
+                                        <div class="panel-title"><label> Sender ID - 24x7 Sms </label></div>
+                                        <div class="panel-options">
+                                            <a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a>
+                                        </div>
+                                    </div>
+                                    <div class="panel-body" style="display: block;">
+                                        <input type="text" class="form-control" name="otp_24x7sms_sender_id" id="otp_24x7sms_sender_id" value="{{ !empty($AdminOTPCredentials->otp_24x7sms_sender_id) ? $AdminOTPCredentials->otp_24x7sms_sender_id : null }}" placeholder="xxxxxxxxxxxxxx" />
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-6">
+                                <div class="panel panel-primary" data-collapsed="0">
+                                    <div class="panel-heading">
+                                        <div class="panel-title"><label> Sevice Name - 24x7 Sms </label></div>
+                                        <div class="panel-options">
+                                            <a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a>
+                                        </div>
+                                    </div>
+
+                                    <div class="panel-body" >
+                                        <select  class="form-control sevicename"  name="otp_24x7sms_sevicename" >
+                                           <option value=" " > Select the Sevice Name</option>
+                                           <option value="TEMPLATE_BASED" {{ @$AdminOTPCredentials->otp_24x7sms_sevicename == "TEMPLATE_BASED" ? 'selected' : null }}  > TEMPLATE_BASED </option>
+                                           <option value="PROMOTIONAL_HIGH" {{ @$AdminOTPCredentials->otp_24x7sms_sevicename == "PROMOTIONAL_HIGH" ? 'selected'  : null }}  > PROMOTIONAL_HIGH </option>
+                                           <option value="PROMOTIONAL_SPL" {{ @$AdminOTPCredentials->otp_24x7sms_sevicename == "PROMOTIONAL_SPL" ? 'selected'  : null }}  > PROMOTIONAL_SPL </option>
+                                           <option value="OPTIN_OPTOUT" {{ @$AdminOTPCredentials->otp_24x7sms_sevicename == "OPTIN_OPTOUT" ? 'selected'  : null }}  > OPTIN_OPTOUT </option>
+                                           <option value="INTERNATIONAL" {{ @$AdminOTPCredentials->otp_24x7sms_sevicename == "INTERNATIONAL" ? 'selected'  : null }}  > INTERNATIONAL </option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div><br>
+                        </div>
+                        <br>
+
+                        <div class="row col-md-12 24x7sms_div TEMPLATE_BASED_div" style="{{ @$AdminOTPCredentials->otp_vai == '24x7sms' &&  @$AdminOTPCredentials->otp_24x7sms_sevicename == 'TEMPLATE_BASED' ? 'display=none !important;' : 'display: none !important' }} ">
+                            
+                            <div class="col-md-6">
+                                <div class="panel panel-primary" data-collapsed="0">
+                                    <div class="panel-heading">
+                                        <div class="panel-title"><label> DLT Template ID - 24x7 Sms </label></div>
+                                        <div class="panel-options">
+                                            <a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a>
+                                        </div>
+                                    </div>
+
+                                    <div class="panel-body" style="display: block;">
+                                        <input type="text" class="form-control" name="DLTTemplateID" id="DLTTemplateID" value="{{ !empty($AdminOTPCredentials->DLTTemplateID) ? $AdminOTPCredentials->DLTTemplateID : null }}" placeholder="xxxxxxxxxxxxxx" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="panel panel-primary" data-collapsed="0">
+                                    <div class="panel-heading">
+                                        <div class="panel-title"><label> Template Message - 24x7 Sms </label></div>
+                                        <div class="panel-options">
+                                            <a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a>
+                                        </div>
+                                    </div>
+                                    <div class="panel-body" style="display: block;">
+                                        <textarea  class="form-control" name="template_message" id="" cols="30" rows="10" placeholder="Template custom Message">
+                                            {{  @$AdminOTPCredentials->template_message }}
+                                        </textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
 
                         <div class="panel-body mt-3 ml-2 text-right">
                             <input type="submit" value="Update" class="btn btn-primary" />
@@ -161,14 +238,17 @@
     <script>
         $( document ).ready(function() {
 
-            $('.24x7sms_div,.fast2sms_div').hide();
-
             $('.otp_vai').on('change', function() {
                 $('.24x7sms_div,.fast2sms_div').hide();
-                
                 var otp_vai_div= ( $(this).find(":selected").val() );
-
                 $( '.' + otp_vai_div +'_div' ).show();
+            });
+
+            $('.sevicename').on('change', function() {
+
+                $('.TEMPLATE_BASED_div').hide();
+                var TEMPLATE_BASED_div= ( $(this).find(":selected").val() );
+                $( '.' + TEMPLATE_BASED_div +'_div' ).show();
             });
         });
 
