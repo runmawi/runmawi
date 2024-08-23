61ed5ef65cfd6366b4c120505c7dc3e996dc5459
Balaji <balaji.b@webnexs.in>
2024-08-23 12:28:14 +0530

    live banner

--- public/themes/default/views/video-js-Player/Livestream/live-player.blade.php
+++ public/themes/default/views/video-js-Player/Livestream/live-player.blade.php
@@ -7,7 +7,7 @@
         $recurring_program_Status = true ;
 
         $recurring_timezone = App\TimeZone::where('id', $Livestream_details->recurring_timezone)->pluck('time_zone')->first();
-        
+
         $Current_time = Carbon\Carbon::now(current_timezone());
         $convert_time = $Current_time->copy()->timezone($recurring_timezone);
 
@@ -40,7 +40,7 @@
                     $recurring_program_Status = false ;
                 }
                 break;
-            
+
             default:
                 break;
         }
@@ -62,7 +62,7 @@
     @if ( $Livestream_details->users_video_visibility_status)
 
         <div id="video sda" class="fitvid" style="margin: 0 auto;">
-            @if ( $Livestream_details->url_type == "embed" ) 
+            @if ( $Livestream_details->url_type == "embed" )
 
                 <iframe class="responsive-iframe" src="{{ $Livestream_details->livestream_URL }}" poster="{{ $Livestream_details->Player_thumbnail }}"
                     frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" width="200" height="200"
@@ -76,12 +76,12 @@
                     <i class="fa fa-chevron-left" aria-hidden="true"></i>
                 </button>
 
-                <video id="live-stream-player" class="vjs-theme-city my-video video-js vjs-big-play-centered vjs-play-control customVideoPlayer vjs-fluid vjs_video_1462 vjs-controls-enabled vjs-picture-in-picture-control vjs-workinghover vjs-v7 vjs-quality-selector vjs-has-started vjs-paused vjs-layout-x-large vjs-user-inactive" controls 
+                <video id="live-stream-player" class="vjs-theme-city my-video video-js vjs-big-play-centered vjs-play-control customVideoPlayer vjs-fluid vjs_video_1462 vjs-controls-enabled vjs-picture-in-picture-control vjs-workinghover vjs-v7 vjs-quality-selector vjs-has-started vjs-paused vjs-layout-x-large vjs-user-inactive" controls
                     width="auto" height="auto" poster="{{ $Livestream_details->Player_thumbnail }}" playsinline="playsinline"
                     autoplay>
                     <source src="{{ $Livestream_details->livestream_URL }}" type="{{ $Livestream_details->livestream_player_type }}">
                 </video>
-            @endif  
+            @endif
 
                                     {{-- Free duration --}}
             <div class="video" id="visibilityMessage" style="color: white; display: none; background: linear-gradient(333deg, rgba(4, 21, 45, 0) 0%, #050505 100.17%), url('{{  $Livestream_details->player_image_url  }}');background-size: cover; height:100vh;">
@@ -113,7 +113,7 @@
                                     <span class="text pr-2"> {{ __( 'Purchase Now' ) }} </span>
                                 </div>
                             </a>
-                        @endif 
+                        @endif
                     </div>
                 </div>
             </div>
@@ -123,7 +123,7 @@
 
         <div class="video" style="background: linear-gradient(333deg, rgba(4, 21, 45, 0) 0%, #050505 100.17%), url('{{  $Livestream_details->player_Thumbnail  }}');background-size: cover; height:100vh;">
             <div class="row container" style="padding-top:4em;">
-                                
+
                                     {{-- Back Button --}}
                 <button class="staticback-btn" onclick="history.back()" title="Back Button">
                     <i class="fa fa-arrow-left" aria-hidden="true" style="font-size:25px;"></i>
@@ -134,7 +134,7 @@
                 <div class="col-lg-3 col-6 mt-5">
                     <img class="posterImg w-100"  src="{{ $Livestream_details->Thumbnail }}" >
                 </div>
-                
+
                 <div class="col-lg-6 col-6 mt-5">
                     <h2 class="title">{{ optional($Livestream_details)->title }} </h2><br>
                     <h5 class="title"> {{ $Livestream_details->users_video_visibility_status_message }}</h5><br>
@@ -155,7 +155,7 @@
                                 <span class="text pr-2"> {{ __( 'Purchase Now' ) }} </span>
                             </div>
                         </a>
-                    @endif 
+                    @endif
                 </div>
             </div>
         </div>
@@ -164,11 +164,11 @@
 @elseif( $recurring_program_Status == true )
 
     <div id="" style="background: linear-gradient(0deg, rgba(0, 0, 0, 1.4), rgba(0, 0, 0, 0.3)), url({{ URL::to('/') }}/public/uploads/images/{{ $Livestream_details->player_image }}); background-repeat: no-repeat; background-size: cover; padding: 150px 10px;">
-        
-        <h2>{{ ucwords($Livestream_details->title) }}</h2><br>
+
+        <h2 class="banner-live-details">{{ ucwords($Livestream_details->title) }}</h2><br>
 
         @if ($Livestream_details->publish_type == "recurring_program")
-        
+
             @php
                 $timezone = App\TimeZone::where('id',$Livestream_details->recurring_timezone)->pluck('time_zone')->first();
                 $startTime = Carbon\Carbon::parse($Livestream_details->program_start_time)->isoFormat('h:mm A');
@@ -177,8 +177,8 @@
 
             @if ($Livestream_details->recurring_program == "daily")
 
-                <h2>Live Streaming On {{ $Livestream_details->recurring_program }} from {{ $startTime }} to {{ $endTime }} - {{ $timezone }}</h2>
-            
+                <h2 class="banner-live-details">Live Streaming On {{ $Livestream_details->recurring_program }} from {{ $startTime }} to {{ $endTime }} - {{ $timezone }}</h2>
+
             @elseif ($Livestream_details->recurring_program == "weekly")
 
                 @switch($Livestream_details->recurring_program_week_day)
@@ -206,13 +206,13 @@
                     @default
                         @php $recurring_program_week_day = "Unknown"; @endphp
                 @endswitch
-            
+
                 <h2>Live Streaming On Every Week {{ $recurring_program_week_day }} from {{ $startTime }} to {{ $endTime }} - {{ $timezone }}</h2>
-            
+
             @elseif ($Livestream_details->recurring_program == "monthly")
-                    
+
                 <h2>Live Streaming On Every Month on Day {{ $Livestream_details->recurring_program_month_day }} from {{ $startTime }} to {{ $endTime }} - {{ $timezone }}</h2>
-            
+
             @elseif ($Livestream_details->recurring_program == "custom")
 
                 @php
@@ -224,26 +224,159 @@
                 <h3>({{ $timezone }})</h3>
             @endif
         @endif
+
+        <div class="container-fluid video-details">
+            <div class="row">
+
+                            {{-- BREADCRUMBS --}}
+
+                <div class="col-sm-12 col-md-12 col-xs-12">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="bc-icons-2">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a class="black-text"  href="{{ route('liveList') }}"> {{ucwords(__('Livestreams')) }}</a>
+                                        <i class="fa fa-angle-double-right mx-2" aria-hidden="true"></i>
+                                    </li>
+
+
+                                    <li class="breadcrumb-item"><a class="black-text">{{ strlen($Livestream_details->title) > 50 ? ucwords(substr($Livestream_details->title, 0, 120) . '...') : ucwords($Livestream_details->title) }}</a></li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-9 col-md-9 col-xs-12">
+                    <h1 class="trending-text big-title text-uppercase mt-3">
+                        {!!  optional($Livestream_details)->title !!}
+                    </h1>
+                </div>
+
+                <div class="col-sm-3 col-md-3 col-xs-12">
+                    <div class=" d-flex mt-4 pull-right">
+                        <div class="views">
+                            <span class="view-count"><i class="fa fa-eye"></i>
+                                {{ isset($view_increment) && $view_increment ? $Livestream_details->views + 1 : $Livestream_details->views }} {{  __('Views')  }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                    <!-- languages, categories time, public_current_time ,age_restrict -->
+            <div class=" align-items-center text-white text-detail p-0">
+                <span class="badge badge-secondary p-2">{{ __(@$Livestream_details->languages->name) }}</span>
+                <span class="badge badge-secondary p-2">{{ __(@$Livestream_details->categories->name) }}</span>
+                <span class="badge badge-secondary p-2">{{ __('Published On :'. $Livestream_details->public_current_time ) }} </span>
+                <span class="badge badge-secondary p-2">{{ __(@$Livestream_details->age_restrict) }}</span>
+            </div>
+
+                    <!-- Social Share, Like Dislike -->
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-xs-12">
+                    <ul class="list-inline p-0 mt-4 share-icons music-play-lists">
+
+                        {!! Theme::uses("default")->load("public/themes/default/views/video-js-Player/Livestream/live-socail-share", ['Livestream_details' => $Livestream_details, 'play_btn_svg' => $play_btn_svg])->content() !!}
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+                {{-- Description --}}
+        <div class="container-fluid">
+            <div class="text-white col-md-6 p-0">
+                <p class="trending-dec w-100 mb-0 text-white"> {!! html_entity_decode(__($Livestream_details->description)) !!}</p>
+                <p class="trending-dec w-100 mb-3 text-white">{!! html_entity_decode(__($Livestream_details->details)) !!}</p>
+            </div>
+        </div>
+
     </div>
 
 @elseif( $live_publish_later_status == true )
     <div id="" style="background: linear-gradient(0deg, rgba(0, 0, 0, 1.4), rgba(0, 0, 0, 0.3)), url({{ URL::to('/') }}/public/uploads/images/{{ $Livestream_details->player_image }}); background-repeat: no-repeat; background-size: cover; padding: 150px 10px;">
-            
-        <h2>{{ ucwords($Livestream_details->title) }}</h2><br>
 
-        <h2>{{ "Live Streaming Coming Soon On ". Carbon\Carbon::createFromFormat('Y-m-d\TH:i', $Livestream_details->publish_time)->format('l, jS F Y g:ia') }}</h2>
+        <h2 class="banner-live-details">{{ ucwords($Livestream_details->title) }}</h2><br>
+
+        <h2 class="banner-live-details">{{ "Live Streaming Coming Soon On ". Carbon\Carbon::createFromFormat('Y-m-d\TH:i', $Livestream_details->publish_time)->format('l, jS F Y g:ia') }}</h2>
+
+        <div class="container-fluid video-details">
+            <div class="row">
+
+                            {{-- BREADCRUMBS --}}
+
+                <div class="col-sm-12 col-md-12 col-xs-12">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="bc-icons-2">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a class="black-text"  href="{{ route('liveList') }}"> {{ucwords(__('Livestreams')) }}</a>
+                                        <i class="fa fa-angle-double-right mx-2" aria-hidden="true"></i>
+                                    </li>
+
+
+                                    <li class="breadcrumb-item"><a class="black-text">{{ strlen($Livestream_details->title) > 50 ? ucwords(substr($Livestream_details->title, 0, 120) . '...') : ucwords($Livestream_details->title) }}</a></li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-9 col-md-9 col-xs-12">
+                    <h1 class="trending-text big-title text-uppercase mt-3">
+                        {!!  optional($Livestream_details)->title !!}
+                    </h1>
+                </div>
+
+                <div class="col-sm-3 col-md-3 col-xs-12">
+                    <div class=" d-flex mt-4 pull-right">
+                        <div class="views">
+                            <span class="view-count"><i class="fa fa-eye"></i>
+                                {{ isset($view_increment) && $view_increment ? $Livestream_details->views + 1 : $Livestream_details->views }} {{  __('Views')  }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                    <!-- languages, categories time, public_current_time ,age_restrict -->
+            <div class=" align-items-center text-white text-detail p-0">
+                <span class="badge badge-secondary p-2">{{ __(@$Livestream_details->languages->name) }}</span>
+                <span class="badge badge-secondary p-2">{{ __(@$Livestream_details->categories->name) }}</span>
+                <span class="badge badge-secondary p-2">{{ __('Published On :'. $Livestream_details->public_current_time ) }} </span>
+                <span class="badge badge-secondary p-2">{{ __(@$Livestream_details->age_restrict) }}</span>
+            </div>
+
+                    <!-- Social Share, Like Dislike -->
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-xs-12">
+                    <ul class="list-inline p-0 mt-4 share-icons music-play-lists">
+
+                        {!! Theme::uses("default")->load("public/themes/default/views/video-js-Player/Livestream/live-socail-share", ['Livestream_details' => $Livestream_details, 'play_btn_svg' => $play_btn_svg])->content() !!}
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+                {{-- Description --}}
+        <div class="container-fluid">
+            <div class="text-white col-md-6 p-0">
+                <p class="trending-dec w-100 mb-0 text-white"> {!! html_entity_decode(__($Livestream_details->description)) !!}</p>
+                <p class="trending-dec w-100 mb-3 text-white">{!! html_entity_decode(__($Livestream_details->details)) !!}</p>
+            </div>
+        </div>
 
     </div>
 @endif
 
 <script>
-    $(document).ready(function () {  
+    $(document).ready(function () {
 
         let recurring_program_check_exist = "{{ json_encode($recurring_program_Status) }}";
         let live_publish_later_status_exist  = "{{ json_encode($live_publish_later_status) }}";
 
         if (recurring_program_check_exist == "true") {
-            
+
             let reloadInterval = setInterval(function() {
 
                 recurring_program_check_exist = "{{ json_encode($recurring_program_Status) }}";
