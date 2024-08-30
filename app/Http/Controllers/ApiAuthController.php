0fe7a8719507380609ce9316f947c6869f5afef9
Manikandan <manikandan@webnexs.in>
2024-08-30 19:08:11 +0530

    EPG_Pagelist

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -15747,7 +15747,7 @@ public function QRCodeMobileLogout(Request $request)
         $carbon_current_time =  $carbon_now->format('H:i:s');
         $carbon_today =  $carbon_now->format('n-j-Y');
 
-        $data =  AdminEPGChannel::where('status',1)->get()->map(function ($item) use ($homepage_default_image_url , $carbon_now , $carbon_today , $current_timezone) {
+        $data =  AdminEPGChannel::where('status',1)->limit($homepage_input_array['limit'])->get()->map(function ($item) use ($homepage_default_image_url , $carbon_now , $carbon_today , $current_timezone) {
                     
           $item['image_url'] = $item->image != null ? URL::to('public/uploads/EPG-Channel/'.$item->image ) : $homepage_default_image_url['homepage_default_vertical_image_url'] ;
           $item['Player_image_url'] = $item->player_image != null ?  URL::to('public/uploads/EPG-Channel/'.$item->player_image ) : $homepage_default_image_url['homepage_default_horizontal_image_url'] ;
@@ -15790,6 +15790,11 @@ public function QRCodeMobileLogout(Request $request)
       $source_name = $request->source_name;
       $data = [];
       $Page_List_Name = 'No data';
+
+      $Pagelist_default_image_url = array(
+        'default_vertical_image_url' => default_vertical_image_url(),
+        'default_horizontal_image_url' => default_horizontal_image_url(),
+      );
       
       if ($source_name != null) {
 
@@ -15929,6 +15934,11 @@ public function QRCodeMobileLogout(Request $request)
               case 'Series_based_on_genre':
                 $data = $this->Series_based_on_genre_Pagelist();
                 $Page_List_Name = 'Series_based_on_genre_Pagelist';
+                break;  
+
+              case 'EPG':
+                $data = $this->EPG_Pagelist( $Pagelist_default_image_url );
+                $Page_List_Name = 'EPG_Pagelist';
                 break;  
           }
       }
@@ -16654,6 +16664,45 @@ public function QRCodeMobileLogout(Request $request)
     
   }
 
+  private static function EPG_Pagelist( $Pagelist_default_image_url ){
+
+    $current_timezone = current_timezone();
+    $carbon_now = Carbon::now($current_timezone);
+    $carbon_current_time =  $carbon_now->format('H:i:s');
+    $carbon_today =  $carbon_now->format('n-j-Y');
+
+    $data =  AdminEPGChannel::where('status',1)->get()->map(function ($item) use ($Pagelist_default_image_url , $carbon_now , $carbon_today , $current_timezone) {
+                
+      $item['image_url'] = $item->image != null ? URL::to('public/uploads/EPG-Channel/'.$item->image ) : $Pagelist_default_image_url['default_vertical_image_url'] ;
+      $item['Player_image_url'] = $item->player_image != null ?  URL::to('public/uploads/EPG-Channel/'.$item->player_image ) : $Pagelist_default_image_url['default_horizontal_image_url'] ;
+      $item['tv_image_url']     = $item->player_image != null ?  URL::to('public/uploads/EPG-Channel/'.$item->player_image ) : $Pagelist_default_image_url['default_horizontal_image_url'] ;
+      $item['Logo_url'] = $item->logo != null ?  URL::to('public/uploads/EPG-Channel/'.$item->logo ) : $Pagelist_default_image_url['default_vertical_image_url'] ;
+                                          
+      $item['ChannelVideoScheduler_current_video_details']  =  ChannelVideoScheduler::where('channe_id',$item->id)->where('choosed_date' , $carbon_today )
+                                                                  ->get()->map(function ($item) use ($carbon_now , $current_timezone) {
+
+                                                                      $TimeZone   = TimeZone::where('id',$item->time_zone)->first();
+
+                                                                      $converted_start_time =Carbon::createFromFormat('m-d-Y H:i:s', $item->choosed_date . $item->start_time, $TimeZone->time_zone )
+                                                                                                                      ->copy()->tz( $current_timezone );
+
+                                                                      $converted_end_time =Carbon::createFromFormat('m-d-Y H:i:s', $item->choosed_date . $item->end_time, $TimeZone->time_zone )
+                                                                                                                      ->copy()->tz( $current_timezone );
+
+                                                                      if ($carbon_now->between($converted_start_time, $converted_end_time)) {
+                                                                          $item['video_image_url'] = URL::to('public/uploads/images/'.$item->image ) ;
+                                                                          $item['converted_start_time'] = $converted_start_time->format('h:i A');
+                                                                          $item['converted_end_time']   =   $converted_end_time->format('h:i A');
+                                                                          return $item ;
+                                                                      }
+
+                                                                  })->filter()->first();
+      $item['source'] = 'EPG';
+      return $item;
+    });
+    
+    return $data;
+  }
 
   public function website_baseurl()
   {
