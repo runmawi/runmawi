168125bb2decb67f4fc75ac15f142a8204e2ea21
Manivel S <manivel@webnexs.in>
2024-09-12 13:42:09 +0530

    expiry duration for ppv in admin panel

---
+++ database/migrations/2024_09_12_062249_rename_expiry_dayts_column_in_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameExpiryDaytsColumnInSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->renameColumn('started_video_expiry_datys', 'started_video_expiry_days');
+            $table->renameColumn('before_video_expiry_datys', 'before_video_expiry_days');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            // Reverse the renaming process
+            $table->renameColumn('started_video_expiry_days', 'started_video_expiry_datys');
+            $table->renameColumn('before_video_expiry_days', 'before_video_expiry_datys');
+        });
+    }
+}
