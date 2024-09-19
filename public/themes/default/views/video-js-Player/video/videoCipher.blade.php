93824ffeef836e94af14a8775727a6e59012b53f
Manivel S <manivel@webnexs.in>
2024-09-19 18:44:48 +0530

    player back btn & title

--- public/themes/default/views/video-js-Player/video/videoCipher.blade.php
+++ public/themes/default/views/video-js-Player/video/videoCipher.blade.php
@@ -1,6 +1,11 @@
 @php include public_path('themes/default/views/header.php');  @endphp
 
     <div class="container-fluid p-0" style="position:relative">
+        <button class="staticback-btn" onclick="history.back()" title="Back Button">
+            <i class="fa fa-chevron-left" aria-hidden="true"></i>
+        </button>
+
+        <div class="vjs-title-bar">{{$videodetail->title}}</div>
 
         <iframe
         src="https://player.vdocipher.com/v2/?otp={{ $videodetail->otp }}&playbackInfo={{ $videodetail->playbackInfo }}&primaryColor=4245EF"
@@ -15,4 +20,6 @@
     <style>
         #main-header{display: none;}
         .container-fluid.p-0{height: 100vh;overflow: hidden;}
+        .staticback-btn{display:inline-block;position:absolute;background:transparent;z-index:1;top:5%;left:1%;color:#fff;border:none;cursor:pointer;font-size:25px}
+        .vjs-title-bar{position:absolute;background:transparent;z-index:1;top:5.6%;left:4%;color:#fff;font-size:20px;font-weight:500;border-top:none;border-bottom:none;border-right:none;border-left:3px solid #fd0000;padding-left:10px;height:25px;display:flex;align-items:center}
     </style>
