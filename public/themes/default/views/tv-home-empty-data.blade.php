edc07375f09a84d051cfd92a4ac6e37d5b4e204b
Balaji <balaji.b@webnexs.in>
2024-10-07 17:41:54 +0530

    bug fixes

--- public/themes/default/views/tv-home-empty-data.blade.php
+++ public/themes/default/views/tv-home-empty-data.blade.php
@@ -6,7 +6,7 @@
     <div class="col-md-12 text-center mt-4"
         style="background: url(<?= URL::to('/assets/img/watch.png') ?>);heigth: 500px;background-position:center;background-repeat: no-repeat;background-size:contain;height: 500px!important;">
         <p>
-        <h3 class="text-center">{{ __('No Tv-Show Videos Available') }}</h3>
+        <h3 class="text-center">{{ __('No Tv-Show Available') }}</h3>
     </div>
 
 
