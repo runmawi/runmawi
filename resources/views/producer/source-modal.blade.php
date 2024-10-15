b1867d94edc8a128858a045561ef9fd2d5dccfcd
Manikandan <manikandan19598@gmail.com>
2024-10-15 22:54:03 +0530

    style css

--- resources/views/producer/source-modal.blade.php
+++ resources/views/producer/source-modal.blade.php
@@ -13,33 +13,34 @@
 
                 <h6>Movies</h6>
                 <p>
-                    @foreach ($sources_data['video'] as $item)
-                        <div class="collection">
-                            <a href="{{ route('producer.stats',['video', $item->id]) }}"> {{ @$item->title }}  </a>
-                            <br>
-                        </div>
-                    @endforeach
+                    <div class="collection">
+                        @foreach ($sources_data['video'] as $item)
+                            <a class="collection-item" href="{{ route('producer.stats',['video', $item->id]) }}"> {{ @$item->title }}  </a>
+                        @endforeach
+                    </div>
                 </p>
 
                 <h6>Tv Shows (Series)</h6>
                 <p>
                     @foreach ($sources_data['series'] as $item)
                         <div class="collection">
-                            <a href="{{ route('producer.stats',['series', $item->id]) }}"> {{ @$item->title }}  </a>
+                            <a class="collection-item" href="{{ route('producer.stats',['series', $item->id]) }}"> {{ @$item->title }}  </a>
                             <br>
                         </div>
                     @endforeach
                 </p>
+                <hr>
 
                 <h6>Livestream</h6>
                 <p>
                     @foreach ($sources_data['livestream'] as $item)
                         <div class="collection">
-                            <a href="{{ route('producer.stats',['live_stream', $item->id]) }}"> {{ @$item->title }}  </a>
+                            <a class="collection-item" href="{{ route('producer.stats',['live_stream', $item->id]) }}"> {{ @$item->title }}  </a>
                             <br>
                         </div>
                     @endforeach
                 </p>
+                <hr>
             </div>
             <div class="modal-footer">
                 <a href="#" class="modal-close waves-effect waves-green btn-flat">Cancel</a>
@@ -47,3 +48,9 @@
         </div>
     </div>
 </div>
+
+<style>
+    a.collection-item{font-size: 20px;}
+    h6{font-size: 1.5rem;line-height: 150%;}
+    a.modal-close.waves-effect.waves-green.btn-flat{font-size: 18px;}
+</style>
