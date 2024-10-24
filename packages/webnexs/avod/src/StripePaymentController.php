f7cbb5d00106fdb5e69049f905905d1f3f596679
Manikandan <manikandan19598@gmail.com>
2024-10-24 12:08:58 +0530

    Ads Payment

---
+++ packages/webnexs/avod/src/StripePaymentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Webnexs\Avod;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class StripePaymentController extends Controller
+{
+  public function FunctionName(Type $var = null)
+  {
+    # code...
+  }
+
+ 
+}
