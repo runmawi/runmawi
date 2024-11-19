f0f15ed0c04e70f7beb8f9a313c2b387b411519e
Manivel <manivel@webnexs.in>
2024-11-19 17:51:35 +0530

    share box hovering issue, video details pg icons functionality issues fixed

--- public/themes/default/views/video-js-Player/video/videos-details.blade.php
+++ public/themes/default/views/video-js-Player/video/videos-details.blade.php
@@ -232,6 +232,18 @@ input[type="radio"].payment_btn:checked::before, input[type="radio"].quality_opt
         display: flex !important;
         opacity: 1 !important;
     }
+    .share-box {
+        opacity: 0;
+        visibility: hidden;
+        transition: opacity 0.9s ease, visibility 0s 9s; /* 3s delay for hiding */
+    }
+
+    .share:hover .share-box {
+        opacity: 1;
+        visibility: visible;
+        transition: opacity 0.9s ease, visibility 0s 0s; /* Remove delay when hovered */
+    }
+
     
     /* @media(max-width:2800px){    .my-video.vjs-fluid{height:65vh !important;}} */
         #trailermodal .my-video.vjs-fluid{height: 68vh !important;}
@@ -373,12 +385,12 @@ input[type="radio"].payment_btn:checked::before, input[type="radio"].quality_opt
                                         <!-- Watchlater -->
                                     <li class="share">
                                         <span  data-toggle="modal"  data-video-id={{ $videodetail->id }} onclick="video_watchlater(this)" >
-                                            <i class="video-watchlater {{ !is_null($videodetail->watchlater_exist) ? "fal fa-minus" : "fal fa-plus "  }}"></i>
+                                            <i class="video-watchlater {{ !empty($videodetail->watchlater_exist) ? "fal fa-minus" : "fal fa-plus "  }}"></i>
                                         </span>
                                         <div class="share-box box-watchtrailer " onclick="video_watchlater(this)" style="top:41px">
                                             <div class="playbtn"  data-toggle="modal">  
                                                 <span class="text" style="background-color: transparent; font-size: 14px; width:124px; height:21px">
-                                                {{ !is_null($videodetail->watchlater_exist) ? "Remove from Watchlist" : "Add To Watchlist"  }}
+                                                {{ !empty($videodetail->watchlater_exist) ? "Remove from Watchlist" : "Add To Watchlist"  }}
                                                 </span>
                                             </div>
                                         </div>
@@ -1377,6 +1389,13 @@ input[type="radio"].payment_btn:checked::before, input[type="radio"].quality_opt
             player.pause();
             $('#trailermodal').modal('hide');
         });
+    });
+
+    window.addEventListener('pageshow', function (event) {
+        if (event.persisted) {
+            console.log('reloaded');
+            window.location.reload();
+        }
     });
 </script>
 
