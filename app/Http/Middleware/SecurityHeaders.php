363c70f41b2db3e2a41aa6ecbd7571eafcb12510
Manivel <manivel@webnexs.in>
2025-02-12 13:14:04 +0530

    changes

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -13,7 +13,7 @@ class SecurityHeaders
 
         // Clickjacking Protection
         $response->headers->set('X-Frame-Options', 'DENY'); 
-        $response->headers->set('Content-Security-Policy', "frame-ancestors 'none';");
+        // $response->headers->set('Content-Security-Policy', "frame-ancestors 'none';");
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
         $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none'; base-uri 'self'; frame-src 'none'");
