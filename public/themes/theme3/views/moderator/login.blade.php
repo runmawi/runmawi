b97884f7801d1508068c1c1ac00122ee9de48df9
Manivel <manivel@webnexs.in>
2025-02-17 18:27:07 +0530

    changes

--- public/themes/theme3/views/moderator/login.blade.php
+++ public/themes/theme3/views/moderator/login.blade.php
@@ -49,7 +49,7 @@ $system_settings = App\SystemSetting::find(1);
 </style>
 </head>
 <body>
-<section class="sign-in-page" style="background:url('<?php echo URL::to('/').'/public/uploads/settings/'.$settings->login_content; ?>') no-repeat scroll 0 0;;background-size: cover;">
+<section class="sign-in-page" style="background:black;background-size: cover;">
    <div class="container">
       <div class="row  align-items-center justify-content-center height-self-center">
           <!-- <div class="col-lg-  col-md-6">
