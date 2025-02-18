74d700a2a7281eca7f536c4da670a9136d416e1c
Manivel <manivel@webnexs.in>
2025-02-18 15:03:28 +0530

    embed url playing issue fixed

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -12,7 +12,7 @@ class SecurityHeaders
         $response = $next($request);
 
         // Clickjacking Protection
-        $response->headers->set('X-Frame-Options', 'DENY'); 
+        // $response->headers->set('X-Frame-Options', 'DENY'); 
         // $response->headers->set('Content-Security-Policy', "frame-ancestors 'none';");
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
