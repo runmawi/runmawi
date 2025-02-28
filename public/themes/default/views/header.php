9226b8c4e8e8f3823e5543cbb430dbae607eaf4e
Arun Jyothi <arun.u@webnexs.in>
2025-02-28 11:53:44 +0530

    preload

--- public/themes/default/views/header.php
+++ public/themes/default/views/header.php
@@ -278,6 +278,7 @@
    <!-- js -->
    <link rel="preload" fetchpriority="high" href="<?= URL::to('/assets/js/jquery-3.4.1.min.js') ?>" as="script"/>
    <link rel="preload" fetchpriority="high" href="<?= URL::to('/assets/js/jquery.lazy.min.js');?>" as="script">
+   <link rel="preload" fetchpriority="high" href="https://runmawi.tv/public/uploads/images/player-image-1737718190.webp" as="image"/>
 
    <link rel="preload" href="<?= URL::to('/assets/js/jquery.3.4.1.js') ?>" as="script"/>
 
