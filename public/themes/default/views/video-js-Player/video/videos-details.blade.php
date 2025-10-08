1260e01bdd53f9856bc848276599d7e5f8eca200
Runmawi Production <runmawi1@Runmawis-Mac-mini-2.local>
2025-10-08 15:41:27 +0530

    Update on trailer

--- public/themes/default/views/video-js-Player/video/videos-details.blade.php
+++ public/themes/default/views/video-js-Player/video/videos-details.blade.php
@@ -557,9 +557,9 @@ input[type="radio"].payment_btn:checked::before, input[type="radio"].quality_opt
                         @if( optional($videodetail)->trailer )
 
                             <ul class="list-inline p-0 m-0 share-icons music-play-lists">
-                                <li class="share sharemobres">
-                                    <span  data-bs-toggle="modal" data-bs-target="#trailermodal" style="width: 80px !important;">   {{-- Trailer --}}
-                                        <i class="fal fa-play"></i> Watch trailer
+                                <li class="share sharemobres" style="width: 120px !important;">
+                                    <span  data-bs-toggle="modal" data-bs-target="#trailermodal" style="width: 120px !important;">   {{-- Trailer --}}
+                                        <i class="fal fa-play"></i> &nbsp; Watch trailer
                                     </span>
 
                                     <div class="share-box box-watchtrailer">
