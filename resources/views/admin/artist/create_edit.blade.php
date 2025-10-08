cd331d7ddb911444b203e8169dab1ebb74fc2691
Runmawi Production <runmawi1@Runmawis-Mac-mini-2.local>
2025-10-08 15:15:41 +0530

    Correct value

--- resources/views/admin/artist/create_edit.blade.php
+++ resources/views/admin/artist/create_edit.blade.php
@@ -124,7 +124,8 @@ border-radius: 0px 4px 4px 0px;
                                 <option value="Line_Producer" @if (!empty($artist->artist_type) && $artist->artist_type == 'Line_Producer'){{ 'selected' }} @endif > {{ 'Line Producer' }} </option>
                                 <option value="Asst_Director" @if (!empty($artist->artist_type) && $artist->artist_type == 'Asst_Director'){{ 'selected' }} @endif > {{ 'Asst. Director' }} </option>
                                 <option value="Actress" @if (!empty($artist->artist_type) && $artist->artist_type == 'Actress'){{ 'selected' }} @endif > {{ 'Actress' }} </option>
-                                <option value="Supporting_Actor/Actress" @if (!empty($artist->artist_type) && $artist->artist_type == 'Supporting_Actor/Actress'){{ 'selected' }} @endif > {{ 'Supporting Actor/Actress' }} </option>
+                                <option value="Supporting_Actress" @if (!empty($artist->artist_type) && $artist->artist_type == 'Supporting_Actress'){{ 'selected' }} @endif > {{ 'Supporting Actress' }} </option>
+                                <option value="Supporting_Actor" @if (!empty($artist->artist_type) && $artist->artist_type == 'Supporting_Actor'){{ 'selected' }} @endif > {{ 'Supporting Actor' }} </option>
                                 <option value="Screen_Writer" @if (!empty($artist->artist_type) && $artist->artist_type == 'Screen_Writer'){{ 'selected' }} @endif > {{ 'Screen Writer' }} </option>
                                 <option value="Editor" @if (!empty($artist->artist_type) && $artist->artist_type == 'Editor'){{ 'selected' }} @endif > {{ 'Editor' }} </option>
                                 
